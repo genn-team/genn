79e62effc5d1b660655d4d24ef93482fec1886e7
Esin Yavuz <esinyavuz@yahoo.com>
2014-02-04 18:59:20 +0000

    Corrected the Izhikevich model with fixed parameters (used for the OneComp example) with respect to the previous changes.

--- lib/include/utils.h
+++ lib/include/utils.h
@@ -259,16 +259,16 @@ void prepareStandardModels()
   n.pNames.push_back(tS("d")); // after-spike reset value of U
   n.dpNames.clear(); 
   //TODO: replace the resetting in the following with BRIAN-like threshold and resetting 
-  n.simCode= tS("    if ($(V) = 30){\n\
+  n.simCode= tS("    if ($(V) >= 30){\n\
       $(V)=$(c);\n\
 		  $(U)+=$(d);\n\
     } \n\
     $(V)+=0.5f*(0.04f*$(V)*$(V)+5*$(V)+140-$(U)+$(Isyn))*DT; //at two times for numerical stability\n\
     $(V)+=0.5f*(0.04f*$(V)*$(V)+5*$(V)+140-$(U)+$(Isyn))*DT;\n\
     $(U)+=$(a)*($(b)*$(V)-$(U))*DT;\n\
-    if ($(V) > 30){\n   //keep this only for visualisation -- not really necessaary otherwise \
+    if ($(V) > 30){   //keep this only for visualisation -- not really necessaary otherwise \n\
       $(V)=30;\n\
-    }");
+    }\n");
     
  /* n.thresholdCode=tS("//threshold code is here\n\
       bool _cond=FALSE;\n \ 
