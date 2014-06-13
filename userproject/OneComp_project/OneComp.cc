8bfe48bb00a1a3c2d98238ad15e462dfe87b685b
James Turner <jt273@sussex.ac.uk>
2014-06-13 18:07:45 +0100

    Applied block size optimisation update to development branch - see rev. 51b7e9 commit notes for details. Also changed generateALL.cpp back to generateALL.cc, to avoid confusion.

---
+++ userproject/OneComp_project/OneComp.cc
@@ -0,0 +1,60 @@
+/*--------------------------------------------------------------------------
+   Author: Thomas Nowotny
+  
+   Institute: Center for Computational Neuroscience and Robotics
+              University of Sussex
+	      Falmer, Brighton BN1 9QJ, UK 
+  
+   email to:  T.Nowotny@sussex.ac.uk
+  
+   initial version: 2010-02-07
+  
+--------------------------------------------------------------------------*/
+#define DT 1.0
+#include "modelSpec.h"
+#include "modelSpec.cc"
+
+float exIzh_p[4]={
+//Izhikevich model parameters - tonic spiking
+	0.02,	// 0 - a
+	0.2, 	// 1 - b
+	-65, 	// 2 - c
+	6 	// 3 - d
+};
+
+float exIzh_ini[2]={
+//Izhikevich model initial conditions - tonic spiking
+	-65,	//0 - V
+	-20	//1 - U
+};
+
+
+float mySyn_p[3]= {
+  0.0,           // 0 - Erev: Reversal potential
+  -20.0,         // 1 - Epre: Presynaptic threshold potential
+  1.0            // 2 - tau_S: decay time constant for S [ms]
+};
+
+float postExp[2]={
+  1.0,            // 0 - tau_S: decay time constant for S [ms]
+  0.0		  // 1 - Erev: Reversal potential
+};
+float postSynV[0]={
+};
+
+
+#include "../../userproject/include/sizes.h"
+
+float inpIzh1 = 4.0;
+//float gIzh1= 0.01;
+
+void modelDefinition(NNmodel &model) 
+{
+  model.setName("OneComp");
+  model.addNeuronPopulation("Izh1", _NC1, IZHIKEVICH, exIzh_p, exIzh_ini);        	 
+ // model.addSynapsePopulation("IzhIzh", NSYNAPSE, ALLTOALL, INDIVIDUALG, NO_DELAY, IZHIKEVICH_PS, "Izh1", "Izh1", mySyn_p, postSynV, postExp);
+ // model.setSynapseG("IzhIzh", gIzh1);
+  
+  model.activateDirectInput("Izh1", CONSTINP);
+  model.setConstInp("Izh1", inpIzh1);
+}
---
+++ userproject/include/sizes.h
@@ -0,0 +1,6 @@
+#define _NExc 80000
+#define _NInh 20000
+#define _NMaxConnP0 854
+#define _NMaxConnP1 254
+#define _NMaxConnP2 844
+#define _NMaxConnP3 250
