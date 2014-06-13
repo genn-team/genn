29357480ac80695f9a8c5517f1830a9bf104263a
James Turner <jt273@sussex.ac.uk>
2014-06-13 18:07:45 +0100

    Applied block size optimisation update to development branch - see rev. 51b7e9 commit notes for details. Also changed generateALL.cpp back to generateALL.cc, to avoid confusion.

--- lib/src/generateALL.cc
+++ lib/src/generateALL.cc
@@ -90,18 +90,16 @@ int chooseDevice(ostream &mos,   //!< output stream for messages
   if (optimiseBlockSize) { // IF OPTIMISATION IS ON: Choose the device which supports the highest warp occupancy.
     mos << "optimizing block size..." << endl;
 
-    stringstream command;
-    char pipeBuffer[256];
-    stringstream ptxInfo;
+    char buffer[256];
+    stringstream command, ptxInfo;
     string kernelName, junk;
-    int reqRegs, reqSmem;
-    bool ptxInfoFound = false;
+    int reqRegs, reqSmem, requiredBlocks, ptxInfoFound = 0;
 
     unsigned int *bestSynBlkSz= new unsigned int[deviceCount];
     unsigned int *bestLrnBlkSz= new unsigned int[deviceCount];
     unsigned int *bestNrnBlkSz= new unsigned int[deviceCount];
-    unsigned int **blockSizePointer;
-    float warpAllocGran, regAllocGran, smemAllocGran;
+    unsigned int **blockSizePtr;
+    vector<unsigned int> *groupSizePtr;
     float blockLimit, mainBlockLimit, bestOccupancy;
     int deviceOccupancy, bestDeviceOccupancy = 0;
 
@@ -114,70 +112,97 @@ int chooseDevice(ostream &mos,   //!< output stream for messages
       bestNrnBlkSz[device] = 0;
 
       // Run NVCC and pipe output to this process.
+      mos << "dry-run compile for device " << device << endl;
+      command.str("");
       command << "nvcc -x cu -cubin -Xptxas=-v -arch=sm_" << deviceProp[device].major;
       command << deviceProp[device].minor << " -DDT -D\"CHECK_CUDA_ERRORS(call){call;}\" ";
       command << path << "/" << (*model).name << "_CODE/runner.cc 2>&1";
+      //mos << command.str() << endl;
+
 #ifdef _WIN32
       FILE *nvccPipe = _popen(command.str().c_str(), "r");
 #else // UNIX
       FILE *nvccPipe = popen(command.str().c_str(), "r");
 #endif
-      command.str("");
-      mos << "dry-run compile for device " << device << endl;
-      //mos << command.str() << endl;
+
       if (!nvccPipe) {
 	mos << "ERROR: failed to open nvcc pipe" << endl;
 	exit(EXIT_FAILURE);
       }
 
-      // Read pipe until reg / smem usage is found, then calculate optimum block size for each kernel.
-      while (fgets(pipeBuffer, 256, nvccPipe) != NULL) {
-	if (strstr(pipeBuffer, "error:") != NULL) {
-	  cout << pipeBuffer;
+      // Get the sizes of each synapse / learn / neuron group present on this host and device
+      vector<unsigned int> synapseN, learnN;
+      for (int group = 0; group < (*model).synapseGrpN; group++) {
+	if ((*model).synapseConnType[group] == SPARSE) {
+	  synapseN.push_back((*model).maxConn[group]);
 	}
-	else if (strstr(pipeBuffer, "calcSynapses") != NULL) {
-	  blockSizePointer = &bestSynBlkSz;
-	  kernelName = "synapse";
+	else {
+	  synapseN.push_back((*model).neuronN[(*model).synapseTarget[group]]);
 	}
-	else if (strstr(pipeBuffer, "learnSynapses") != NULL) {
-	  blockSizePointer = &bestLrnBlkSz;
-	  kernelName = "learn";
+	if ((*model).synapseType[group] == LEARN1SYNAPSE) {
+	  learnN.push_back((*model).neuronN[(*model).synapseSource[group]]);
 	}
-	else if (strstr(pipeBuffer, "calcNeurons") != NULL) {
-	  blockSizePointer = &bestNrnBlkSz;
-	  kernelName = "neuron";
-	}
-	if (strncmp(pipeBuffer, "ptxas info    : Used", 20) == 0) {
-	  ptxInfoFound = true;
-	  bestOccupancy = 0;
-	  ptxInfo << pipeBuffer;
-	  ptxInfo >> junk >> junk >> junk >> junk >> reqRegs >> junk >> reqSmem;
-	  ptxInfo.str("");
-	  mos << "kernel: " << kernelName << ", regs needed: " << reqRegs << ", smem needed: " << reqSmem << endl;
+      }
 
-	  // This data is required for block size optimisation, but cannot be found in deviceProp.
-	  if (deviceProp[device].major == 1) {
-	    smemAllocGran = 512;
-	    warpAllocGran = 2;
-	    regAllocGran = (deviceProp[device].minor < 2) ? 256 : 512;
-	  }
-	  else if (deviceProp[device].major == 2) {
-	    smemAllocGran = 128;
-	    warpAllocGran = 2;
-	    regAllocGran = 128;
-	  }
-	  else { // major == 3
-	    smemAllocGran = 256;
-	    warpAllocGran = 4;
-	    regAllocGran = 256;
-	  }
+      // This data is required for block size optimisation, but cannot be found in deviceProp.
+      float warpAllocGran, regAllocGran, smemAllocGran, maxBlocksPerSM;
+      if (deviceProp[device].major == 1) {
+	smemAllocGran = 512;
+	warpAllocGran = 2;
+	regAllocGran = (deviceProp[device].minor < 2) ? 256 : 512;
+	maxBlocksPerSM = 8;
+      }
+      else if (deviceProp[device].major == 2) {
+	smemAllocGran = 128;
+	warpAllocGran = 2;
+	regAllocGran = 128;
+	maxBlocksPerSM = 8;
+      }
+      else if (deviceProp[device].major == 3) {
+	smemAllocGran = 256;
+	warpAllocGran = 4;
+	regAllocGran = 256;
+	maxBlocksPerSM = 16;
+      }
+      else {
+	cerr << "Error: unsupported CUDA device major version: " << deviceProp[device].major << endl;
+	exit(EXIT_FAILURE);
+      }
+
+      // Read pipe until reg / smem usage is found, then calculate optimum block size for each kernel.
+      while (fgets(buffer, 256, nvccPipe) != NULL) {
+	if (strstr(buffer, "error:") != NULL) {
+	  cout << buffer;
+	}
+	else if (strstr(buffer, "calcSynapses") != NULL) {
+	  kernelName = "synapse";
+	  blockSizePtr = &bestSynBlkSz;
+	  groupSizePtr = &synapseN;
+	}
+	else if (strstr(buffer, "learnSynapses") != NULL) {
+	  kernelName = "learn";
+	  blockSizePtr = &bestLrnBlkSz;
+	  groupSizePtr = &learnN;
+	}
+	else if (strstr(buffer, "calcNeurons") != NULL) {
+	  kernelName = "neuron";
+	  blockSizePtr = &bestNrnBlkSz;
+	  groupSizePtr = &(*model).neuronN;
+	}
+	if (strncmp(buffer, "ptxas info    : Used", 20) == 0) {
+	  ptxInfoFound = 1;
+	  bestOccupancy = 0;
+	  ptxInfo.str("");
+	  ptxInfo << buffer;
+	  ptxInfo >> junk >> junk >> junk >> junk >> reqRegs >> junk >> reqSmem;
+	  mos << "kernel: " << kernelName << ", regs needed: " << reqRegs << ", smem needed: " << reqSmem << endl;
 
 	  // Test all block sizes (in warps) up to [max warps per block].
 	  for (int blockSize = 1; blockSize <= deviceProp[device].maxThreadsPerBlock / 32; blockSize++) {
 
 	    // BLOCK LIMIT DUE TO THREADS
 	    blockLimit = floor(deviceProp[device].maxThreadsPerMultiProcessor / 32 / blockSize);
-	    if (blockLimit > 8) blockLimit = 8; // mind the blocks per SM limit
+	    if (blockLimit > maxBlocksPerSM) blockLimit = maxBlocksPerSM;
 	    mainBlockLimit = blockLimit;
 
 	    // BLOCK LIMIT DUE TO REGISTERS
@@ -198,13 +223,27 @@ int chooseDevice(ostream &mos,   //!< output stream for messages
 	    blockLimit = floor(deviceProp[device].sharedMemPerBlock / blockLimit);
 	    if (blockLimit < mainBlockLimit) mainBlockLimit = blockLimit;
 
-	    // Update the best thread occupancy and the block size which enables it.
-	    if ((blockSize * 32 * mainBlockLimit) > bestOccupancy) {
-	      bestOccupancy = blockSize * 32 * mainBlockLimit;
-	      (*blockSizePointer)[device] = (unsigned int)blockSize * 32;
+	    // The number of [blockSize] padded thread blocks required to simulate all groups
+	    requiredBlocks = 0;
+	    for (int group = 0; group < groupSizePtr->size(); group++) {
+	      requiredBlocks += ceil((float) (*groupSizePtr)[group] / (float) (blockSize * 32));
+	    }
+
+	    // Use a small block size if it allows all groups to occupy the device concurrently
+	    if (requiredBlocks <= (mainBlockLimit * deviceProp[device].multiProcessorCount)) {
+	      (*blockSizePtr)[device] = (unsigned int) blockSize * 32;
+	      bestDeviceOccupancy = requiredBlocks * blockSize;
+	      chosenDevice = device;
+	      break;
+	    }
+
+	    // Update the best warp occupancy and the block size which enables it.
+	    if ((blockSize * mainBlockLimit) > bestOccupancy) {
+	      bestOccupancy = blockSize * mainBlockLimit;
+	      (*blockSizePtr)[device] = (unsigned int) blockSize * 32;
 
 	      // Choose this device and set optimal block sizes if it enables higher neuron kernel occupancy.
-	      if (blockSizePointer == &bestNrnBlkSz) {
+	      if (blockSizePtr == &bestNrnBlkSz) {
 		deviceOccupancy = bestOccupancy * deviceProp[device].multiProcessorCount;
 		if (deviceOccupancy >= bestDeviceOccupancy) {
 		  bestDeviceOccupancy = deviceOccupancy;
