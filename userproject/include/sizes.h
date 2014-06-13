19afdd917b46e1e947b43bf7ed0bb872ef85fa09
Thomas Nowotny <t.nowotny@sussex.ac.uk>
2014-06-13 21:37:09 +0100

    Fixed the time step dependence of Poisson neurons. They should now be independent of the time step at no cost for 0.5 ms timesteps and small cost for others. In the future one can revisit to see whether input rates can be pre-scaled to avoid the scaling multiplication.
    On the way I also improved on the "time drift problem" - it's simply
    due to rounding errors. Single precision floats are pretty bad ...

--- userproject/include/sizes.h
+++ userproject/include/sizes.h
@@ -1,6 +1,4 @@
-#define _NExc 80000
-#define _NInh 20000
-#define _NMaxConnP0 854
-#define _NMaxConnP1 254
-#define _NMaxConnP2 844
-#define _NMaxConnP3 250
+#define _NAL 100
+#define _NMB 1000
+#define _NLHI 20
+#define _NLB 100
