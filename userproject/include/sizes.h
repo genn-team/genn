096ae7d1b21fd4d660c72cbb53c396879d48ff9d
Esin Yavuz <ey50@sussex.ac.uk>
2014-06-17 18:23:15 +0100

    Fixed the incoming spike looping in generateKernels in case of learning. Fixed SynDelaySim.cu to use spike event variables.

--- userproject/include/sizes.h
+++ userproject/include/sizes.h
@@ -1,6 +1,4 @@
-#define _NExc 8000
-#define _NInh 2000
-#define _NMaxConnP0 840
-#define _NMaxConnP1 295
-#define _NMaxConnP2 849
-#define _NMaxConnP3 248
+#define _NAL 100
+#define _NMB 1000
+#define _NLHI 20
+#define _NLB 100
