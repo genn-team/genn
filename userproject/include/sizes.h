727971dd1485ffe15e20b1d93b6d07a167c9e45c
Esin Yavuz <esinyavuz@yahoo.com>
2014-07-02 11:13:21 +0100

    Some parts of the learning code was still using events. Before atomicAdd for spike counts, we check if the number is bigger than 0 to avoid unnecessary atomic operations (makes the GPU code slightly faster). Removed some unnecessary conditionals.

--- userproject/include/sizes.h
+++ userproject/include/sizes.h
@@ -1,4 +1,6 @@
-#define _NAL 100
-#define _NMB 1000
-#define _NLHI 20
-#define _NLB 100
+#define _NExc 80000
+#define _NInh 20000
+#define _NMaxConnP0 851
+#define _NMaxConnP1 253
+#define _NMaxConnP2 850
+#define _NMaxConnP3 252
