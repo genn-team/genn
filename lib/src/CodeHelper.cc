309c4576bb26e882e200da1f823e04d016a31973
Esin Yavuz <esinyavuz@yahoo.com>
2014-07-02 15:40:02 +0100

    modified include files to support new synapse types

---
+++ lib/src/CodeHelper.cc
@@ -0,0 +1,66 @@
+
+#ifndef __CODE_HELPER_CC
+#define  __CODE_HELPER_CC
+#include <iostream>
+#include <cstring>
+#include <string>
+#include <sstream>
+#include <vector>
+
+#define SAVEP(X) "(" << X << ")" //! Macro for a "safe" output of a parameter into generated code by essentially just adding a bracket around the parameter value in the generated code.
+
+#define OB(X) hlp.openBrace(X) //shortcut nomenclature to open the Xth curly brace { plus a new line
+#define CB(X) hlp.closeBrace(X) //shortcut nomenclature to close the Xth curly brace } plus a new line
+#define ENDL hlp.endl()//shortcut nomenclature to generate a newline followed correct number of indentation characters for the current level
+
+class CodeHelper {
+public:
+	vector<unsigned int>  braces;
+	bool verbose;
+
+	CodeHelper(): verbose(false) {
+		braces.push_back(0);
+	}
+
+	void setVerbose(bool isVerbose) {
+		verbose = isVerbose;
+	}
+	string openBrace(unsigned int level) {
+		braces.push_back(level);
+		if (verbose) printf("%sopen %u.\n",indentBy(braces.size() - 1).c_str(),level);
+		string result  = " {\n";
+		result.append(indentBy(braces.size() - 1));
+		return  result;
+	}
+
+	string closeBrace(unsigned int level) {
+		if (braces.back()==level) {
+			if (verbose) printf("%sclose %u.\n",indentBy(braces.size() - 1).c_str(),level);
+			braces.pop_back();
+			string result  = "}\n";
+			result.append(indentBy(braces.size() - 1));
+			return result;
+		} else {
+			cerr << "Code generation error: Attempted to close brace " << level << ", expecting brace " << braces.back() << "\n" ;
+			exit(1);
+		}
+	}
+
+	string endl() {
+		string result =  "\n";
+		//put out right number of tabs for level depth
+		result.append(indentBy(braces.size() - 1));
+		return result;
+	}
+
+private:
+	string indentBy(unsigned int numIndents) {
+		string result =  ""; ///toString(numIndents);
+		for (int i = 0; i < numIndents; i++) {
+			result.append("\t");
+		}
+		return result;
+	}
+
+};
+#endif
