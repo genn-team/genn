0ea43b74a41b3befd5607f01c78b1b1f6d814337
James Turner <jt273@sussex.ac.uk>
2014-08-17 17:26:03 +0100

    Tested and debugged the Windows buildmodel.bat script, adding important fixes. Confirmed working.

--- lib/bin/buildmodel.bat
+++ lib/bin/buildmodel.bat
@@ -4,21 +4,20 @@ set MODELPATH=%cd%
 echo "model path:" %MODELPATH%
 set MODELNAME=%1
 echo "model name:" %MODELNAME%
-set DBGMODE=%2 # 1 if debugging, 0 if release
+set DBGMODE=%2
+if "%DBGMODE%"=="" set DBGMODE=0
 
 copy "%MODELPATH%\%MODELNAME%.cc" "%GeNNPATH%\lib\src\currentModel.cc"
-cd "$GeNNPATH\lib"
+cd "%GeNNPATH%\lib"
 
-if %DBGMODE% equ 1 (
-  echo "Debugging mode ON"
-
-  rem ---debug launch command goes here---
-
-) else (
+if "%DBGMODE%"=="0" (
   nmake /nologo /f WINmakefile clean
   nmake /nologo /f WINmakefile
   bin\generateALL %MODELPATH%
+) else (
+  echo "Debugging mode ON"
+  rem "Windows debug commands will go here ..."
 )
-cd "%MODELPATH%"
 
-echo "Model build complete ..."
+cd "%MODELPATH%"
+echo Model build complete ...
