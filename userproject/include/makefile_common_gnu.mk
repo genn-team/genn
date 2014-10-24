1ef90feddfa9d5739d46763b905805568c1294fc
jamesturner246 <jamesturner246@gmail.com>
2014-10-24 16:42:13 +0100

    Moved LINK_FLAGS to back of command in GNUmakefiles.

--- userproject/include/makefile_common_gnu.mk
+++ userproject/include/makefile_common_gnu.mk
@@ -61,7 +61,7 @@ all: release
 	$(NVCC) $(NVCCFLAGS) $(INCLUDE_FLAGS) $(GENCODE_FLAGS) $< -o $@ -c
 
 $(EXECUTABLE): $(OBJECTS)
-	$(CXX) $(CXXFLAGS) $(LINK_FLAGS) $(OBJECTS) -o $@
+	$(CXX) $(CXXFLAGS) $(OBJECTS) -o $@ $(LINK_FLAGS)
 
 .PHONY: release
 release: CXXFLAGS +=-O3 -ffast-math
