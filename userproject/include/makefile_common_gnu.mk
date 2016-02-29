9dc848893360593883aee9fadf49e1d910fdb42f
Thomas Nowotny <t.nowotny@sussex.ac.uk>
2016-02-29 21:05:31 +0000

    Fixed a bug where arm architecture on Jetson TK1 was not recognised as 32 bit architecture.

--- userproject/include/makefile_common_gnu.mk
+++ userproject/include/makefile_common_gnu.mk
@@ -17,7 +17,7 @@
 #-----------------------------------------------------------------
 
 # OS name (Linux or Darwin) and architecture (32 bit or 64 bit).
-OS_SIZE		:=$(shell uname -m | sed -e "s/i.86/32/" -e "s/x86_64/64/")
+OS_SIZE		:=$(shell uname -m | sed -e "s/i.86/32/" -e "s/x86_64/64/" -e "s/armv7l/32/")
 OS_UPPER 	:=$(shell uname -s 2>/dev/null | tr [:lower:] [:upper:])
 OS_LOWER 	:=$(shell uname -s 2>/dev/null | tr [:upper:] [:lower:])
 DARWIN  	:=$(strip $(findstring DARWIN, $(OS_UPPER)))
