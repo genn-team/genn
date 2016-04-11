2661d57033e7f7c999a2cde920a6319e717a16ad
jamesturner246 <jamesturner246@gmail.com>
2016-04-11 18:00:00 +0100

    Some syntax fixes to Windows makefiles.

--- userproject/include/makefile_common_win.mk
+++ userproject/include/makefile_common_win.mk
@@ -18,49 +18,49 @@
 
 # Global CUDA compiler settings
 !IFNDEF CPU_ONLY
-    NVCC		="$(CUDA_PATH)\bin\nvcc.exe"
+NVCC			="$(CUDA_PATH)\bin\nvcc.exe"
 !ENDIF
 !IFNDEF DEBUG
-    NVCCFLAGS		="$(NVCCFLAGS) --compiler-options $(OPTIMIZATIONFLAGS)"
+NVCCFLAGS		=$(NVCCFLAGS) --compiler-options $(OPTIMIZATIONFLAGS)
 !ELSE
-    NVCCFLAGS		="$(NVCCFLAGS) -g -G"
+NVCCFLAGS		=$(NVCCFLAGS) -g -G
 !ENDIF
 
 # Global C++ compiler settings
 !IFNDEF CPU_ONLY
-    CXXFLAGS		="$(CXXFLAGS) /nologo /EHsc"
+CXXFLAGS		=$(CXXFLAGS) /nologo /EHsc
 !ELSE
-    CXXFLAGS		="$(CXXFLAGS) /nologo /EHsc /DCPU_ONLY"
+CXXFLAGS		=$(CXXFLAGS) /nologo /EHsc /DCPU_ONLY
 !ENDIF
 !IFNDEF DEBUG
-    CXXFLAGS		="$(CXXFLAGS) $(OPTIMIZATIONFLAGS)"
+CXXFLAGS		=$(CXXFLAGS) $(OPTIMIZATIONFLAGS)
 !ELSE
-    CXXFLAGS		="$(CXXFLAGS) /debug /Zi /Od"
+CXXFLAGS		=$(CXXFLAGS) /debug /Zi /Od
 !ENDIF
 
 # Global include and link flags
 !IFNDEF CPU_ONLY
-    INCLUDE_FLAGS	="/I$(GENN_PATH)\lib\include /I$(GENN_PATH)\userproject\include /I$(CUDA_PATH)\include $(EXTRA_INCLUDE)"
-    !IF "$(PROCESSOR_ARCHITECTURE)" == "AMD64"
-        LINK_FLAGS	="$(CUDA_PATH)\lib\x64\cudart.lib"
-    !ELSEIF "$(PROCESSOR_ARCHITEW6432)" == "AMD64"
-        LINK_FLAGS	="$(CUDA_PATH)\lib\x64\cudart.lib"
-    !ELSE
-        LINK_FLAGS	="$(CUDA_PATH)\lib\Win32\cudart.lib"
-    !ENDIF
+INCLUDE_FLAGS		=/I"$(GENN_PATH)\lib\include" /I"$(GENN_PATH)\userproject\include" /I"$(CUDA_PATH)\include" $(EXTRA_INCLUDE)
+!IF "$(PROCESSOR_ARCHITECTURE)" == "AMD64"
+LINK_FLAGS		="$(CUDA_PATH)\lib\x64\cudart.lib"
+!ELSEIF "$(PROCESSOR_ARCHITEW6432)" == "AMD64"
+LINK_FLAGS		="$(CUDA_PATH)\lib\x64\cudart.lib"
 !ELSE
-    INCLUDE_FLAGS	="/I$(GENN_PATH)\lib\include /I$(GENN_PATH)\userproject\include $(EXTRA_INCLUDE)"
+LINK_FLAGS		="$(CUDA_PATH)\lib\Win32\cudart.lib"
+!ENDIF
+!ELSE
+INCLUDE_FLAGS		=/I"$(GENN_PATH)\lib\include" /I"$(GENN_PATH)\userproject\include" $(EXTRA_INCLUDE)
 !ENDIF
 
 # An auto-generated file containing your cuda device's compute capability
 !IFNDEF CPU_ONLY
-    !INCLUDE sm_version.mk
+!INCLUDE sm_version.mk
 !ENDIF
 
 # Infer object file names from source file names
-OBJECTS		="$(SOURCES:.cc=.obj)"
-OBJECTS		="$(OBJECTS:.cpp=.obj)"
-OBJECTS		="$(OBJECTS:.cu=.obj)"
+OBJECTS		=$(SOURCES:.cc=.obj)
+OBJECTS		=$(OBJECTS:.cpp=.obj)
+OBJECTS		=$(OBJECTS:.cu=.obj)
 
 # Target rules
 .SUFFIXES: .cu
@@ -68,16 +68,16 @@ OBJECTS		="$(OBJECTS:.cu=.obj)"
 all: $(EXECUTABLE)
 
 .cc.obj:
-	$(CXX) $(CXXFLAGS) $(INCLUDE_FLAGS) $< /Fo$@ /c
+	$(CXX) $(CXXFLAGS) $(INCLUDE_FLAGS) $** /Fo$@ /c
 
 .cpp.obj:
-	$(CXX) $(CXXFLAGS) $(INCLUDE_FLAGS) $< /Fo$@ /c
+	$(CXX) $(CXXFLAGS) $(INCLUDE_FLAGS) $** /Fo$@ /c
 
 .cu.obj:
-	$(NVCC) $(NVCCFLAGS) $(INCLUDE_FLAGS:/I=-I) $(GENCODE_FLAGS) $< /Fo$@ -c
+	$(NVCC) $(NVCCFLAGS) $(INCLUDE_FLAGS:/I=-I) $(GENCODE_FLAGS) $** /Fo$@ -c
 
 $(EXECUTABLE): $(OBJECTS)
-	$(CXX) $(CXXFLAGS) $(LINK_FLAGS) $(OBJECTS) /Fe$@
+	$(CXX) $(CXXFLAGS) $(LINK_FLAGS) $** /Fe$@
 
 clean:
 	-del $(EXECUTABLE) *.obj *.ilk *.pdb 2>nul
