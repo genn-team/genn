8768fae7eb45d4cbec0b04253b55ffc6ad057a68
jamie <neworderofjamie@gmail.com>
2017-01-26 18:15:54 +0000

    post vars in synapse dynamics tests

---
+++ tests/features/post_vars_in_synapse_dynamics_sparse/model.cc
@@ -0,0 +1,51 @@
+#include "modelSpec.h"
+
+// NEURONS
+//==============
+double neuron_ini[2] = { // one neuron variable
+    0.0, // 0 - the time
+    0.0  // 1 - individual shift
+};
+
+// Synapses
+//==================================================
+
+double synapses_ini[1]= {
+    0.0 // the copied time value
+};
+
+void modelDefinition(NNmodel &model)
+{
+  initGeNN();
+  model.setDT(0.1);
+  model.setName("post_vars_in_synapse_dynamics_sparse");
+
+  neuronModel n;
+  n.varNames = {"x", "shift"};
+  n.varTypes = {"scalar", "scalar"};
+
+  n.simCode= "$(x)= $(t)+$(shift);";
+
+  const int DUMMYNEURON= nModels.size();
+  nModels.push_back(n);
+
+  weightUpdateModel s;
+  s.varNames = {"w"};
+  s.varTypes = {"scalar"};
+  s.synapseDynamics = "$(w)= $(x_post);";
+  const int DUMMYSYNAPSE= weightUpdateModels.size();
+  weightUpdateModels.push_back(s);
+
+  model.addNeuronPopulation("pre", 10, DUMMYNEURON, NULL, neuron_ini);
+  model.addNeuronPopulation("post", 10, DUMMYNEURON, NULL, neuron_ini);
+  string synName= "syn";
+  for (int i= 0; i < 10; i++)
+  {
+      string theName= synName + std::to_string(i);
+      model.addSynapsePopulation(theName, DUMMYSYNAPSE, SPARSE, INDIVIDUALG, i,IZHIKEVICH_PS, "pre", "post",
+                                 synapses_ini, NULL,
+                                 NULL, NULL);
+  }
+  model.setPrecision(GENN_FLOAT);
+  model.finalize();
+}
