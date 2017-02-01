e8ca854125373453c55dfbfbbc48de36fb2b8a0a
jamie <neworderofjamie@gmail.com>
2017-02-01 17:52:12 +0000

    moved lcov functionality into calc_coverage (controlled by option)

--- tests/calc_coverage.sh
+++ tests/calc_coverage.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 # By default no flags are passed to genn-buildmodel.sh
 BUILD_FLAGS=""
+LCOV=0
 
 # Parse command line arguments
 OPTIND=1
-while getopts "c" opt; do
+while getopts "cl" opt; do
     case "$opt" in
     c)  BUILD_FLAGS="-c"
+        ;;
+    l) LCOV=1
         ;;
     esac
 done
@@ -29,7 +32,19 @@ for f in features/*;
 
         # Build and generate model (measuring coverage)
         genn-buildmodel.sh $BUILD_FLAGS -v model.cc &>msg || exit $?
-	
+
         # Pop feature directory
         popd
     done;
+
+if [ $LCOV -eq 1 ]; then
+  echo "Combining coverage data..."
+
+  # Combine all GCOV ouratput in child directories
+  lcov --directory . --capture --output-file coverage.txt -rc lcov_branch_coverage=1 &>msg
+
+  echo "Generating HTML coverage report..."
+
+  # Generate browseable HTML
+  genhtml coverage.txt --branch-coverage --output-directory ./code_coverage_report/ &>msg
+fi
