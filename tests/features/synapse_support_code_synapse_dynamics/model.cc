1aef7cef4d05170c71fa098d10927c6030b97fe2
jamie <neworderofjamie@gmail.com>
2017-02-15 15:01:38 +0000

    Test for accessing synapse support code from synapse dynamics

---
+++ tests/features/synapse_support_code_synapse_dynamics/model.cc
@@ -0,0 +1,52 @@
+#include "modelSpec.h"
+
+// NEURONS
+//==============
+double neuron_ini[2] = { // one neuron variable
+    0.0, // 0 - the time
+    0.0  // 1 - individual shift
+};
+
+// Synapses
+//==================================================
+
+double synapses_ini[1]= {
+    0.0 // the copied time value
+};
+
+void modelDefinition(NNmodel &model)
+{
+  initGeNN();
+  model.setDT(0.1);
+  model.setName("synapse_support_code_synapse_dynamics");
+
+  neuronModel n;
+  n.varNames = {"x", "shift"};
+  n.varTypes = {"scalar", "scalar"};
+
+  n.simCode= "$(x)= $(t)+$(shift);";
+
+  const int DUMMYNEURON= nModels.size();
+  nModels.push_back(n);
+
+  weightUpdateModel s;
+  s.varNames = {"w"};
+  s.varTypes = {"scalar"};
+  s.synapseDynamics_supportCode = "scalar getWeight(scalar x){ return x; }";
+  s.synapseDynamics = "$(w)= getWeight($(x_post));";
+  const int DUMMYSYNAPSE= weightUpdateModels.size();
+  weightUpdateModels.push_back(s);
+
+  model.addNeuronPopulation("pre", 10, DUMMYNEURON, NULL, neuron_ini);
+  model.addNeuronPopulation("post", 10, DUMMYNEURON, NULL, neuron_ini);
+  string synName= "syn";
+  for (int i= 0; i < 10; i++)
+  {
+      string theName= synName + std::to_string(i);
+      model.addSynapsePopulation(theName, DUMMYSYNAPSE, DENSE, INDIVIDUALG, i,IZHIKEVICH_PS, "pre", "post",
+                                 synapses_ini, NULL,
+                                 NULL, NULL);
+  }
+  model.setPrecision(GENN_FLOAT);
+  model.finalize();
+}
