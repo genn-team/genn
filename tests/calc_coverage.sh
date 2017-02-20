f10adebcd4d86e011f8e97fc263e13e0d4057542
jamie <neworderofjamie@gmail.com>
2017-02-20 17:36:26 +0000

    write all compiler output when building coverage to a single file and make the report optional rather than coverage calculation itself

--- tests/calc_coverage.sh
+++ tests/calc_coverage.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 # By default no flags are passed to genn-buildmodel.sh
 BUILD_FLAGS=""
-LCOV=0
+REPORT=0
 
 # Parse command line arguments
 OPTIND=1
-while getopts "cl" opt; do
+while getopts "cr" opt; do
     case "$opt" in
     c)  BUILD_FLAGS="-c"
         ;;
-    l) LCOV=1
+    r) REPORT=1
         ;;
     esac
 done
@@ -18,6 +18,9 @@ done
 pushd $GENN_PATH/lib
 make clean
 popd
+
+# Delete existing output
+rm -f msg
 
 # Loop through feature tests
 for f in features/*;
@@ -28,21 +31,21 @@ for f in features/*;
         pushd $f
 
         # Clean
-        make clean &>msg
+        make clean &>> ../../msg
 
         # Build and generate model (measuring coverage)
-        genn-buildmodel.sh $BUILD_FLAGS -v model.cc &>msg || exit $?
+        genn-buildmodel.sh $BUILD_FLAGS -v model.cc  &>>../../msg || exit $?
 
         # Pop feature directory
         popd
     done;
 
-if [ $LCOV -eq 1 ]; then
-  echo "Combining coverage data..."
+echo "Combining coverage data..."
 
-  # Combine all GCOV ouratput in child directories
-  lcov --directory . --capture --output-file coverage.txt -rc lcov_branch_coverage=1 &>msg
+# Combine all GCOV ouratput in child directories
+lcov --directory . --capture --output-file coverage.txt -rc lcov_branch_coverage=1 &>msg
 
+if [ $REPORT -eq 1 ]; then
   echo "Generating HTML coverage report..."
 
   # Generate browseable HTML
