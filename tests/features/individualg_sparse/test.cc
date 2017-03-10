c33988d4ab4750a1ce8954cc9150254b731a50e8
jamie <neworderofjamie@gmail.com>
2017-03-10 17:26:15 +0000

    first go at a test for synaptic transmission through different types of matrix

---
+++ tests/features/individualg_sparse/test.cc
@@ -0,0 +1,114 @@
+// Google test includes
+#include "gtest/gtest.h"
+
+// Auto-generated simulation code includess
+#include "individualg_sparse_CODE/definitions.h"
+
+// **NOTE** base-class for simulation tests must be
+// included after auto-generated globals are includes
+#include "../../utils/simulation_test.h"
+
+//----------------------------------------------------------------------------
+// SimulationTestIndividualGSparse
+//----------------------------------------------------------------------------
+class SimulationTestIndividualGSparse : public SimulationTest
+{
+public:
+    //----------------------------------------------------------------------------
+    // SimulationTest virtuals
+    //----------------------------------------------------------------------------
+    virtual void Init()
+    {
+        // Allocate sparse matrix
+        allocateSyn(17);
+
+        // Loop through presynaptic neurons
+        unsigned int c = 0;
+        for(unsigned int i = 0; i < 10; i++)
+        {
+            // Set start index for this presynaptic neuron's weight matrix row
+            CSyn.indInG[i] = c;
+            for(unsigned int j = 0; j < 4; j++)
+            {
+                // Get value this post synaptic neuron represents
+                const unsigned int j_value = (1 << j);
+
+                // If this postsynaptic neuron should be connected, add index
+                if(((i + 1) & j_value) != 0)
+                {
+                    CSyn.ind[c++] = j;
+                }
+            }
+        }
+
+        // Add end index
+        CSyn.indInG[10] = c;
+
+        // Fill weights
+        std::fill(&gSyn[0], &gSyn[17], 1.0f);
+
+
+    }
+
+    bool Simulate()
+    {
+        // Initialize sparse arrays
+        initializeAllSparseArrays();
+
+        for (int i = 0; i < (int)(10.0f / DT); i++)
+        {
+            // What value should neurons be representing this time step?
+            const unsigned int in_value = (i / 10) + 1;
+
+            // Input spike representing value
+            // **NOTE** neurons start from zero
+            glbSpkCntPre[0] = 1;
+            glbSpkPre[0] = (in_value - 1);
+
+#ifndef CPU_ONLY
+            if(GetParam())
+            {
+                pushPreSpikesToDevice();
+            }
+#endif  // CPU_ONLY
+
+            // Step GeNN
+            StepGeNN();
+
+            // Loop through output neurons
+            unsigned int out_value = 0;
+            for(unsigned int j = 0; j < 4; j++)
+            {
+                // If this neuron is representing 1 add value it represents to output
+                if(fabs(xPost[j] - 1.0f) < 1E-5)
+                {
+                    out_value += (1 << j);
+                }
+            }
+
+            // If input value isn't correctly decoded, return false
+            if(out_value != in_value)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
+
+TEST_P(SimulationTestIndividualGSparse, AcceptableError)
+{
+    // Check total error is less than some tolerance
+    EXPECT_TRUE(Simulate());
+}
+
+#ifndef CPU_ONLY
+auto simulatorBackends = ::testing::Values(true, false);
+#else
+auto simulatorBackends = ::testing::Values(false);
+#endif
+
+INSTANTIATE_TEST_CASE_P(SimCodeEventSparseInv,
+                        SimulationTestIndividualGSparse,
+                        simulatorBackends);
