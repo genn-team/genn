cf95d3ccd9f7f3a1e33144dbb22793b9e2e3c411
jamie <neworderofjamie@gmail.com>
2017-03-10 17:37:09 +0000

    dense version

---
+++ tests/features/decode_matrix_individualg_dense/test.cc
@@ -0,0 +1,54 @@
+// Google test includes
+#include "gtest/gtest.h"
+
+// Auto-generated simulation code includess
+#include "decode_matrix_individualg_dense_CODE/definitions.h"
+
+// **NOTE** base-class for simulation tests must be
+// included after auto-generated globals are includes
+#include "../../utils/simulation_test_decoder_matrix.h"
+
+//----------------------------------------------------------------------------
+// SimulationTestIndividualGDense
+//----------------------------------------------------------------------------
+class SimulationTestIndividualGDense : public SimulationTestDecoderMatrix
+{
+public:
+    //----------------------------------------------------------------------------
+    // SimulationTest virtuals
+    //----------------------------------------------------------------------------
+    virtual void Init()
+    {
+        // Loop through presynaptic neurons
+        unsigned int c = 0;
+        for(unsigned int i = 0; i < 10; i++)
+        {
+            // Set start index for this presynaptic neuron's weight matrix row
+            for(unsigned int j = 0; j < 4; j++)
+            {
+                // Get value this post synaptic neuron represents
+                const unsigned int j_value = (1 << j);
+
+                // If this postsynaptic neuron should be connected, add 1.0 otherwise 0.0
+                gSyn[c++] = (((i + 1) & j_value) != 0) ? 1.0f : 0.0f;
+
+            }
+        }
+    }
+};
+
+TEST_P(SimulationTestIndividualGDense, CorrectDecoding)
+{
+    // Check total error is less than some tolerance
+    EXPECT_TRUE(Simulate());
+}
+
+#ifndef CPU_ONLY
+auto simulatorBackends = ::testing::Values(true, false);
+#else
+auto simulatorBackends = ::testing::Values(false);
+#endif
+
+INSTANTIATE_TEST_CASE_P(DecodeMatrix,
+                        SimulationTestIndividualGDense,
+                        simulatorBackends);
