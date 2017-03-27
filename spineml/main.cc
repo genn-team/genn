cb45379cd529d3091977019707548d9312a647d5
jamie <neworderofjamie@gmail.com>
2017-03-27 12:35:44 +0100

    Added a makefile and entry point for a SpineML builder

---
+++ spineml/main.cc
@@ -0,0 +1,7 @@
+#include <cstdlib>
+
+int main(int argc,     //!< number of arguments; expected to be 2
+         char *argv[])  //!< Arguments; expected to contain the target directory for code generation.
+{
+    return EXIT_SUCCESS;
+}
