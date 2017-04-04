93d859db5613331632e0a6ccc24cc0ea9af3b649
jamie <neworderofjamie@gmail.com>
2017-04-04 18:25:02 +0100

    Updated SynDelay - @jamesturner246 the safety net provided by the new API suggested that there was something odd about the postsynapse model - you were specifying parameters but using the IZHIKEVICH_PS model which doesn't take any. Was I right in thinking you meant to use EXPONENTIAL?

--- userproject/SynDelay_project/SynDelay.cc
+++ userproject/SynDelay_project/SynDelay.cc
@@ -13,6 +13,26 @@
 #include "global.h"
 
 
+class MyIzhikevich : public NeuronModels::Izhikevich
+{
+public:
+    DECLARE_MODEL(MyIzhikevich, 5, 2);
+
+    SET_SIM_CODE(
+        "if ($(V) >= 30.0) {\n"
+        "    $(V)=$(c);\n"
+        "    $(U)+=$(d);\n"
+        "}\n"
+        "$(V) += 0.5*(0.04*$(V)*$(V)+5.0*$(V)+140.0-$(U)+$(I0)+$(Isyn))*DT; //at two times for numerical stability\n"
+        "$(V) += 0.5*(0.04*$(V)*$(V)+5.0*$(V)+140.0-$(U)+$(I0)+$(Isyn))*DT;\n"
+        "$(U) += $(a)*($(b)*$(V)-$(U))*DT;\n"
+        "//if ($(V) > 30.0) { // keep this only for visualisation -- not really necessaary otherwise\n"
+        "//    $(V) = 30.0;\n"
+        "//}\n");
+    SET_PARAM_NAMES({"a", "b", "c", "d", "I0"});
+};
+IMPLEMENT_MODEL(MyIzhikevich);
+
 void modelDefinition(NNmodel &model) 
 {
     initGeNN();
@@ -26,114 +46,100 @@ void modelDefinition(NNmodel &model)
     model.setName("SynDelay");
     model.setDT(1.0);
     model.setPrecision(GENN_FLOAT);
-
-    neuronModel n= nModels[IZHIKEVICH];
-    n.pNames.push_back("I0");
-    n.simCode= "if ($(V) >= 30.0) {\n\
-        $(V)=$(c);\n\
-        $(U)+=$(d);\n\
-    }\n\
-    $(V) += 0.5*(0.04*$(V)*$(V)+5.0*$(V)+140.0-$(U)+$(I0)+$(Isyn))*DT; //at two times for numerical stability\n\
-    $(V) += 0.5*(0.04*$(V)*$(V)+5.0*$(V)+140.0-$(U)+$(I0)+$(Isyn))*DT;\n\
-    $(U) += $(a)*($(b)*$(V)-$(U))*DT;\n\
-   //if ($(V) > 30.0) { // keep this only for visualisation -- not really necessaary otherwise\n\
-   //    $(V) = 30.0;\n\
-   //}\n";
-    unsigned int MYIZHIKEVICH= nModels.size();
-    nModels.push_back(n);
-
     
     // INPUT NEURONS
     //==============
+    MyIzhikevich::ParamValues input_p( // Izhikevich parameters - tonic spiking
+        0.02,  // 0 - a
+        0.2,   // 1 - b
+        -65,   // 2 - c
+        6,      // 3 - d
+        4.0     // 4 - I0 (input current)
+                              );
 
-    double input_p[5] = { // Izhikevich parameters - tonic spiking
-	0.02,  // 0 - a
-	0.2,   // 1 - b
-	-65,   // 2 - c
-	6,      // 3 - d
-	4.0     // 4 - I0 (input current)
-    };
+    MyIzhikevich::VarValues input_ini( // Izhikevich variables - tonic spiking
+        -65,   // 0 - V
+        -20    // 1 - U
+    );
 
-    double input_ini[2] = { // Izhikevich variables - tonic spiking
-	-65,   // 0 - V
-	-20    // 1 - U
-    };
+    PostsynapticModels::ExpCond::ParamValues postExpInp(
+        1.0,            // 0 - tau_S: decay time constant for S [ms]
+        0.0              // 1 - Erev: Reversal potential
+    );
 
-    double postExpInp[2] = {
-	1.0,            // 0 - tau_S: decay time constant for S [ms]
-	0.0		  // 1 - Erev: Reversal potential
-    };
-
-    model.addNeuronPopulation("Input", 500, MYIZHIKEVICH, input_p, input_ini);
+    model.addNeuronPopulation<MyIzhikevich>("Input", 500, input_p, input_ini);
 
 
     // INTERNEURONS
     //=============
+    NeuronModels::Izhikevich::ParamValues inter_p( // Izhikevich parameters - tonic spiking
+        0.02,      // 0 - a
+        0.2,       // 1 - b
+        -65,       // 2 - c
+        6      // 3 - d
+    );
 
-    double inter_p[4] = { // Izhikevich parameters - tonic spiking
-	0.02,	   // 0 - a
-	0.2, 	   // 1 - b
-	-65, 	   // 2 - c
-	6 	   // 3 - d
-    };
+    NeuronModels::Izhikevich::VarValues inter_ini( // Izhikevich variables - tonic spiking
+        -65,       // 0 - V
+        -20        // 1 - U
+    );
 
-    double inter_ini[2] = { // Izhikevich variables - tonic spiking
-	-65,	   // 0 - V
-	-20	   // 1 - U
-    };
+    PostsynapticModels::ExpCond::ParamValues postExpInt(
+        1.0,            // 0 - tau_S: decay time constant for S [ms]
+        0.0               // 1 - Erev: Reversal potential
+    );
 
-    double postExpInt[2] = {
-	1.0,            // 0 - tau_S: decay time constant for S [ms]
-	0.0		  // 1 - Erev: Reversal potential
-    };
-
-    model.addNeuronPopulation("Inter", 500, IZHIKEVICH, inter_p, inter_ini);
+    model.addNeuronPopulation<NeuronModels::Izhikevich>("Inter", 500, inter_p, inter_ini);
 
 
     // OUTPUT NEURONS
     //===============
 
-    double output_p[5] = { // Izhikevich parameters - tonic spiking
-	0.02,	   // 0 - a
-	0.2, 	   // 1 - b
-	-65, 	   // 2 - c
-	6 	   // 3 - d
-    };
+    NeuronModels::Izhikevich::ParamValues output_p( // Izhikevich parameters - tonic spiking
+        0.02,      // 0 - a
+        0.2,       // 1 - b
+        -65,       // 2 - c
+        6          // 3 - d
+    );
 
-    double output_ini[2] = { // Izhikevich variables - tonic spiking
-	-65,	   // 0 - V
-	-20	   // 1 - U
-    };
-    double postExpOut[2] = {
-	1.0,            // 0 - tau_S: decay time constant for S [ms]
-	0.0		  // 1 - Erev: Reversal potential
-    };
+    NeuronModels::Izhikevich::VarValues output_ini( // Izhikevich variables - tonic spiking
+        -65,       // 0 - V
+        -20    // 1 - U
+    );
+    PostsynapticModels::ExpCond::ParamValues postExpOut(
+        1.0,            // 0 - tau_S: decay time constant for S [ms]
+        0.0		  // 1 - Erev: Reversal potential
+    );
 
-    model.addNeuronPopulation("Output", 500, IZHIKEVICH, output_p, output_ini);
+    model.addNeuronPopulation<NeuronModels::Izhikevich>("Output", 500, output_p, output_ini);
 
 
     // INPUT-INTER, INPUT-OUTPUT & INTER-OUTPUT SYNAPSES
     //==================================================
+    WeightUpdateModels::StaticPulse::VarValues inputInter_ini(
+        0.06   // 0 - default synaptic conductance
+    );
 
-    double *synapses_p= NULL;
+    WeightUpdateModels::StaticPulse::VarValues inputOutput_ini(
+        0.03   // 0 - default synaptic conductance
+    );
 
-    double inputInter_ini[1] = {
-	0.06   // 0 - default synaptic conductance
-    };
+    WeightUpdateModels::StaticPulse::VarValues interOutput_ini(
+        0.03   // 0 - default synaptic conductance
+    );
 
-    double inputOutput_ini[1] = {
-	0.03   // 0 - default synaptic conductance
-    };
-
-    double interOutput_ini[1] = {
-	0.03   // 0 - default synaptic conductance
-    };
-
-    double *postSynV = NULL;
-
-    model.addSynapsePopulation("InputInter", NSYNAPSE, DENSE, GLOBALG, 3, IZHIKEVICH_PS, "Input", "Inter", inputInter_ini, synapses_p, postSynV, postExpInp);
-    model.addSynapsePopulation("InputOutput", NSYNAPSE, DENSE, GLOBALG, 6, IZHIKEVICH_PS, "Input", "Output", inputOutput_ini, synapses_p, postSynV, postExpOut);
-    model.addSynapsePopulation("InterOutput", NSYNAPSE, DENSE, GLOBALG, NO_DELAY, IZHIKEVICH_PS, "Inter", "Output", interOutput_ini, synapses_p, postSynV, postExpInt);
+    model.addSynapsePopulation<WeightUpdateModels::StaticPulse, PostsynapticModels::ExpCond>("InputInter", SynapseMatrixType::DENSE_GLOBALG, 3,
+                                                                                             "Input", "Inter",
+                                                                                             {}, inputInter_ini,
+                                                                                             postExpInp, {});
+    model.addSynapsePopulation<WeightUpdateModels::StaticPulse, PostsynapticModels::ExpCond>("InputOutput", SynapseMatrixType::DENSE_GLOBALG, 6,
+                                                                                             "Input", "Output",
+                                                                                             {}, inputOutput_ini,
+                                                                                             postExpOut, {});
+    model.addSynapsePopulation<WeightUpdateModels::StaticPulse, PostsynapticModels::ExpCond>("InterOutput", SynapseMatrixType::DENSE_GLOBALG, NO_DELAY,
+                                                                                             "Inter", "Output",
+                                                                                             {}, interOutput_ini,
+                                                                                             postExpInt, {});
 
 
     model.finalize();
