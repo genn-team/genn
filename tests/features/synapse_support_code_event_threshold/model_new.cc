85bdea9e87364d493a73404d1b524c731b27180b
Jamie <neworderofjamie@gmail.com>
2017-04-07 11:35:50 +0100

    Fixed tests for new name of postsynapse type

--- tests/features/synapse_support_code_event_threshold/model_new.cc
+++ tests/features/synapse_support_code_event_threshold/model_new.cc
@@ -49,7 +49,7 @@ void modelDefinition(NNmodel &model)
     for (int i= 0; i < 10; i++)
     {
         string theName= synName + std::to_string(i);
-        model.addSynapsePopulation<WeightUpdateModel, PostsynapticModels::Izhikevich>(
+        model.addSynapsePopulation<WeightUpdateModel, PostsynapticModels::DeltaCurr>(
             theName, SynapseMatrixType::DENSE_INDIVIDUALG, i, "pre", "post",
             WeightUpdateModel::ParamValues((double)2*(i+1)), WeightUpdateModel::VarValues(0.0),
             {}, {});
