87105f9f690dd60a560459c8b6b5f5e31901dfb1
jamie <neworderofjamie@gmail.com>
2017-04-12 10:46:53 +0100

    GCC version test for regex insufficient

--- lib/src/stringUtils.cc
+++ lib/src/stringUtils.cc
@@ -8,7 +8,21 @@
 
 #include <limits>
 
-#if !defined(__GNUC__) || (__GNUC__ >= 4 && __GNUC_MINOR__ >= 9)
+// Is C++ regex library operational?
+// We assume it is for:
+// 1) Non GCC compilers
+// 2) GCC 5.X.X and future
+// 3) Any future (4.10.X?) releases
+// 4) 4.9.1 and subsequent patch releases (GCC fully implemented regex in 4.9.0
+// BUT bug 61227 https://gcc.gnu.org/bugzilla/show_bug.cgi?id=61227 prevented \w from working.
+#if !defined(__GNUC__) || \
+    __GNUC__ > 4 || \
+    (__GNUC__ == 4 && (__GNUC_MINOR__ > 9 || \
+                      (__GNUC_MINOR__ == 9 && __GNUC_PATCHLEVEL__ >= 1)))
+    #define REGEX_OPERATIONAL
+#endif
+
+#ifdef REGEX_OPERATIONAL
 #include <regex>
 #endif
 
@@ -349,9 +363,7 @@ string ensureFtype(const string &oldcode, const string &type)
     return code;
 }
 
-
-#if !defined(__GNUC__) || (__GNUC__ >= 4 && __GNUC_MINOR__ >= 9)
-
+#ifdef REGEX_OPERATIONAL
 //--------------------------------------------------------------------------
 /*! \brief This function checks for unknown variable definitions and returns a gennError if any are found
  */
@@ -374,6 +386,7 @@ void checkUnreplacedVariables(string code, string codeName)
 #else
 void checkUnreplacedVariables(string code, string codeName) 
 {
+#error "WHY NO REGEX"
 }
 #endif
 
