62abb57465f17e9f75829c8d1e70d6dd87ce285d
jamie <neworderofjamie@gmail.com>
2017-04-12 17:39:05 +0100

    Manually merge GCC version test for regex insufficient from master
    # Conflicts:
    #	lib/src/codeGenUtils.cc

--- lib/src/codeGenUtils.cc
+++ lib/src/codeGenUtils.cc
@@ -1,7 +1,23 @@
 #include "codeGenUtils.h"
 
+// Is C++ regex library operational?
+// We assume it is for:
+// 1) Non GCC compilers
+// 2) GCC 5.X.X and future
+// 3) Any future (4.10.X?) releases
+// 4) 4.9.1 and subsequent patch releases (GCC fully implemented regex in 4.9.0
+// BUT bug 61227 https://gcc.gnu.org/bugzilla/show_bug.cgi?id=61227 prevented \w from working)
+#if !defined(__GNUC__) || \
+    __GNUC__ > 4 || \
+    (__GNUC__ == 4 && (__GNUC_MINOR__ > 9 || \
+                      (__GNUC_MINOR__ == 9 && __GNUC_PATCHLEVEL__ >= 1)))
+    #define REGEX_OPERATIONAL
+#endif
+
 // Standard includes
+#ifdef REGEX_OPERATIONAL
 #include <regex>
+#endif
 
 // GeNN includes
 #include "modelSpec.h"
@@ -295,6 +311,9 @@ string ensureFtype(const string &oldcode, const string &type)
     return code;
 }
 
+
+#ifdef REGEX_OPERATIONAL
+
 //--------------------------------------------------------------------------
 /*! \brief This function checks for unknown variable definitions and returns a gennError if any are found
  */
@@ -314,6 +333,12 @@ void checkUnreplacedVariables(const string &code, const string &codeName)
         gennError("The "+vars+"undefined in code "+codeName+".");
     }
 }
+#else
+void checkUnreplacedVariables(const string &, const string &)
+{
+}
+#endif
+
 
 //-------------------------------------------------------------------------
 /*!
