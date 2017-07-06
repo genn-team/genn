4cef3af7e880c7c98921694bd0801c05d87ac093
jamie <neworderofjamie@gmail.com>
2017-07-06 15:28:08 +0100

    Added several more connector tests

--- tests/spineml/simulator/connectors.cc
+++ tests/spineml/simulator/connectors.cc
@@ -20,7 +20,29 @@ using namespace SpineMLSimulator;
 //------------------------------------------------------------------------
 namespace
 {
+SparseProjection sparse5;
+SparseProjection sparse10;
 SparseProjection sparse42;
+
+void allocate5(unsigned int connN){
+    sparse5.connN = connN;
+    sparse5.indInG = new unsigned int[6];
+    sparse5.ind = new unsigned int[connN];
+    sparse5.preInd = NULL;
+    sparse5.revIndInG = NULL;
+    sparse5.revInd = NULL;
+    sparse5.remap = NULL;
+}
+
+void allocate10(unsigned int connN){
+    sparse10.connN = connN;
+    sparse10.indInG = new unsigned int[11];
+    sparse10.ind = new unsigned int[connN];
+    sparse10.preInd = NULL;
+    sparse10.revIndInG = NULL;
+    sparse10.revInd = NULL;
+    sparse10.remap = NULL;
+}
 
 void allocate42(unsigned int connN){
     sparse42.connN = connN;
@@ -30,6 +52,27 @@ void allocate42(unsigned int connN){
     sparse42.revIndInG = NULL;
     sparse42.revInd = NULL;
     sparse42.remap = NULL;
+}
+
+void checkOneToOne(const SparseProjection &projection, unsigned int num)
+{
+    EXPECT_EQ(projection.connN, num);
+    for(unsigned int i = 0; i < num; i++) {
+        EXPECT_EQ(projection.ind[i], i);
+        EXPECT_EQ(projection.indInG[i], i);
+    }
+    EXPECT_EQ(projection.indInG[num], num);
+}
+
+void checkTriangle(const SparseProjection &projection, unsigned int num)
+{
+    for(unsigned int i = 0; i < num; i++) {
+        const unsigned int rowLength = projection.indInG[i + 1] - projection.indInG[i];
+        EXPECT_EQ(rowLength, i);
+        for(unsigned int j = 0; j < rowLength; j++){
+            EXPECT_EQ(projection.ind[projection.indInG[i] + j], j);
+        }
+    }
 }
 }   // Anonymous namespace
 
@@ -200,13 +243,7 @@ TEST(OneToOneConnectionTest, Sparse) {
     Connectors::create(synapse, 42, 42,
                        &sparse42, allocate42, basePath);
 
-    // Check structure
-    EXPECT_EQ(sparse42.connN, 42);
-    for(unsigned int i = 0; i < 42; i++) {
-        EXPECT_EQ(sparse42.ind[i], i);
-        EXPECT_EQ(sparse42.indInG[i], i);
-    }
-    EXPECT_EQ(sparse42.indInG[42], 42);
+    checkOneToOne(sparse42, 42);
 }
 
 
@@ -238,6 +275,141 @@ TEST(ConnectionListTest, DenseDeath) {
     catch(const std::runtime_error &)
     {
     }
+}
+//------------------------------------------------------------------------
+TEST(ConnectionListTest, InlineOneToOneSparse) {
+    // XML fragment specifying connector
+    const char *connectorXML =
+        "<LL:Synapse>\n"
+        "   <ConnectionList>\n"
+        "       <Connection src_neuron=\"0\" trg_neuron=\"0\"/>\n"
+        "       <Connection src_neuron=\"1\" trg_neuron=\"1\"/>\n"
+        "       <Connection src_neuron=\"2\" trg_neuron=\"2\"/>\n"
+        "       <Connection src_neuron=\"3\" trg_neuron=\"3\"/>\n"
+        "       <Connection src_neuron=\"4\" trg_neuron=\"4\"/>\n"
+        "       <Connection src_neuron=\"5\" trg_neuron=\"5\"/>\n"
+        "       <Connection src_neuron=\"6\" trg_neuron=\"6\"/>\n"
+        "       <Connection src_neuron=\"7\" trg_neuron=\"7\"/>\n"
+        "       <Connection src_neuron=\"8\" trg_neuron=\"8\"/>\n"
+        "       <Connection src_neuron=\"9\" trg_neuron=\"9\"/>\n"
+        "   </ConnectionList>\n"
+        "</LL:Synapse>\n";
+
+    // Load XML and get root LL:Synapse element
+    pugi::xml_document connectorDocument;
+    connectorDocument.load_string(connectorXML);
+    auto synapse = connectorDocument.child("LL:Synapse");
+
+    // Parse XML and create sparse connector
+    filesystem::path basePath;
+    Connectors::create(synapse, 10, 10,
+                       &sparse10, allocate10, basePath);
+
+    // Check number of connections matches XML
+    EXPECT_EQ(sparse10.connN, 10);
+    checkOneToOne(sparse10, 10);
+}
+//------------------------------------------------------------------------
+TEST(ConnectionListTest, InlineTriangleSparse) {
+    // XML fragment specifying connector
+    const char *connectorXML =
+        "<LL:Synapse>\n"
+        "   <ConnectionList>\n"
+        "       <Connection src_neuron=\"1\" trg_neuron=\"0\"/>\n"
+
+        "       <Connection src_neuron=\"2\" trg_neuron=\"0\"/>\n"
+        "       <Connection src_neuron=\"2\" trg_neuron=\"1\"/>\n"
+
+        "       <Connection src_neuron=\"3\" trg_neuron=\"0\"/>\n"
+        "       <Connection src_neuron=\"3\" trg_neuron=\"1\"/>\n"
+        "       <Connection src_neuron=\"3\" trg_neuron=\"2\"/>\n"
+
+        "       <Connection src_neuron=\"4\" trg_neuron=\"0\"/>\n"
+        "       <Connection src_neuron=\"4\" trg_neuron=\"1\"/>\n"
+        "       <Connection src_neuron=\"4\" trg_neuron=\"2\"/>\n"
+        "       <Connection src_neuron=\"4\" trg_neuron=\"3\"/>\n"
+        "   </ConnectionList>\n"
+        "</LL:Synapse>\n";
+
+    // Load XML and get root LL:Synapse element
+    pugi::xml_document connectorDocument;
+    connectorDocument.load_string(connectorXML);
+    auto synapse = connectorDocument.child("LL:Synapse");
+
+    // Parse XML and create sparse connector
+    filesystem::path basePath;
+    Connectors::create(synapse, 5, 5,
+                       &sparse5, allocate5, basePath);
+
+    // Check number of connections matches XML
+    EXPECT_EQ(sparse5.connN, 10);
+    checkTriangle(sparse5, 5);
+}
+//------------------------------------------------------------------------
+TEST(ConnectionListTest, InlineShuffleTriangleSparse) {
+    // XML fragment specifying connector
+    const char *connectorXML =
+        "<LL:Synapse>\n"
+        "   <ConnectionList>\n"
+        "       <Connection src_neuron=\"4\" trg_neuron=\"3\"/>\n"
+        "       <Connection src_neuron=\"1\" trg_neuron=\"0\"/>\n"
+        "       <Connection src_neuron=\"3\" trg_neuron=\"0\"/>\n"
+        "       <Connection src_neuron=\"4\" trg_neuron=\"2\"/>\n"
+        "       <Connection src_neuron=\"2\" trg_neuron=\"0\"/>\n"
+        "       <Connection src_neuron=\"4\" trg_neuron=\"0\"/>\n"
+        "       <Connection src_neuron=\"3\" trg_neuron=\"1\"/>\n"
+        "       <Connection src_neuron=\"4\" trg_neuron=\"1\"/>\n"
+        "       <Connection src_neuron=\"2\" trg_neuron=\"1\"/>\n"
+        "       <Connection src_neuron=\"3\" trg_neuron=\"2\"/>\n"
+        "   </ConnectionList>\n"
+        "</LL:Synapse>\n";
+
+    // Load XML and get root LL:Synapse element
+    pugi::xml_document connectorDocument;
+    connectorDocument.load_string(connectorXML);
+    auto synapse = connectorDocument.child("LL:Synapse");
+
+    // Parse XML and create sparse connector
+    filesystem::path basePath;
+    Connectors::create(synapse, 5, 5,
+                       &sparse5, allocate5, basePath);
+
+    // Check number of connections matches XML
+    EXPECT_EQ(sparse5.connN, 10);
+    checkTriangle(sparse5, 5);
+}
+//------------------------------------------------------------------------
+TEST(ConnectionListTest, InlineShuffleOneToOneSparse) {
+    // XML fragment specifying connector
+    const char *connectorXML =
+        "<LL:Synapse>\n"
+        "   <ConnectionList>\n"
+        "       <Connection src_neuron=\"6\" trg_neuron=\"6\"/>\n"
+        "       <Connection src_neuron=\"1\" trg_neuron=\"1\"/>\n"
+        "       <Connection src_neuron=\"7\" trg_neuron=\"7\"/>\n"
+        "       <Connection src_neuron=\"3\" trg_neuron=\"3\"/>\n"
+        "       <Connection src_neuron=\"4\" trg_neuron=\"4\"/>\n"
+        "       <Connection src_neuron=\"9\" trg_neuron=\"9\"/>\n"
+        "       <Connection src_neuron=\"8\" trg_neuron=\"8\"/>\n"
+        "       <Connection src_neuron=\"0\" trg_neuron=\"0\"/>\n"
+        "       <Connection src_neuron=\"5\" trg_neuron=\"5\"/>\n"
+        "       <Connection src_neuron=\"2\" trg_neuron=\"2\"/>\n"
+        "   </ConnectionList>\n"
+        "</LL:Synapse>\n";
+
+    // Load XML and get root LL:Synapse element
+    pugi::xml_document connectorDocument;
+    connectorDocument.load_string(connectorXML);
+    auto synapse = connectorDocument.child("LL:Synapse");
+
+    // Parse XML and create sparse connector
+    filesystem::path basePath;
+    Connectors::create(synapse, 10, 10,
+                       &sparse10, allocate10, basePath);
+
+    // Check number of connections matches XML
+    EXPECT_EQ(sparse10.connN, 10);
+    checkOneToOne(sparse10, 10);
 }
 //------------------------------------------------------------------------
 TEST(ConnectionListTest, BinaryFileSparse) {
