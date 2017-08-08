53987fbac8e3ece9367bf9cd80a34c30ed8759ca
jamie <neworderofjamie@gmail.com>
2017-08-08 10:10:31 +0100

    W.I.P. test for first order, linear ODE

--- spineml/generator/objectHandler.cc
+++ spineml/generator/objectHandler.cc
@@ -1,5 +1,9 @@
 #include "objectHandler.h"
 
+// Standard C++ includes
+#include <iostream>
+#include <regex>
+
 // pugixml includes
 #include "pugixml/pugixml.hpp"
 
@@ -51,6 +55,15 @@ void SpineMLGenerator::ObjectHandler::TimeDerivative::onObject(const pugi::xml_n
     std::string codeString = node.child_value("MathInline");
     expandAliases(codeString, m_Aliases);
 
+    // Find name of state variable
+    const std::string stateVariable = node.attribute("variable").value();
+
+    std::regex regex("\\s*-\\s*" + stateVariable + "\\s*\\/\\s*([a-zA-Z_]+)\\s*");
+    std::cmatch match;
+    if(std::regex_match(codeString.c_str(), match, regex)) {
+        std::cout << "POTENTIAL FIRST-ORDER LINEAR ODE TAU:" << match[1].str() << std::endl;
+    }
+
     // **TODO** identify cases where Euler is REALLY stupid
-    m_CodeStream << node.attribute("variable").value() << " += DT * (" << codeString << ");" << std::endl;
+    m_CodeStream << stateVariable << " += DT * (" << codeString << ");" << std::endl;
 }
