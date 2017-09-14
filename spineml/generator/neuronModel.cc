8fcc117a0f03702f791aaf395f9b1f75978c4d43
jamie <neworderofjamie@gmail.com>
2017-09-14 10:59:40 +0100

    Neuron models can also handle incoming events * These are received as standard additional input vars, interpreted as an event count * These can cause a spike to be emitted in which case this event count is used as a trigger condition * Alternatively they can cause a state assignment of the form var += X which gets converted to var += COUNT * X

--- spineml/generator/neuronModel.cc
+++ spineml/generator/neuronModel.cc
@@ -2,6 +2,7 @@
 
 // Standard C++ includes
 #include <iostream>
+#include <regex>
 #include <sstream>
 
 // Standard C includes
@@ -25,42 +26,22 @@ using namespace SpineMLCommon;
 namespace
 {
 //----------------------------------------------------------------------------
-// ObjectHandlerNeuronCondition
+// RegimeThresholds
 //----------------------------------------------------------------------------
-class ObjectHandlerNeuronCondition : public SpineMLGenerator::ObjectHandler::Condition
+class RegimeThresholds
 {
 public:
-    ObjectHandlerNeuronCondition(SpineMLGenerator::CodeStream &codeStream, const std::map<std::string, std::string> &aliases, const std::string &sendPortSpike)
-        : SpineMLGenerator::ObjectHandler::Condition(codeStream, aliases), m_SendPortSpike(sendPortSpike){}
-
-    //----------------------------------------------------------------------------
-    // SpineMLGenerator::ObjectHandler::Condition virtuals
-    //----------------------------------------------------------------------------
-    void onObject(const pugi::xml_node &node, unsigned int currentRegimeID,
-                  unsigned int targetRegimeID)
-    {
-        // Superclass
-        SpineMLGenerator::ObjectHandler::Condition::onObject(node, currentRegimeID,
-                                                             targetRegimeID);
-        // If this condition emits a spike
-        // **TODO** also handle spike like event clause
-        pugi::xpath_variable_set spikeEventsOutVars;
-        spikeEventsOutVars.set("portName", m_SendPortSpike.c_str());
-        auto spikeEventOut = node.select_node("EventOut[@port=$portName]", &spikeEventsOutVars);
-        if(spikeEventOut) {
-            // Add current regime and trigger condition to map
-            // **NOTE** cannot build code immediately as we don't know if there are multiple regimes
-            std::string triggerCodeString = node.child("Trigger").child("MathInline").text().get();
-            SpineMLGenerator::expandAliases(triggerCodeString, getAliases());
-            if(!m_RegimeThresholds.insert(std::make_pair(currentRegimeID, triggerCodeString)).second) {
-                throw std::runtime_error("Only one spike trigger is supported per regime");
-            }
-        }
-    }
-
     //----------------------------------------------------------------------------
     // Public API
     //----------------------------------------------------------------------------
+    void addTriggerCode(unsigned int regimeID, const std::string &triggerCodeString) {
+        // If there isn't already a piece of trigger code for this regime, add one
+        // **THINK** is this a fair restriction - if not should they be ORed or ANDed together?
+        if(!m_RegimeThresholds.insert(std::make_pair(regimeID, triggerCodeString)).second) {
+            throw std::runtime_error("Only one spike trigger is supported per regime");
+        }
+    }
+
     std::string getThresholdCode(bool multipleRegimes) const
     {
         // If there are multiple regimes
@@ -72,7 +53,7 @@ public:
                 if(thresholdCodeStream.tellp() > 0) {
                     thresholdCodeStream << " || ";
                 }
-                
+
                 // Add test, ANDing test for correct regime ID with threshold condition
                 thresholdCodeStream << "(_regimeID == " << r.first << " && (" << r.second << "))";
             }
@@ -95,9 +76,121 @@ private:
     // Members
     //----------------------------------------------------------------------------
     std::map<unsigned int, std::string> m_RegimeThresholds;
-    std::string m_SendPortSpike;
 };
-}
+
+//----------------------------------------------------------------------------
+// ObjectHandlerCondition
+//----------------------------------------------------------------------------
+class ObjectHandlerCondition : public SpineMLGenerator::ObjectHandler::Condition
+{
+public:
+    ObjectHandlerCondition(SpineMLGenerator::CodeStream &codeStream, const std::map<std::string, std::string> &aliases, const std::string &sendPortSpike, RegimeThresholds &regimeThresholds)
+        : SpineMLGenerator::ObjectHandler::Condition(codeStream, aliases), m_SendPortSpike(sendPortSpike), m_RegimeThresholds(regimeThresholds){}
+
+    //----------------------------------------------------------------------------
+    // SpineMLGenerator::ObjectHandler::Condition virtuals
+    //----------------------------------------------------------------------------
+    void onObject(const pugi::xml_node &node, unsigned int currentRegimeID,
+                  unsigned int targetRegimeID)
+    {
+        // Superclass
+        SpineMLGenerator::ObjectHandler::Condition::onObject(node, currentRegimeID,
+                                                             targetRegimeID);
+        // If this condition emits a spike
+        // **TODO** also handle spike like event clause
+        pugi::xpath_variable_set spikeEventsOutVars;
+        spikeEventsOutVars.set("portName", m_SendPortSpike.c_str());
+        auto spikeEventOut = node.select_node("EventOut[@port=$portName]", &spikeEventsOutVars);
+        if(spikeEventOut) {
+            // Add current regime and trigger condition to map
+            // **NOTE** cannot build code immediately as we don't know if there are multiple regimes
+            std::string triggerCodeString = node.child("Trigger").child("MathInline").text().get();
+            SpineMLGenerator::expandAliases(triggerCodeString, getAliases());
+            m_RegimeThresholds.addTriggerCode(currentRegimeID, triggerCodeString);
+        }
+    }
+
+private:
+    //----------------------------------------------------------------------------
+    // Members
+    //----------------------------------------------------------------------------
+    std::string m_SendPortSpike;
+    RegimeThresholds &m_RegimeThresholds;
+};
+
+//----------------------------------------------------------------------------
+// ObjectHandlerEvent
+//----------------------------------------------------------------------------
+class ObjectHandlerEvent : public SpineMLGenerator::ObjectHandler::Base
+{
+public:
+    ObjectHandlerEvent(SpineMLGenerator::CodeStream &codeStream, RegimeThresholds &regimeThresholds)
+        : m_CodeStream(codeStream), m_RegimeThresholds(regimeThresholds){}
+
+    //------------------------------------------------------------------------
+    // ObjectHandler::Base virtuals
+    //------------------------------------------------------------------------
+    virtual void onObject(const pugi::xml_node &node, unsigned int currentRegimeID,
+                          unsigned int targetRegimeID)
+    {
+        // If event is handled with a state assignement
+        const std::string srcPort = node.attribute("src_port").value();
+        if(auto stateAssigment = node.child("StateAssignment")) {
+            // Extract variable name
+            const std::string variableName = stateAssigment.attribute("variable").value();
+
+            // Match for A + B type expression with any amount of whitespace
+            // **TODO** generalise this - anything linear on variable would be fine
+            auto stateAssigmentCode = stateAssigment.child_value("MathInline");
+            std::regex regex("\\s*([a-zA-Z_]+)\\s*\\+\\s*([a-zA-Z_]+)\\s*");
+            std::cmatch match;
+            if(std::regex_match(stateAssigmentCode, match, regex)) {
+                // If match is successful, write state assignement code to add scaled source port value to variable name
+                if(match[1].str() == variableName) {
+                    writeStateAssignement(variableName, match[2].str(), srcPort);
+                }
+                else if(match[2].str() == variableName) {
+                    writeStateAssignement(variableName, match[1].str(), srcPort);
+                }
+                else {
+                    throw std::runtime_error("GeNN only supports neuron models which multiply event count by variable");
+                }
+            }
+            else {
+                throw std::runtime_error("GeNN only supports neuron models which perform linear computation on incoming events");
+            }
+        }
+        // Otherwise if this event should simply trigger an output event, add a trigger
+        // to the regime's threshold condition to test if any events have been reveived
+        else if(auto eventOut = node.child("EventOut")) {
+            m_RegimeThresholds.addTriggerCode(currentRegimeID, srcPort + " > 0");
+        }
+        else {
+             throw std::runtime_error("GeNN only supports neuron models which either emit events or perform state assignement on incomign events");
+        }
+
+        // If this event results in a regime change
+        if(currentRegimeID != targetRegimeID) {
+            throw std::runtime_error("GeNN does not support neuron models which change state based on incoming events");
+        }
+    }
+
+private:
+    //------------------------------------------------------------------------
+    // Private methods
+    //------------------------------------------------------------------------
+    void writeStateAssignement(const std::string &variableName, const std::string &multiplier, const std::string &srcPort)
+    {
+        m_CodeStream << variableName << " += " << multiplier << " * " << srcPort << ";" << std::endl;
+    }
+
+    //------------------------------------------------------------------------
+    // Members
+    //------------------------------------------------------------------------
+    SpineMLGenerator::CodeStream &m_CodeStream;
+    RegimeThresholds &m_RegimeThresholds;
+};
+}   // Anonymous namespace
 
 //----------------------------------------------------------------------------
 // SpineMLGenerator::NeuronModel
@@ -147,6 +240,7 @@ SpineMLGenerator::NeuronModel::NeuronModel(const ModelParams::Neuron &params, co
 
     // Check that there are no unhandled receive ports
     std::cout << "\t\tReceive ports:" << std::endl;
+    std::string trueSpikeReceivePort;
     for(auto receivePort : componentClass.select_nodes(SpineMLUtils::xPathNodeHasSuffix("ReceivePort").c_str())) {
         std::string nodeType = receivePort.node().name();
         const char *portName = receivePort.node().attribute("name").value();
@@ -154,6 +248,11 @@ SpineMLGenerator::NeuronModel::NeuronModel(const ModelParams::Neuron &params, co
         if(nodeType == "AnalogReceivePort") {
             std::cout << "\t\t\tImplementing analogue receive port '" << portName << "' as GeNN additional input variable" << std::endl;
             m_AdditionalInputVars.push_back(std::make_pair(portName, std::make_pair("scalar", 0.0)));
+        }
+        else if(nodeType == "EventReceivePort") {
+            std::cout << "\t\t\tImplementing event receive port '" << portName << "' as a GeNN additional input variable" << std::endl;
+            m_AdditionalInputVars.push_back(std::make_pair(portName, std::make_pair("scalar", 0.0)));
+            trueSpikeReceivePort = portName;
         }
         else {
             throw std::runtime_error("GeNN does not support '" + nodeType + "' receive ports in neuron models");
@@ -188,10 +287,15 @@ SpineMLGenerator::NeuronModel::NeuronModel(const ModelParams::Neuron &params, co
         };
 
     // Generate model code using specified condition handler
-    ObjectHandlerNeuronCondition objectHandlerCondition(simCodeStream, aliases, m_SendPortSpike);
+    RegimeThresholds regimeThresholds;
+    ObjectHandlerCondition objectHandlerCondition(simCodeStream, aliases, m_SendPortSpike, regimeThresholds);
+    ObjectHandlerEvent objectHandlerTrueSpike(simCodeStream, regimeThresholds);
     ObjectHandler::TimeDerivative objectHandlerTimeDerivative(simCodeStream, aliases);
-    const bool multipleRegimes = generateModelCode(componentClass, {}, &objectHandlerCondition,
-                                                   {}, &objectHandlerTimeDerivative,
+    const bool multipleRegimes = generateModelCode(componentClass,
+                                                   {
+                                                       {trueSpikeReceivePort, &objectHandlerTrueSpike},
+                                                   },
+                                                   &objectHandlerCondition, {}, &objectHandlerTimeDerivative,
                                                    regimeEndFunc);
 
     // Loop through send ports which send an alias
@@ -206,7 +310,7 @@ SpineMLGenerator::NeuronModel::NeuronModel(const ModelParams::Neuron &params, co
 
     // Store generated code in class
     m_SimCode = simCodeStream.str();
-    m_ThresholdConditionCode = objectHandlerCondition.getThresholdCode(multipleRegimes);
+    m_ThresholdConditionCode = regimeThresholds.getThresholdCode(multipleRegimes);
 
     // Build the final vectors of parameter names and variables from model
     tie(m_ParamNames, m_Vars) = findModelVariables(componentClass, variableParams, multipleRegimes);
