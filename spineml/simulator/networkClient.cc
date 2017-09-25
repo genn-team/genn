0787b3b28ff0807633fae48b01c07e18f7c5f5dc
jamie <neworderofjamie@gmail.com>
2017-09-25 09:56:37 +0100

    Fixes to networkClient to get it compiling * Added to makefiles * Various typos * Template specializations need to not be declared within class definition

--- spineml/simulator/networkClient.cc
+++ spineml/simulator/networkClient.cc
@@ -1,21 +1,29 @@
 #include "networkClient.h"
 
 // Standard C++ includes
-#include <iostreams>
-
-// POSIX includes
-#ifdef _WIN32
-    #include <winsock2.h>
-#else
-    #include <arpa/inet.h>
-    #include <netinet/in.h>
-    #include <sys/socket.h>
-    #include <sys/types.h>
-    #include <unistd.h>
-#endif
+#include <stdexcept>
 
 //----------------------------------------------------------------------------
 // SpineMLSimulator::NetworkClient
+//----------------------------------------------------------------------------
+SpineMLSimulator::NetworkClient::NetworkClient() : m_Socket(-1)
+{
+}
+//----------------------------------------------------------------------------
+SpineMLSimulator::NetworkClient::NetworkClient(const std::string &hostname, int port, int size, DataType dataType, Mode mode, const std::string &connectionName)
+{
+    if(!connect(hostname, port, size, dataType, mode, connectionName)) {
+        throw std::runtime_error("Cannot connect network client");
+    }
+}
+//----------------------------------------------------------------------------
+SpineMLSimulator::NetworkClient::~NetworkClient()
+{
+    // Close socket
+    if(m_Socket >= 0) {
+        close(m_Socket);
+    }
+}
 //----------------------------------------------------------------------------
 bool SpineMLSimulator::NetworkClient::connect(const std::string &hostname, int port, int size, DataType dataType, Mode mode, const std::string &connectionName)
 {
@@ -29,7 +37,7 @@ bool SpineMLSimulator::NetworkClient::connect(const std::string &hostname, int p
     // Disable Nagle algorithm
     // **THINK** should we?
     const int disableNagle = 1;
-    if(setsockopt(sockfd, IPPROTO_TCP, TCP_NODELAY, reinterpret_cast<const char *>(&flag), sizeof(int)) < 0) {
+    if(setsockopt(m_Socket, IPPROTO_TCP, TCP_NODELAY, reinterpret_cast<const char*>(&disableNagle), sizeof(int)) < 0) {
         std::cerr << "Unable to set socket options" << std::endl;
         return false;
     }
@@ -38,10 +46,12 @@ bool SpineMLSimulator::NetworkClient::connect(const std::string &hostname, int p
     sockaddr_in destAddress = {
         .sin_family = AF_INET, 
         .sin_port = htons(port), 
-        .sin_addr = { .s_addr = inet_addr(hostname.c_str()) }};
+        .sin_addr = { .s_addr = inet_addr(hostname.c_str()) },
+        .sin_zero = {0},
+    };
    
    // Connect socket
-   if (connect(m_Socket, reinterpret_cast<sockaddr*>(&destAddress), sizeof(destAddress)) < 0) {
+   if (::connect(m_Socket, reinterpret_cast<sockaddr*>(&destAddress), sizeof(destAddress)) < 0) {
         std::cerr << "Unable to connect to " << hostname << ":" << port << std::endl;
         return false;
     }
@@ -53,7 +63,7 @@ bool SpineMLSimulator::NetworkClient::connect(const std::string &hostname, int p
     }
     // Check response is a hello
     if(handshakeResponse != Response::Hello) {
-        std::cerr << "Invalid handshake response:" << handshakeResponse << std::endl;
+        std::cerr << "Invalid handshake response:" << static_cast<int>(handshakeResponse) << std::endl;
         return false;
     }
     
@@ -89,4 +99,7 @@ bool SpineMLSimulator::NetworkClient::connect(const std::string &hostname, int p
         std::cerr << "Remote host aborted" << std::endl;
         return false;
     }
+
+    // Success!
+    return true;
 }
--- spineml/simulator/networkClient.h
+++ spineml/simulator/networkClient.h
@@ -1,9 +1,20 @@
 #pragma once
 
 // Standard C++ includes
+#include <iostream>
 #include <string>
 
-
+// POSIX includes
+#ifdef _WIN32
+    #include <winsock2.h>
+#else
+    #include <arpa/inet.h>
+    #include <netinet/in.h>
+    #include <netinet/tcp.h>
+    #include <sys/socket.h>
+    #include <sys/types.h>
+    #include <unistd.h>
+#endif
 
 //----------------------------------------------------------------------------
 // SpineMLSimulator::NetworkClient
@@ -16,13 +27,13 @@ public:
     //------------------------------------------------------------------------
     // Enumerations
     //------------------------------------------------------------------------
-    class enum Mode : char
+    enum class Mode : char
     {
         Source =  45,
         Target = 46,
     };
     
-    class enum DataType : char
+    enum class DataType : char
     {
         Analogue = 31,
         Events = 32,
@@ -42,7 +53,7 @@ private:
     //------------------------------------------------------------------------
     // Enumerations
     //------------------------------------------------------------------------
-    class enum Response : char
+    enum class Response : char
     {
         Hello = 41,
         Receive = 42,
@@ -54,45 +65,54 @@ private:
     // Private API
     //----------------------------------------------------------------------------
     template<typename Type>
-    bool sendRequestReadResponse(Type data, &Response response)
+    bool sendRequestReadResponse(Type data, Response &response)
     {
         // Send request
         if(send(m_Socket, &data, sizeof(Type), MSG_DONTWAIT) < 0) {
             std::cerr << "Unable to send request" << std::endl;
             return false;
         }
-        
+
         // Receive handshake response
         if (recv(m_Socket, &response, sizeof(Response), MSG_WAITALL) < 1) {
             std::cerr << "Unable to receive response" << std::endl;
             return false;
         }
-        
+
         return true;
     }
-    
-    template<> 
-    bool sendRequestReadResponse<const std::string &>(const std::string &data, &Response response)
-    {
-        // Send string length
-        const int stringLength = data.size();
-        if(send(m_Socket, &stringLength, sizeof(int), MSG_DONTWAIT) < 0) {
-            std::cerr << "Unable to send size" << std::endl;
-            return false;
-        }
-        
-        // Send string
-        if(send(m_Socket, &data.c_str(), stringLength, MSG_DONTWAIT) < 0) {
-            std::cerr << "Unable to send string" << std::endl;
-            return false;
-        }
-        
-        return true;
-    } 
-    
+
     //----------------------------------------------------------------------------
     // Private members
     //----------------------------------------------------------------------------
-    int m_Socket
+    int m_Socket;
 };
+
+//----------------------------------------------------------------------------
+// Template specializations
+//----------------------------------------------------------------------------
+template<>
+bool NetworkClient::sendRequestReadResponse<const std::string &>(const std::string &data, Response &response)
+{
+    // Send string length
+    const int stringLength = data.size();
+    if(send(m_Socket, &stringLength, sizeof(int), MSG_DONTWAIT) < 0) {
+        std::cerr << "Unable to send size" << std::endl;
+        return false;
+    }
+
+    // Send string
+    if(send(m_Socket, data.c_str(), stringLength, MSG_DONTWAIT) < 0) {
+        std::cerr << "Unable to send string" << std::endl;
+        return false;
+    }
+
+    // Receive handshake response
+    if (recv(m_Socket, &response, sizeof(Response), MSG_WAITALL) < 1) {
+        std::cerr << "Unable to receive response" << std::endl;
+        return false;
+    }
+
+    return true;
+}
 }   // namespace SpineMLSimulator
