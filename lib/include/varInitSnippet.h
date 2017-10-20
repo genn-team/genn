3545420ca6b120c3e203dc67a66f069c1db8dad3
jamie <neworderofjamie@gmail.com>
2017-10-20 17:50:32 +0100

    Uniformly distributed parameter value

--- lib/include/varInitSnippet.h
+++ lib/include/varInitSnippet.h
@@ -35,4 +35,20 @@ public:
 
     SET_PARAM_NAMES({"value"});
 };
+
+//----------------------------------------------------------------------------
+// VarInitSnippet::Uniform
+//----------------------------------------------------------------------------
+class Uniform : public Base
+{
+public:
+    DECLARE_SNIPPET(VarInitSnippet::Uniform, 2);
+
+    SET_CODE(
+        "const scalar scale = $(max) - $(min);\n"
+        "const scalar value = $(min) + ($(gennrand_uniform) * scale);\n"
+        "$(set_value, value);");
+
+    SET_PARAM_NAMES({"min", "max"});
+};
 }   // namespace VarInitSnippet
--- lib/src/varInitSnippet.cc
+++ lib/src/varInitSnippet.cc
@@ -2,3 +2,4 @@
 
 // Implement value initialization snippets
 IMPLEMENT_SNIPPET(VarInitSnippet::Constant);
+IMPLEMENT_SNIPPET(VarInitSnippet::Uniform);
