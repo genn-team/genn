0d5ee811cc3ec7f41a955e382bbc4ffb7e4e2250
jamie <neworderofjamie@gmail.com>
2017-10-31 16:45:00 +0000

    Feature test for variable initialisation

---
+++ tests/features/var_init/model_new.cc
@@ -0,0 +1,95 @@
+#include "modelSpec.h"
+
+//----------------------------------------------------------------------------
+// Neuron
+//----------------------------------------------------------------------------
+class Neuron : public NeuronModels::Base
+{
+public:
+    DECLARE_MODEL(Neuron, 0, 3);
+
+    SET_VARS({{"constant", "scalar"}, {"uniform", "scalar"}, {"normal", "scalar"}});
+};
+IMPLEMENT_MODEL(Neuron);
+
+//----------------------------------------------------------------------------
+// PostsynapticModel
+//----------------------------------------------------------------------------
+class PostsynapticModel : public PostsynapticModels::Base
+{
+public:
+    DECLARE_MODEL(PostsynapticModel, 0, 3);
+
+    SET_VARS({{"pconstant", "scalar"}, {"puniform", "scalar"}, {"pnormal", "scalar"}});
+};
+IMPLEMENT_MODEL(PostsynapticModel);
+
+//----------------------------------------------------------------------------
+// WeightUpdateModel
+//----------------------------------------------------------------------------
+class WeightUpdateModel : public WeightUpdateModels::Base
+{
+public:
+    DECLARE_MODEL(WeightUpdateModel, 0, 3);
+
+    SET_VARS({{"constant", "scalar"}, {"uniform", "scalar"}, {"normal", "scalar"}});
+};
+IMPLEMENT_MODEL(WeightUpdateModel);
+
+void modelDefinition(NNmodel &model)
+{
+    initGeNN();
+
+    model.setSeed(1234);
+    model.setDT(0.1);
+    model.setName("var_init_new");
+
+    GENN_PREFERENCES::autoInitSparseVars = true;
+
+    // Parameters for configuring uniform and normal distributions
+    VarInitSnippet::Uniform::ParamValues uniformParams(
+        0.0,        // 0 - min
+        1.0);       // 1 - max
+
+    VarInitSnippet::Uniform::ParamValues normalParams(
+        0.0,        // 0 - mean
+        1.0);       // 1 - sd
+
+    // Neuron parameters
+    Neuron::VarValues neuronInit(
+        13.0,
+        initVar<VarInitSnippet::Uniform>(uniformParams),
+        initVar<VarInitSnippet::Normal>(normalParams));
+
+    // PostsynapticModel parameters
+    PostsynapticModel::VarValues postsynapticInit(
+        13.0,
+        initVar<VarInitSnippet::Uniform>(uniformParams),
+        initVar<VarInitSnippet::Normal>(normalParams));
+
+    // WeightUpdateModel parameters
+    WeightUpdateModel::VarValues weightUpdateInit(
+        13.0,
+        initVar<VarInitSnippet::Uniform>(uniformParams),
+        initVar<VarInitSnippet::Normal>(normalParams));
+
+    // Neuron populations
+    model.addNeuronPopulation<NeuronModels::SpikeSource>("SpikeSource", 1, {}, {});
+    model.addNeuronPopulation<Neuron>("Pop", 10000, {}, neuronInit);
+
+    // Synapse populations
+    model.addSynapsePopulation<WeightUpdateModel, PostsynapticModel>(
+        "Dense", SynapseMatrixType::DENSE_INDIVIDUALG, NO_DELAY,
+        "SpikeSource", "Pop",
+        {}, weightUpdateInit,
+        {}, postsynapticInit);
+
+    // Synapse populations
+    model.addSynapsePopulation<WeightUpdateModel, PostsynapticModel>(
+        "Sparse", SynapseMatrixType::SPARSE_INDIVIDUALG, NO_DELAY,
+        "SpikeSource", "Pop",
+        {}, weightUpdateInit,
+        {}, postsynapticInit);
+    model.setPrecision(GENN_FLOAT);
+    model.finalize();
+}
