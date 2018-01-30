30db42a00729a3cb0e140f205c6855aab4b6c8b5
jamie <neworderofjamie@gmail.com>
2018-01-30 10:53:17 +0000

    Deleted nmake and updated generate run for PoissonIzh

--- userproject/PoissonIzh_project/generate_run.cc
+++ userproject/PoissonIzh_project/generate_run.cc
@@ -93,13 +93,23 @@ CPU_ONLY=0 or CPU_ONLY=1 (default 0): Whether to compile in (CUDA independent) \
   if (dbgMode) cmd += " -d";
   if (cpu_only) cmd += " -c";
 #ifdef _WIN32
-  cmd += " && nmake /nologo /f WINmakefile clean all ";
+  cmd += " && msbuild PoissonIzh.vcxproj /p:Configuration=";
+  if (dbgMode) {
+	  cmd += "Debug";
+  }
+  else {
+	  cmd += "Release";
+  }
+  if (cpu_only) {
+	  cmd += "_CPU_ONLY";
+  }
 #else // UNIX
-  cmd += " && make clean all ";
-#endif
-  cmd += "SIM_CODE=" + modelName + "_CODE";
+  cmd += " && make clean all SIM_CODE=" + modelName + "_CODE";
   if (dbgMode) cmd += " DEBUG=1";
   if (cpu_only) cmd += " CPU_ONLY=1";
+#endif
+  
+  
   cout << cmd << endl;
   retval=system(cmd.c_str());
   if (retval != 0){
@@ -136,7 +146,7 @@ CPU_ONLY=0 or CPU_ONLY=1 (default 0): Whether to compile in (CUDA independent) \
   cout << "running test..." << endl;
 #ifdef _WIN32
   if (dbgMode == 1) {
-    cmd = "devenv /debugexe model\\PoissonIzh_sim.exe " + string(argv[6]) + " " + string(which);
+    cmd = "devenv /debugexe model\\PoissonIzh_sim.exe " + string(argv[6]) + " " + to_string(which);
   }
   else {
     cmd = "model\\PoissonIzh_sim.exe " + string(argv[6]) + " " + to_string(which);
