9275a4d2f5dfac6ef6fea0d5375ecaad2e33b728
jamie <neworderofjamie@gmail.com>
2018-01-30 17:13:31 +0000

    Whitespace tidy

--- userproject/MBody1_project/model/MBody1.cc
+++ userproject/MBody1_project/model/MBody1.cc
@@ -26,43 +26,43 @@
 
 
 NeuronModels::Poisson::ParamValues myPOI_p(
-  0.1,        // 0 - firing rate
-  2.5,        // 1 - refratory period
-  20.0,       // 2 - Vspike
-  -60.0       // 3 - Vrest
+    0.1,    // 0 - firing rate
+    2.5,    // 1 - refratory period
+    20.0,   // 2 - Vspike
+    -60.0   // 3 - Vrest
 );
 
 NeuronModels::Poisson::VarValues myPOI_ini(
- -60.0,        // 0 - V
-  0,           // 1 - seed
-  -10.0        // 2 - SpikeTime
+    -60.0,  // 0 - V
+    0,      // 1 - seed
+    -10.0   // 2 - SpikeTime
 );
 
 NeuronModels::TraubMiles::ParamValues stdTM_p(
-  7.15,          // 0 - gNa: Na conductance in 1/(mOhms * cm^2)
-  50.0,          // 1 - ENa: Na equi potential in mV
-  1.43,          // 2 - gK: K conductance in 1/(mOhms * cm^2)
-  -95.0,         // 3 - EK: K equi potential in mV
-  0.02672,       // 4 - gl: leak conductance in 1/(mOhms * cm^2)
-  -63.563,       // 5 - El: leak equi potential in mV
-  0.143          // 6 - Cmem: membr. capacity density in muF/cm^2
+    7.15,       // 0 - gNa: Na conductance in 1/(mOhms * cm^2)
+    50.0,       // 1 - ENa: Na equi potential in mV
+    1.43,       // 2 - gK: K conductance in 1/(mOhms * cm^2)
+    -95.0,      // 3 - EK: K equi potential in mV
+    0.02672,    // 4 - gl: leak conductance in 1/(mOhms * cm^2)
+    -63.563,    // 5 - El: leak equi potential in mV
+    0.143       // 6 - Cmem: membr. capacity density in muF/cm^2
 );
 
 
 NeuronModels::TraubMiles::VarValues stdTM_ini(
-  -60.0,                       // 0 - membrane potential E
-  0.0529324,                   // 1 - prob. for Na channel activation m
-  0.3176767,                   // 2 - prob. for not Na channel blocking h
-  0.5961207                    // 3 - prob. for K channel activation n
+    -60.0,      // 0 - membrane potential E
+    0.0529324,  // 1 - prob. for Na channel activation m
+    0.3176767,  // 2 - prob. for not Na channel blocking h
+    0.5961207   // 3 - prob. for K channel activation n
 );
 
 WeightUpdateModels::StaticPulse::VarValues myPNKC_ini(
-  uninitialisedVar()    // 0 - g: initial synaptic conductance
+    uninitialisedVar()  // 0 - g: initial synaptic conductance
 );
 
 PostsynapticModels::ExpCond::ParamValues postExpPNKC(
-  1.0,            // 0 - tau_S: decay time constant for S [ms]
-  0.0             // 1 - Erev: Reversal potential
+    1.0,    // 0 - tau_S: decay time constant for S [ms]
+    0.0     // 1 - Erev: Reversal potential
 );
 
 WeightUpdateModels::StaticPulse::VarValues myPNLHI_ini(
@@ -70,13 +70,13 @@ WeightUpdateModels::StaticPulse::VarValues myPNLHI_ini(
 );
 
 PostsynapticModels::ExpCond::ParamValues postExpPNLHI(
-  1.0,            // 0 - tau_S: decay time constant for S [ms]
-  0.0		  // 1 - Erev: Reversal potential
+    1.0,    // 0 - tau_S: decay time constant for S [ms]
+    0.0     // 1 - Erev: Reversal potential
 );
 
 WeightUpdateModels::StaticGraded::ParamValues myLHIKC_p(
-  -40.0,          // 0 - Epre: Presynaptic threshold potential
-  50.0            // 1 - Vslope: Activation slope of graded release 
+    -40.0,  // 0 - Epre: Presynaptic threshold potential
+    50.0    // 1 - Vslope: Activation slope of graded release
 );
 
 WeightUpdateModels::StaticGraded::VarValues myLHIKC_ini(
@@ -84,45 +84,45 @@ WeightUpdateModels::StaticGraded::VarValues myLHIKC_ini(
 );
 
 PostsynapticModels::ExpCond::ParamValues postExpLHIKC(
-    1.5, //3.0,            // 0 - tau_S: decay time constant for S [ms]
-  -92.0                    // 1 - Erev: Reversal potential
+    1.5, //3.0, // 0 - tau_S: decay time constant for S [ms]
+    -92.0       // 1 - Erev: Reversal potential
 );
 
 WeightUpdateModels::PiecewiseSTDP::ParamValues myKCDN_p(
-  50.0,               // 0 - TLRN: time scale of learning changes
-  50.0,               // 1 - TCHNG: width of learning window
-  50000.0,            // 2 - TDECAY: time scale of synaptic strength decay
-  100000.0,           // 3 - TPUNISH10: Time window of suppression in response to 1/0
-  200.0,              // 4 - TPUNISH01: Time window of suppression in response to 0/1
-  0.015,              // 5 - GMAX: Maximal conductance achievable
-  0.0075,             // 6 - GMID: Midpoint of sigmoid g filter curve
-  33.33,              // 7 - GSLOPE: slope of sigmoid g filter curve
-  10.0,               // 8 - TAUSHIFT: shift of learning curve
-  0.00006             // 9 - GSYN0: value of syn conductance g decays to
+    50.0,       // 0 - TLRN: time scale of learning changes
+    50.0,       // 1 - TCHNG: width of learning window
+    50000.0,    // 2 - TDECAY: time scale of synaptic strength decay
+    100000.0,   // 3 - TPUNISH10: Time window of suppression in response to 1/0
+    200.0,      // 4 - TPUNISH01: Time window of suppression in response to 0/1
+    0.015,      // 5 - GMAX: Maximal conductance achievable
+    0.0075,     // 6 - GMID: Midpoint of sigmoid g filter curve
+    33.33,      // 7 - GSLOPE: slope of sigmoid g filter curve
+    10.0,       // 8 - TAUSHIFT: shift of learning curve
+    0.00006     // 9 - GSYN0: value of syn conductance g decays to
 );
 
 WeightUpdateModels::PiecewiseSTDP::VarValues myKCDN_ini(
-  uninitialisedVar(),   // 0 - g: synaptic conductance
-  uninitialisedVar()    // 1 - graw: raw synaptic conductance
+    uninitialisedVar(), // 0 - g: synaptic conductance
+    uninitialisedVar()  // 1 - graw: raw synaptic conductance
 );
 
 PostsynapticModels::ExpCond::ParamValues postExpKCDN(
-  5.0,            // 0 - tau_S: decay time constant for S [ms]
-  0.0             // 1 - Erev: Reversal potential
+    5.0,    // 0 - tau_S: decay time constant for S [ms]
+    0.0     // 1 - Erev: Reversal potential
 );
 
 WeightUpdateModels::StaticGraded::ParamValues myDNDN_p(
-  -30.0,        // 0 - Epre: Presynaptic threshold potential 
-  50.0          // 1 - Vslope: Activation slope of graded release 
+    -30.0,  // 0 - Epre: Presynaptic threshold potential
+    50.0    // 1 - Vslope: Activation slope of graded release
 );
 
 WeightUpdateModels::StaticGraded::VarValues myDNDN_ini(
-    5.0/_NLB            // 0 - g: synaptic conductance
+    5.0/_NLB    // 0 - g: synaptic conductance
 );
 
 PostsynapticModels::ExpCond::ParamValues postExpDNDN(
-  2.5,            // 0 - tau_S: decay time constant for S [ms]
-  -92.0           // 1 - Erev: Reversal potential
+    2.5,    // 0 - tau_S: decay time constant for S [ms]
+    -92.0   // 1 - Erev: Reversal potential
 );
 
 //--------------------------------------------------------------------------
@@ -140,9 +140,6 @@ void modelDefinition(NNmodel &model)
     GENN_PREFERENCES::optimizeCode = true;
 #endif // DEBUG
 
-    //GENN_PREFERENCES::autoChooseDevice= 0;
-    //GENN_PREFERENCES::optimiseBlockSize= 0;
-    //GENN_PREFERENCES::neuronBlockSize= 192;
     GENN_PREFERENCES::defaultVarMode = VarMode::LOC_HOST_DEVICE_INIT_DEVICE;
 
     model.setName("MBody1");
