26c12813c356cf922153ec7901ac8bd2d6d810bd
jamie <neworderofjamie@gmail.com>
2018-07-24 14:27:15 +0100

    Tests for continuous synapse models going via connection-generated matrices

---
+++ tests/features/decode_matrix_conn_gen_cont_individualg_ragged/model_new.cc
@@ -0,0 +1,96 @@
+#include "modelSpec.h"
+
+//----------------------------------------------------------------------------
+// Decoder
+//----------------------------------------------------------------------------
+class Decoder : public InitSparseConnectivitySnippet::Base
+{
+public:
+    DECLARE_SNIPPET(Decoder, 0);
+
+    SET_ROW_BUILD_CODE(
+        "$(prevJ)++;\n"
+        "if($(isPostNeuronValid, $(prevJ))) {\n"
+        "   const unsigned int jValue = (1 << $(prevJ));\n"
+        "   if((($(id_pre) + 1) & jValue) != 0)\n"
+        "   {\n"
+        "       $(addSynapse, $(prevJ));\n"
+        "   }\n"
+        "}\n"
+        "else {\n"
+        "   $(endRow);\n"
+        "}\n");
+};
+IMPLEMENT_SNIPPET(Decoder);
+
+//----------------------------------------------------------------------------
+// PreNeuron
+//----------------------------------------------------------------------------
+class PreNeuron : public NeuronModels::Base
+{
+public:
+    DECLARE_MODEL(PreNeuron, 0, 1);
+
+    SET_VARS({{"x", "scalar"}});
+};
+
+IMPLEMENT_MODEL(PreNeuron);
+
+//----------------------------------------------------------------------------
+// PostNeuron
+//----------------------------------------------------------------------------
+class PostNeuron : public NeuronModels::Base
+{
+public:
+    DECLARE_MODEL(PostNeuron, 0, 1);
+
+    SET_SIM_CODE("$(x)= $(Isyn);\n");
+
+    SET_VARS({{"x", "scalar"}});
+};
+
+IMPLEMENT_MODEL(PostNeuron);
+
+//---------------------------------------------------------------------------
+// Continuous
+//---------------------------------------------------------------------------
+class Continuous : public WeightUpdateModels::Base
+{
+public:
+    DECLARE_MODEL(Continuous, 0, 1);
+
+    SET_VARS({{"g", "scalar"}});
+
+    SET_SYNAPSE_DYNAMICS_CODE(
+        "$(addtoinSyn) = $(g) * $(x_pre);\n"
+        "$(updatelinsyn);\n");
+};
+IMPLEMENT_MODEL(Continuous);
+
+
+void modelDefinition(NNmodel &model)
+{
+    initGeNN();
+
+    GENN_PREFERENCES::autoInitSparseVars = true;
+    GENN_PREFERENCES::defaultVarMode = VarMode::LOC_HOST_DEVICE_INIT_DEVICE;
+    GENN_PREFERENCES::defaultSparseConnectivityMode = VarMode::LOC_HOST_DEVICE_INIT_DEVICE;
+
+    model.setDT(1.0);
+    model.setName("decode_matrix_conn_genn_cont_individualg_ragged_new");
+
+    // Continuous synapse parameters
+    Continuous::VarValues staticSynapseInit(1.0);    // 0 - Wij (nA)
+
+    model.addNeuronPopulation<PreNeuron>("Pre", 10, {}, PreNeuron::VarValues(0.0));
+    model.addNeuronPopulation<PostNeuron>("Post", 4, {}, PostNeuron::VarValues(0.0));
+
+    model.addSynapsePopulation<Continuous, PostsynapticModels::DeltaCurr>(
+        "Syn", SynapseMatrixType::RAGGED_INDIVIDUALG, NO_DELAY, "Pre", "Post",
+        {}, staticSynapseInit,
+        {}, {},
+        initConnectivity<Decoder>({}));
+
+    model.setPrecision(GENN_FLOAT);
+    model.finalize();
+}
