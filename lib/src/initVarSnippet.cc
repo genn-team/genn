5a8f1613789ac934f7bb4e1eec4297f62a0375ce
neworderofjamie <neworderofjamie@gmail.com>
2018-11-22 10:03:17 +0000

    InitVarSnippet::Gamma for gamma-initialising variables

--- lib/src/initVarSnippet.cc
+++ lib/src/initVarSnippet.cc
@@ -6,3 +6,4 @@ IMPLEMENT_SNIPPET(InitVarSnippet::Constant);
 IMPLEMENT_SNIPPET(InitVarSnippet::Uniform);
 IMPLEMENT_SNIPPET(InitVarSnippet::Normal);
 IMPLEMENT_SNIPPET(InitVarSnippet::Exponential);
+IMPLEMENT_SNIPPET(InitVarSnippet::Gamma);
