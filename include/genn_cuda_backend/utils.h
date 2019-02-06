37346fa7fac2b89a46f37766bf603f8ea4c5b825
Jamie <neworderofjamie@gmail.com>
2019-02-06 15:15:46 +0000

    Use LOG for CUDA errors

--- include/genn_cuda_backend/utils.h
+++ include/genn_cuda_backend/utils.h
@@ -3,35 +3,32 @@
 // Standard C++ includes
 #include <iostream>
 
+// PLOG includes
+#include <plog/Log.h>
+
 #if CUDA_VERSION >= 6050
-#define CHECK_CU_ERRORS(call)                                        \
-  {                                                                \
-    CUresult error = call;                                        \
-    if (error != CUDA_SUCCESS)                                        \
-      {                                                                \
-        const char *errStr;                                        \
-        cuGetErrorName(error, &errStr);                                \
-        std::cerr << __FILE__ << ": " <<  __LINE__;                        \
-        std::cerr << ": cuda driver error " << error << ": ";        \
-        std::cerr << errStr << std::endl;                                        \
-        exit(EXIT_FAILURE);                                        \
-      }                                                                \
-  }
+#define CHECK_CU_ERRORS(call)                                                                       \
+{                                                                                                   \
+    CUresult error = call;                                                                          \
+    if (error != CUDA_SUCCESS) {                                                                    \
+        const char *errStr;                                                                         \
+        cuGetErrorName(error, &errStr);                                                             \
+        LOGE << __FILE__ << ": " <<  __LINE__ << ": cuda driver error " << error << ": " << errStr; \
+        exit(EXIT_FAILURE);                                                                         \
+    }                                                                                               \
+}
 #else
 #define CHECK_CU_ERRORS(call) call
 #endif
 
-#define CHECK_CUDA_ERRORS(call)                                        \
-  {                                                                \
-    cudaError_t error = call;                                        \
-    if (error != cudaSuccess)                                        \
-      {                                                                \
-        std::cerr << __FILE__ << ": " <<  __LINE__;                        \
-        std::cerr << ": cuda runtime error " << error << ": ";        \
-        std::cerr << cudaGetErrorString(error) << std::endl;                \
-        exit(EXIT_FAILURE);                                        \
-      }                                                                \
-  }
+#define CHECK_CUDA_ERRORS(call)                                                                                         \
+{                                                                                                                       \
+    cudaError_t error = call;                                                                                           \
+    if (error != cudaSuccess) {                                                                                         \
+        LOGE << __FILE__ << ": " <<  __LINE__ << ": cuda runtime error " << error << ": " << cudaGetErrorString(error); \
+        exit(EXIT_FAILURE);                                                                                             \
+    }                                                                                                                   \
+}
 
 //--------------------------------------------------------------------------
 // CodeGenerator::CUDA::Utils
