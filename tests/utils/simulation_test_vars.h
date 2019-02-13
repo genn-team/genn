5942404f1d9046d4e444d533c8704c008a864023
neworderofjamie <J.C.Knight@sussex.ac.uk>
2019-02-13 16:40:51 +0000

    Fixed whitespace in ``SimulationTestVars``

--- tests/utils/simulation_test_vars.h
+++ tests/utils/simulation_test_vars.h
@@ -15,29 +15,29 @@ template<typename NeuronPolicy, typename SynapsePolicy>
 class SimulationTestVars : public SimulationTest
 {
 protected:
-  //--------------------------------------------------------------------------
-  // SimulationTest virtuals
-  //--------------------------------------------------------------------------
-  virtual void Init()
-  {
-    m_NeuronPolicy.Init();
-    m_SynapsePolicy.Init();
-  }
+    //--------------------------------------------------------------------------
+    // SimulationTest virtuals
+    //--------------------------------------------------------------------------
+    virtual void Init()
+    {
+        m_NeuronPolicy.Init();
+        m_SynapsePolicy.Init();
+    }
 
-  // -------------------------------------------------------------------------
-  // Public API
-  // -------------------------------------------------------------------------
-  template<typename UpdateFn>
-  float Simulate(UpdateFn update)
-  {
-      return m_SynapsePolicy.Simulate(update,
-                                     [this](){ StepGeNN(); });
-  }
+    // -------------------------------------------------------------------------
+    // Public API
+    // -------------------------------------------------------------------------
+    template<typename UpdateFn>
+    float Simulate(UpdateFn update)
+    {
+        return m_SynapsePolicy.Simulate(update,
+                                        [this](){ StepGeNN(); });
+    }
 
 private:
-  // -------------------------------------------------------------------------
-  // Members
-  // -------------------------------------------------------------------------
-  NeuronPolicy m_NeuronPolicy;
-  SynapsePolicy m_SynapsePolicy;
+    // -------------------------------------------------------------------------
+    // Members
+    // -------------------------------------------------------------------------
+    NeuronPolicy m_NeuronPolicy;
+    SynapsePolicy m_SynapsePolicy;
 };
