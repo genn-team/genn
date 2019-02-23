1bd52bfc8940a1a0611fefcc787518506dd15d95
Jamie <neworderofjamie@gmail.com>
2019-02-23 20:12:09 +0000

    Logic for caching and generating project GUIDS on windows

--- src/genn_generator/generator.cc
+++ src/genn_generator/generator.cc
@@ -1,6 +1,9 @@
 // Standard C++ includes
 #include <fstream>
 
+#ifdef _WIN32
+#include <Objbase.h>
+#endif
 // PLOG includes
 #include <plog/Log.h>
 #include <plog/Appenders/ConsoleAppender.h>
@@ -79,6 +82,41 @@ int main(int argc,     //!< number of arguments; expected to be 2
     const auto moduleNames = CodeGenerator::generateAll(model, backend, outputPath);
 
 #ifdef _WIN32
+    // If project GUID file doesn't exist
+    const filesystem::path projectGUIDFilename = outputPath / "guid.txt";
+    std::string projectGUIDString;
+    if(!projectGUIDFilename.exists()) {
+        // Create a new GUID for project
+        GUID guid;
+        if(::CoCreateGuid(&guid) != S_OK) {
+            LOGE << "Unable to generate project GUID";
+            return EXIT_FAILURE;
+        }
+        
+        // Write GUID to string stream
+        std::stringstream projectGUIDStream;
+        projectGUIDStream << std::uppercase << std::hex << std::setfill('0');
+        projectGUIDStream << std::setw(8)<< guid.Data1 << '-';
+        projectGUIDStream << std::setw(4) << guid.Data2 << '-';
+        projectGUIDStream << std::setw(4) << guid.Data3 << '-';
+        projectGUIDStream << std::setw(2) << static_cast<short>(guid.Data4[0]) << static_cast<short>(guid.Data4[1]) << '-';
+        projectGUIDStream << static_cast<short>(guid.Data4[2]) << static_cast<short>(guid.Data4[3]) << static_cast<short>(guid.Data4[4]) << static_cast<short>(guid.Data4[5]) << static_cast<short>(guid.Data4[6]) << static_cast<short>(guid.Data4[7]);
+        
+        // Use result as project GUID string
+        projectGUIDString = projectGUIDStream.str();
+        LOGI << "Generated new project GUID:" << projectGUIDString;
+        
+        // Write GUID to project GUID file
+        std::ofstream projectGUIDFile(projectGUIDFilename.str());
+        projectGUIDFile << projectGUIDString << std::endl;
+    }
+    // Otherwise
+    else {
+        // Read GUID from project GUID file
+        std::ifstream projectGUIDFile(projectGUIDFilename.str());
+        std::getline(projectGUIDFile, projectGUIDString);
+        LOGI << "Using previously generated project GUID:" << projectGUIDString;
+    }
     // Create MSBuild project to compile and link all generated modules
     std::ofstream makefile((outputPath / "runner.vcxproj").str());
     CodeGenerator::generateMSBuild(makefile, backend, moduleNames);
