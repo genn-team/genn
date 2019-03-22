a63be003bc841d777c39e3c93f8f33cc5d2a7660
neworderofjamie <neworderofjamie@gmail.com>
2019-03-22 17:44:18 +0000

    Batch file to run tests on windows and update ignore

--- tests/run_tests.bat
+++ tests/run_tests.bat
@@ -1,34 +1,31 @@
 ECHO OFF
 SETLOCAL ENABLEDELAYEDEXPANSION
 
-SET "BUILD_FLAGS=-c"
-SET "MAKE_FLAGS=CPU_ONLY=1"
+REM SET "BUILD_FLAGS=-c"
+REM SET "MAKE_FLAGS=CPU_ONLY=1"
 
-REM suffix to apply to final folder of feature directory to get path
-SET "SUFFIX=_CODE"
+REM Push feature directories
+PUSHD features
 
 REM Loop through feature tests
-FOR /D %%F IN ("features\*") DO (
+FOR /D %%F IN (*) DO (
 	ECHO Running test %%F
 	
 	REM Push feature directory
 	PUSHD %%F
 	
-	REM Clean
-	nmake -f WINMakefile clean SIM_CODE=%%~nxF%SUFFIX%
+	REM Build model
+	CALL genn-buildmodel.bat %BUILD_FLAGS% model.cc
 	
 	REM Build model
-	genn-buildmodel.bat %BUILD_FLAGS% model.cc
-	
-	REM **YUCK** genn-build model seems to mess up path
-	CD %%F
-	
-	REM Build model
-	nmake -f WINMakefile SIM_CODE=%%~nxF%SUFFIX% %MAKE_FLAGS%
+	msbuild "%%F.sln" /t:%%F /p:BuildProjectReferences=true /p:Configuration=Release
 	
 	REM Run tests
     test.exe --gtest_output="xml:test_results.xml"
-	
+
 	REM pop directory
 	POPD
 )
+
+REM pop features
+POPD
