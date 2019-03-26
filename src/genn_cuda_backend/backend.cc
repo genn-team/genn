fad3d14608f458eb869fa61700d99aeb3d2ca2e0
neworderofjamie <neworderofjamie@gmail.com>
2019-03-26 17:35:13 +0000

    Add pragma to ignore warnings on CUDA - it seems like exceptions can be caught in linked code anyway

--- src/genn_cuda_backend/backend.cc
+++ src/genn_cuda_backend/backend.cc
@@ -1420,6 +1420,12 @@ void Backend::genDefinitionsInternalPreamble(CodeStream &os) const
 //--------------------------------------------------------------------------
 void Backend::genRunnerPreamble(CodeStream &os) const
 {
+#ifdef _WIN32
+    // **YUCK** on Windows, disable "function assumed not to throw an exception but does" warning
+    // Setting /Ehs SHOULD solve this but CUDA rules don't give this option and it's not clear it gets through to the compiler anyway
+    os << "#pragma warning(disable: 4297);" << std::endl;
+#endif
+
     // **TODO** move these into a header file shipped with GeNN and copied into generated code along with non-uniform RNGs
     os << "// ------------------------------------------------------------------------" << std::endl;
     os << "// Helper function for allocating memory blocks on the GPU device" << std::endl;
