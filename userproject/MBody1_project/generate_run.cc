6aeb1641eff8ce2bee34ef09c8d73cd25667aac9
neworderofjamie <J.C.Knight@sussex.ac.uk>
2019-04-01 15:17:48 +0100

    Extra generate runner options for bitmask and delayed synapses

--- userproject/MBody1_project/generate_run.cc
+++ userproject/MBody1_project/generate_run.cc
@@ -4,8 +4,10 @@ class GenerateRun : public GenerateRunBase
 {
 public:
     GenerateRun()
-    :   GenerateRunBase("MBody1")
+    :   GenerateRunBase("MBody1"), m_Bitmask(false), m_DelayedSynapses(false)
     {
+        getApp().add_flag("--bitmask", m_Bitmask, "Whether to use bitmasks to represent sparse PN->KC connectivity");
+        getApp().add_flag("--delayed-synapses", m_DelayedSynapses,  "Whether to simulate delays of (5 * DT) ms on KC->DN and of (3 * DT) ms on DN->DN synapse populations");
         getApp().add_option("numAL", m_NumAL, "Number of neurons in the antennal lobe (AL), the input neurons to this model")->required();
         getApp().add_option("numKC", m_NumKC, "Number of Kenyon cells (KC) in the \"hidden layer\"")->required();
         getApp().add_option("numLHI", m_NumLHI, "Number of lateral horn interneurons, implementing gain control")->required();
@@ -26,12 +28,22 @@ public:
         sizes << "#define _NLHI " << m_NumLHI << std::endl;
         sizes << "#define _NDN " << m_NumDN << std::endl;
         sizes << "#define _GScale " << m_GScale << std::endl;
+
+        if(m_Bitmask) {
+            sizes << "#define BITMASK" << std::endl;
+        }
+
+        if(m_DelayedSynapses) {
+            sizes << "#define DELAYED_SYNAPSES" << std::endl;
+        }
     }
 
 private:
     //------------------------------------------------------------------------
     // Members
     //------------------------------------------------------------------------
+    bool m_Bitmask;
+    bool m_DelayedSynapses;
     unsigned int m_NumAL;
     unsigned int m_NumKC;
     unsigned int m_NumLHI;
