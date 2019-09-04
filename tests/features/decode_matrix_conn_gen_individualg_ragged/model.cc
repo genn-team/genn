638a8d4058545c5aeb00250be2da4a46cf521c14
neworderofjamie <neworderofjamie@gmail.com>
2019-09-04 12:56:05 +0100

    Revert "Updated sparse connectivity initialization snippets to use ``$(id_post_begin)`` and ``$(id_post_end)`` rather than ``$(row_length)``"
    This reverts commit 75fca2c2f88feacd61d664276d366684b57568d0.

--- tests/features/decode_matrix_conn_gen_individualg_ragged/model.cc
+++ tests/features/decode_matrix_conn_gen_individualg_ragged/model.cc
@@ -18,7 +18,7 @@ public:
     DECLARE_SNIPPET(Decoder, 0);
 
     SET_ROW_BUILD_CODE(
-        "if(j < $(id_post_end)) {\n"
+        "if(j < $(num_post)) {\n"
         "   const unsigned int jValue = (1 << j);\n"
         "   if((($(id_pre) + 1) & jValue) != 0)\n"
         "   {\n"
@@ -29,7 +29,7 @@ public:
         "   $(endRow);\n"
         "}\n"
         "j++;\n");
-    SET_ROW_BUILD_STATE_VARS({{"j", "unsigned int", "$(id_post_begin)"}});
+    SET_ROW_BUILD_STATE_VARS({{"j", "unsigned int", 0}});
 };
 IMPLEMENT_SNIPPET(Decoder);
 
