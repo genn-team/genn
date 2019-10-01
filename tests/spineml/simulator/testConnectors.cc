2c324f3b8073c28235088316a2fab3e4c1b598b0
neworderofjamie <neworderofjamie@gmail.com>
2019-10-01 16:11:29 +0100

    Add a selection of delay tests

--- tests/spineml/simulator/testConnectors.cc
+++ tests/spineml/simulator/testConnectors.cc
@@ -25,12 +25,33 @@ void checkOneToOne(const unsigned int *rowLength, const unsigned int *ind, unsig
     }
 }
 
+void checkOneToOne(const unsigned int *rowLength, const unsigned int *ind, const uint8_t *delay, unsigned int numPre)
+{
+	checkOneToOne(rowLength, ind, numPre);
+	
+    for(unsigned int i = 0; i < numPre; i++) {
+        EXPECT_EQ(delay[i], (i + 1));
+    }
+}
+
 void checkTriangle(const unsigned int *rowLength, const unsigned int *ind, unsigned int maxRowLength, unsigned int numPre)
 {
     for(unsigned int i = 0; i < numPre; i++) {
         EXPECT_EQ(rowLength[i], i);
         for(unsigned int j = 0; j < rowLength[i]; j++){
             EXPECT_EQ(ind[(i * maxRowLength) + j], j);
+        }
+    }
+}
+
+
+void checkTriangle(const unsigned int *rowLength, const unsigned int *ind, const uint8_t *delay, unsigned int maxRowLength, unsigned int numPre)
+{
+	checkTriangle(rowLength, ind, maxRowLength, numPre);
+	
+    for(unsigned int i = 0; i < numPre; i++) {
+        for(unsigned int j = 0; j < rowLength[i]; j++){
+            EXPECT_EQ(delay[(i * maxRowLength) + j], i);
         }
     }
 }
@@ -87,6 +108,42 @@ TEST(FixedProbabilityConnectionTest, FullyConnectedDense) {
                                         nullptr, nullptr, nullptr, nullptr,
                                         basePath, remapIndices);
     EXPECT_EQ(n, 100 * 100);
+}
+//------------------------------------------------------------------------
+TEST(FixedProbabilityConnectionTest, DendriticDelayDeath) {
+    // XML fragment specifying connector
+    const char *connectorXML =
+        "<LL:Synapse>\n"
+        "   <FixedProbabilityConnection probability=\"0.1\" seed=\"123\"/>\n"
+        "</LL:Synapse>\n";
+
+    // Load XML and get root LL:Synapse element
+    pugi::xml_document connectorDocument;
+    connectorDocument.load_string(connectorXML);
+    auto synapse = connectorDocument.child("LL:Synapse");
+
+    // Parse XML and create sparse connector
+    filesystem::path basePath;
+	const unsigned int maxRowLength = 42;
+    unsigned int *rowLength = new unsigned int[42];
+    unsigned int *ind = new unsigned int[42 * maxRowLength];
+	uint8_t *delay = new uint8_t[42 * maxRowLength];
+	
+    try
+    {
+        std::vector<unsigned int> remapIndices;
+        Connectors::create(synapse, 1.0, 42, 42,
+                           &rowLength, &ind, &delay, &maxRowLength,
+                           basePath, remapIndices);
+        FAIL();
+    }
+    catch(const std::runtime_error &)
+    {
+    }
+	
+	delete [] rowLength;
+	delete [] ind;
+	delete [] delay;
 }
 
 //------------------------------------------------------------------------
@@ -149,6 +206,40 @@ TEST(AllToAllConnectionTest, SparseDeath) {
     delete [] ind;
     delete [] rowLength;
 }
+//------------------------------------------------------------------------
+TEST(AllToAllConnectionTest, DendriticDelayDeath) {
+    // XML fragment specifying connector
+    const char *connectorXML =
+        "<LL:Synapse>\n"
+        "   <AllToAllConnection/>\n"
+        "</LL:Synapse>\n";
+
+    // Load XML and get root LL:Synapse element
+    pugi::xml_document connectorDocument;
+    connectorDocument.load_string(connectorXML);
+    auto synapse = connectorDocument.child("LL:Synapse");
+
+	uint8_t *delay = new uint8_t[100 * 100];
+
+    // Parse XML and check connector creation correctly realises
+    // this should be implemented as dense matrix and thus
+    // doesn't require SparseProjection or allocation function
+    filesystem::path basePath;
+    std::vector<unsigned int> remapIndices;
+    try
+    {
+		Connectors::create(synapse, 1.0, 100, 100,
+						   nullptr, nullptr, &delay, nullptr,
+                           basePath, remapIndices);
+		FAIL();
+    }
+    catch(const std::runtime_error &)
+    {
+    }
+    
+	
+	delete[] delay;
+}
 
 //------------------------------------------------------------------------
 // OneToOneConnection tests
@@ -180,6 +271,39 @@ TEST(OneToOneConnectionTest, DenseDeath) {
     catch(const std::runtime_error &)
     {
     }
+}
+//------------------------------------------------------------------------
+TEST(OneToOneConnectionTest, DendriticDelayDeath) {
+    // XML fragment specifying connector
+    const char *connectorXML =
+        "<LL:Synapse>\n"
+        "   <OneToOneConnection/>\n"
+        "</LL:Synapse>\n";
+
+    // Load XML and get root LL:Synapse element
+    pugi::xml_document connectorDocument;
+    connectorDocument.load_string(connectorXML);
+    auto synapse = connectorDocument.child("LL:Synapse");
+
+	uint8_t *delay = new uint8_t[100 * 100];
+
+    // Parse XML and check connector creation correctly realises
+    // this should be implemented as dense matrix and thus
+    // doesn't require SparseProjection or allocation function
+    filesystem::path basePath;
+    std::vector<unsigned int> remapIndices;
+    try
+    {
+        Connectors::create(synapse, 1.0, 100, 100,
+                           nullptr, nullptr, &delay, nullptr,
+                           basePath, remapIndices);
+        FAIL();
+    }
+    catch(const std::runtime_error &)
+    {
+    }
+	
+	delete [] delay;
 }
 
 //------------------------------------------------------------------------
@@ -254,6 +378,89 @@ TEST(ConnectionListTest, InlineOneToOneSparse) {
     delete [] rowLength;
 }
 //------------------------------------------------------------------------
+TEST(ConnectionListTest, InlineOneToOneSparseDelay) {
+    // XML fragment specifying connector
+    const char *connectorXML =
+        "<LL:Synapse>\n"
+        "   <ConnectionList>\n"
+        "       <Connection src_neuron=\"0\" dst_neuron=\"0\" delay=\"1\"/>\n"
+        "       <Connection src_neuron=\"1\" dst_neuron=\"1\" delay=\"2\"/>\n"
+        "       <Connection src_neuron=\"2\" dst_neuron=\"2\" delay=\"3\"/>\n"
+        "       <Connection src_neuron=\"3\" dst_neuron=\"3\" delay=\"4\"/>\n"
+        "       <Connection src_neuron=\"4\" dst_neuron=\"4\" delay=\"5\"/>\n"
+        "       <Connection src_neuron=\"5\" dst_neuron=\"5\" delay=\"6\"/>\n"
+        "       <Connection src_neuron=\"6\" dst_neuron=\"6\" delay=\"7\"/>\n"
+        "       <Connection src_neuron=\"7\" dst_neuron=\"7\" delay=\"8\"/>\n"
+        "       <Connection src_neuron=\"8\" dst_neuron=\"8\" delay=\"9\"/>\n"
+        "       <Connection src_neuron=\"9\" dst_neuron=\"9\" delay=\"10\"/>\n"
+        "   </ConnectionList>\n"
+        "</LL:Synapse>\n";
+
+    // Load XML and get root LL:Synapse element
+    pugi::xml_document connectorDocument;
+    connectorDocument.load_string(connectorXML);
+    auto synapse = connectorDocument.child("LL:Synapse");
+
+    // Parse XML and create sparse connector
+    filesystem::path basePath;
+    std::vector<unsigned int> remapIndices;
+    const unsigned int maxRowLength = 1;
+    unsigned int *rowLength = new unsigned int[10];
+    unsigned int *ind = new unsigned int[10 * maxRowLength];
+	uint8_t *delay = new uint8_t[10 * maxRowLength];
+    Connectors::create(synapse, 1.0, 10, 10,
+                       &rowLength, &ind, &delay, &maxRowLength,
+                       basePath, remapIndices);
+
+    // Check number of connections matches XML
+    EXPECT_EQ(remapIndices.size(), 10);
+    checkOneToOne(rowLength, ind, delay, 10);
+	delete [] delay;
+    delete [] ind;
+    delete [] rowLength;
+}
+//------------------------------------------------------------------------
+TEST(ConnectionListTest, InlineOneToOneNoDelayDeath) {
+    // XML fragment specifying connector
+    const char *connectorXML =
+        "<LL:Synapse>\n"
+        "   <ConnectionList>\n"
+        "       <Connection src_neuron=\"0\" dst_neuron=\"0\" delay=\"1\"/>\n"
+        "       <Connection src_neuron=\"1\" dst_neuron=\"1\" delay=\"2\"/>\n"
+        "       <Connection src_neuron=\"2\" dst_neuron=\"2\" delay=\"3\"/>\n"
+        "       <Connection src_neuron=\"3\" dst_neuron=\"3\" delay=\"4\"/>\n"
+        "       <Connection src_neuron=\"4\" dst_neuron=\"4\" delay=\"5\"/>\n"
+        "       <Connection src_neuron=\"5\" dst_neuron=\"5\" delay=\"6\"/>\n"
+        "       <Connection src_neuron=\"6\" dst_neuron=\"6\" delay=\"7\"/>\n"
+        "       <Connection src_neuron=\"7\" dst_neuron=\"7\" delay=\"8\"/>\n"
+        "       <Connection src_neuron=\"8\" dst_neuron=\"8\" delay=\"9\"/>\n"
+        "       <Connection src_neuron=\"9\" dst_neuron=\"9\" delay=\"10\"/>\n"
+        "   </ConnectionList>\n"
+        "</LL:Synapse>\n";
+
+    // Load XML and get root LL:Synapse element
+    pugi::xml_document connectorDocument;
+    connectorDocument.load_string(connectorXML);
+    auto synapse = connectorDocument.child("LL:Synapse");
+
+    // Parse XML and create sparse connector
+    filesystem::path basePath;
+    std::vector<unsigned int> remapIndices;
+    const unsigned int maxRowLength = 1;
+    unsigned int *rowLength = new unsigned int[10];
+    unsigned int *ind = new unsigned int[10 * maxRowLength];
+	try
+    {
+		Connectors::create(synapse, 1.0, 10, 10,
+						   &rowLength, &ind, nullptr, &maxRowLength,
+						   basePath, remapIndices);
+		FAIL();
+	}
+	catch(const std::runtime_error &)
+    {
+    }
+}
+//------------------------------------------------------------------------
 TEST(ConnectionListTest, InlineTriangleSparse) {
     // XML fragment specifying connector
     const char *connectorXML =
@@ -303,7 +510,7 @@ TEST(ConnectionListTest, InlineShuffleTriangleSparse) {
         "<LL:Synapse>\n"
         "   <ConnectionList>\n"
         "       <Connection src_neuron=\"4\" dst_neuron=\"3\"/>\n"
-        "       <Connection src_neuron=\"1\" dst_neuron=\"0\"/>\n"
+        "       <Connection src_neuron=\"1\" dst_neuron=\"0\" />\n"
         "       <Connection src_neuron=\"3\" dst_neuron=\"0\"/>\n"
         "       <Connection src_neuron=\"4\" dst_neuron=\"2\"/>\n"
         "       <Connection src_neuron=\"2\" dst_neuron=\"0\"/>\n"
@@ -346,6 +553,61 @@ TEST(ConnectionListTest, InlineShuffleTriangleSparse) {
     EXPECT_EQ(remapIndices[8], (2 * maxRowLength) + 1);
     EXPECT_EQ(remapIndices[9], (3 * maxRowLength) + 2);
 
+    delete [] ind;
+    delete [] rowLength;
+}
+//------------------------------------------------------------------------
+TEST(ConnectionListTest, InlineShuffleTriangleSparseDelay) {
+    // XML fragment specifying connector
+    const char *connectorXML =
+        "<LL:Synapse>\n"
+        "   <ConnectionList>\n"
+        "       <Connection src_neuron=\"4\" dst_neuron=\"3\" delay=\"4\"/>\n"
+        "       <Connection src_neuron=\"1\" dst_neuron=\"0\" delay=\"1\"/>\n"
+        "       <Connection src_neuron=\"3\" dst_neuron=\"0\" delay=\"3\"/>\n"
+        "       <Connection src_neuron=\"4\" dst_neuron=\"2\" delay=\"4\"/>\n"
+        "       <Connection src_neuron=\"2\" dst_neuron=\"0\" delay=\"2\"/>\n"
+        "       <Connection src_neuron=\"4\" dst_neuron=\"0\" delay=\"4\"/>\n"
+        "       <Connection src_neuron=\"3\" dst_neuron=\"1\" delay=\"3\"/>\n"
+        "       <Connection src_neuron=\"4\" dst_neuron=\"1\" delay=\"4\"/>\n"
+        "       <Connection src_neuron=\"2\" dst_neuron=\"1\" delay=\"2\"/>\n"
+        "       <Connection src_neuron=\"3\" dst_neuron=\"2\" delay=\"3\"/>\n"
+        "   </ConnectionList>\n"
+        "</LL:Synapse>\n";
+
+    // Load XML and get root LL:Synapse element
+    pugi::xml_document connectorDocument;
+    connectorDocument.load_string(connectorXML);
+    auto synapse = connectorDocument.child("LL:Synapse");
+
+    // Parse XML and create sparse connector
+    filesystem::path basePath;
+    std::vector<unsigned int> remapIndices;
+    const unsigned int maxRowLength = 4;
+    unsigned int *rowLength = new unsigned int[5];
+    unsigned int *ind = new unsigned int[5 * maxRowLength];
+	uint8_t *delay = new uint8_t[5 * maxRowLength];
+    Connectors::create(synapse, 1.0, 5, 5,
+                       &rowLength, &ind, &delay, &maxRowLength,
+                       basePath, remapIndices);
+
+    // Check that resultant connectivity is triangle
+    checkTriangle(rowLength, ind, delay, maxRowLength, 5);
+
+    // Check number of connections matches XML and that remapping is correct
+    EXPECT_EQ(remapIndices.size(), 10);
+    EXPECT_EQ(remapIndices[0], (4 * maxRowLength) + 3);
+    EXPECT_EQ(remapIndices[1], (1 * maxRowLength) + 0);
+    EXPECT_EQ(remapIndices[2], (3 * maxRowLength) + 0);
+    EXPECT_EQ(remapIndices[3], (4 * maxRowLength) + 2);
+    EXPECT_EQ(remapIndices[4], (2 * maxRowLength) + 0);
+    EXPECT_EQ(remapIndices[5], (4 * maxRowLength) + 0);
+    EXPECT_EQ(remapIndices[6], (3 * maxRowLength) + 1);
+    EXPECT_EQ(remapIndices[7], (4 * maxRowLength) + 1);
+    EXPECT_EQ(remapIndices[8], (2 * maxRowLength) + 1);
+    EXPECT_EQ(remapIndices[9], (3 * maxRowLength) + 2);
+
+	delete [] delay;
     delete [] ind;
     delete [] rowLength;
 }
