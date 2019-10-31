71c4ca090cf15cc229e7a8caa8389dbdce846a92
neworderofjamie <neworderofjamie@gmail.com>
2019-10-31 12:57:42 +0000

    new test for presynaptic bitmask parallelism

---
+++ tests/features/decode_matrix_globalg_bitmask_pre/model.cc
@@ -0,0 +1,47 @@
+//--------------------------------------------------------------------------
+/*! \file decode_matrix_globalg_bitmask_pre/model.cc
+
+\brief model definition file that is part of the feature testing
+suite of minimal models with known analytic outcomes that are used for continuous integration testing.
+*/
+//--------------------------------------------------------------------------
+
+
+#include "modelSpec.h"
+
+//----------------------------------------------------------------------------
+// Neuron
+//----------------------------------------------------------------------------
+class Neuron : public NeuronModels::Base
+{
+public:
+    DECLARE_MODEL(Neuron, 0, 1);
+
+    SET_SIM_CODE("$(x)= $(Isyn);\n");
+
+    SET_VARS({{"x", "scalar"}});
+};
+
+IMPLEMENT_MODEL(Neuron);
+
+
+void modelDefinition(ModelSpec &model)
+{
+    model.setDT(0.1);
+    model.setName("decode_matrix_globalg_bitmask_pre");
+
+    // Static synapse parameters
+    WeightUpdateModels::StaticPulse::VarValues staticSynapseInit(1.0);    // 0 - Wij (nA)
+
+    model.addNeuronPopulation<NeuronModels::SpikeSource>("Pre", 10, {}, {});
+    model.addNeuronPopulation<Neuron>("Post", 4, {}, Neuron::VarValues(0.0));
+
+
+    auto syn = model.addSynapsePopulation<WeightUpdateModels::StaticPulse, PostsynapticModels::DeltaCurr>(
+        "Syn", SynapseMatrixType::BITMASK_GLOBALG, NO_DELAY, "Pre", "Post",
+        {}, staticSynapseInit,
+        {}, {});
+    syn->setSpanType(SynapseGroup::SpanType::PRESYNAPTIC);
+    
+    model.setPrecision(GENN_FLOAT);
+}
