c823e6bfbbdb026bdb89809851ff113a8a3eefbd
neworderofjamie <neworderofjamie@gmail.com>
2019-11-14 13:21:52 +0000

    Added tests for merging neuron groups

--- tests/unit/neuronGroup.cc
+++ tests/unit/neuronGroup.cc
@@ -2,7 +2,7 @@
 #include "gtest/gtest.h"
 
 // GeNN includes
-#include "modelSpec.h"
+#include "modelSpecInternal.h"
 
 //--------------------------------------------------------------------------
 // Tests
@@ -58,4 +58,126 @@ TEST(NeuronGroup, Poisson)
     ASSERT_FALSE(ng->isZeroCopyEnabled());
     ASSERT_TRUE(ng->isSimRNGRequired());
     ASSERT_FALSE(ng->isInitRNGRequired());
+}
+
+TEST(NeuronGroup, CompareDifferentParams)
+{
+    ModelSpecInternal model;
+
+    // Add two neuron groups to model
+    NeuronModels::Izhikevich::ParamValues paramValsA(0.02, 0.2, -65.0, 8.0);
+    NeuronModels::Izhikevich::ParamValues paramValsB(0.02, 0.2, -65.0, 4.0);
+    NeuronModels::Izhikevich::VarValues varVals(0.0, 0.0);
+    auto *ng0 = model.addNeuronPopulation<NeuronModels::Izhikevich>("Neurons0", 10, paramValsA, varVals);
+    auto *ng1 = model.addNeuronPopulation<NeuronModels::Izhikevich>("Neurons1", 10, paramValsA, varVals);
+    auto *ng2 = model.addNeuronPopulation<NeuronModels::Izhikevich>("Neurons2", 10, paramValsB, varVals);
+
+    model.finalize();
+
+    NeuronGroupInternal *ng0Internal = static_cast<NeuronGroupInternal *>(ng0);
+    ASSERT_TRUE(ng0Internal->canBeMerged(*ng1));
+    ASSERT_FALSE(ng0Internal->canBeMerged(*ng2));
+}
+
+TEST(NeuronGroup, CompareCurrentSources)
+{
+    ModelSpecInternal model;
+
+    // Add four neuron groups to model
+    NeuronModels::Izhikevich::ParamValues paramVals(0.02, 0.2, -65.0, 8.0);
+    NeuronModels::Izhikevich::VarValues varVals(0.0, 0.0);
+    auto *ng0 = model.addNeuronPopulation<NeuronModels::Izhikevich>("Neurons0", 10, paramVals, varVals);
+    auto *ng1 = model.addNeuronPopulation<NeuronModels::Izhikevich>("Neurons1", 10, paramVals, varVals);
+    auto *ng2 = model.addNeuronPopulation<NeuronModels::Izhikevich>("Neurons2", 10, paramVals, varVals);
+    auto *ng3 = model.addNeuronPopulation<NeuronModels::Izhikevich>("Neurons3", 10, paramVals, varVals);
+
+    // Add one gaussian and one DC current source to Neurons0
+    CurrentSourceModels::GaussianNoise::ParamValues cs0ParamVals(0.0, 0.1);
+    CurrentSourceModels::DC::ParamValues cs1ParamVals(0.4);
+    model.addCurrentSource<CurrentSourceModels::GaussianNoise>("CS0", "Neurons0", cs0ParamVals, {});
+    model.addCurrentSource<CurrentSourceModels::DC>("CS1", "Neurons0", cs1ParamVals, {});
+
+    // Do the same for Neuron1
+    model.addCurrentSource<CurrentSourceModels::GaussianNoise>("CS2", "Neurons1", cs0ParamVals, {});
+    model.addCurrentSource<CurrentSourceModels::DC>("CS3", "Neurons1", cs1ParamVals, {});
+
+    // Do the same, but in the opposite order for Neuron2
+    model.addCurrentSource<CurrentSourceModels::DC>("CS4", "Neurons2", cs1ParamVals, {});
+    model.addCurrentSource<CurrentSourceModels::GaussianNoise>("CS5", "Neurons2", cs0ParamVals, {});
+    
+    // Add two DC sources to Neurons3
+    model.addCurrentSource<CurrentSourceModels::DC>("CS6", "Neurons3", cs1ParamVals, {});
+    model.addCurrentSource<CurrentSourceModels::DC>("CS7", "Neurons3", cs1ParamVals, {});
+
+    model.finalize();
+
+    NeuronGroupInternal *ng0Internal = static_cast<NeuronGroupInternal *>(ng0);
+    ASSERT_TRUE(ng0Internal->canBeMerged(*ng1));
+    ASSERT_TRUE(ng0Internal->canBeMerged(*ng2));
+    ASSERT_FALSE(ng0Internal->canBeMerged(*ng3));
+}
+
+TEST(NeuronGroup, ComparePostsynapticModels)
+{
+    ModelSpecInternal model;
+
+    // Add two neuron groups to model
+    NeuronModels::Izhikevich::ParamValues paramVals(0.02, 0.2, -65.0, 8.0);
+    NeuronModels::Izhikevich::VarValues varVals(0.0, 0.0);
+    model.addNeuronPopulation<NeuronModels::SpikeSource>("SpikeSource", 10, {}, {});
+    auto *ng0 = model.addNeuronPopulation<NeuronModels::Izhikevich>("Neurons0", 10, paramVals, varVals);
+    auto *ng1 = model.addNeuronPopulation<NeuronModels::Izhikevich>("Neurons1", 10, paramVals, varVals);
+    auto *ng2 = model.addNeuronPopulation<NeuronModels::Izhikevich>("Neurons2", 10, paramVals, varVals);
+    auto *ng3 = model.addNeuronPopulation<NeuronModels::Izhikevich>("Neurons3", 10, paramVals, varVals);
+
+    // Add incoming synapse groups with Delta and DeltaCurr postsynaptic models to Neurons0
+    WeightUpdateModels::StaticPulse::VarValues staticPulseVarVals(0.1);
+    PostsynapticModels::ExpCurr::ParamValues expCurrParamVals(0.5);
+    model.addSynapsePopulation<WeightUpdateModels::StaticPulse, PostsynapticModels::DeltaCurr>("SG0", SynapseMatrixType::SPARSE_GLOBALG, NO_DELAY,
+                                                                                               "SpikeSource", "Neurons0",
+                                                                                               {}, staticPulseVarVals,
+                                                                                               {}, {});
+    model.addSynapsePopulation<WeightUpdateModels::StaticPulse, PostsynapticModels::ExpCurr>("SG1", SynapseMatrixType::SPARSE_GLOBALG, NO_DELAY,
+                                                                                             "SpikeSource", "Neurons0",
+                                                                                             {}, staticPulseVarVals,
+                                                                                             expCurrParamVals, {});
+
+    // Do the same for Neuron1
+    model.addSynapsePopulation<WeightUpdateModels::StaticPulse, PostsynapticModels::DeltaCurr>("SG2", SynapseMatrixType::SPARSE_GLOBALG, NO_DELAY,
+                                                                                               "SpikeSource", "Neurons1",
+                                                                                               {}, staticPulseVarVals,
+                                                                                               {}, {});
+    model.addSynapsePopulation<WeightUpdateModels::StaticPulse, PostsynapticModels::ExpCurr>("SG3", SynapseMatrixType::SPARSE_GLOBALG, NO_DELAY,
+                                                                                             "SpikeSource", "Neurons1",
+                                                                                             {}, staticPulseVarVals,
+                                                                                             expCurrParamVals, {});
+
+    // Do the same, but in the opposite order for Neuron2
+    model.addSynapsePopulation<WeightUpdateModels::StaticPulse, PostsynapticModels::ExpCurr>("SG4", SynapseMatrixType::SPARSE_GLOBALG, NO_DELAY,
+                                                                                             "SpikeSource", "Neurons2",
+                                                                                             {}, staticPulseVarVals,
+                                                                                             expCurrParamVals, {});
+    model.addSynapsePopulation<WeightUpdateModels::StaticPulse, PostsynapticModels::DeltaCurr>("SG5", SynapseMatrixType::SPARSE_GLOBALG, NO_DELAY,
+                                                                                               "SpikeSource", "Neurons2",
+                                                                                               {}, staticPulseVarVals,
+                                                                                               {}, {});
+
+    // Add two incoming synapse groups with DeltaCurr postsynaptic models sources to Neurons3
+    model.addSynapsePopulation<WeightUpdateModels::StaticPulse, PostsynapticModels::DeltaCurr>("SG6", SynapseMatrixType::SPARSE_GLOBALG, NO_DELAY,
+                                                                                               "SpikeSource", "Neurons3",
+                                                                                               {}, staticPulseVarVals,
+                                                                                               {}, {});
+
+    model.addSynapsePopulation<WeightUpdateModels::StaticPulse, PostsynapticModels::DeltaCurr>("SG7", SynapseMatrixType::SPARSE_GLOBALG, NO_DELAY,
+                                                                                               "SpikeSource", "Neurons3",
+                                                                                               {}, staticPulseVarVals,
+                                                                                               {}, {});
+
+
+    model.finalize();
+
+    NeuronGroupInternal *ng0Internal = static_cast<NeuronGroupInternal *>(ng0);
+    ASSERT_TRUE(ng0Internal->canBeMerged(*ng1));
+    ASSERT_TRUE(ng0Internal->canBeMerged(*ng2));
+    ASSERT_FALSE(ng0Internal->canBeMerged(*ng3));
 }
