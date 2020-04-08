19b8f96973c19cf0999833a36f6e3a2c3fbae43b
ginpachi <m.fawaadali98@gmail.com>
2020-04-08 19:25:00 +0500

    Add definition for CHECK_OPENCL_ERRORS

--- src/genn/backends/opencl/backend.cc
+++ src/genn/backends/opencl/backend.cc
@@ -656,55 +656,17 @@ void Backend::genDefinitionsInternalPreamble(CodeStream& os) const
     os << "#include <CL/cl.hpp>" << std::endl;
     os << std::endl;
     os << "#define DEVICE_INDEX " << m_ChosenDeviceID << std::endl;
-    /*os << std::endl;
-    os << "// Function for getting OpenCL errors as string" << std::endl;
-    os << "const char * clGetErrorString(cl_int error)";
-    {
-        CodeStream::Scope b(os);
-        std::map<cl_int, std::string> allClErrors = {
-            // run-time and JIT compiler errors
-            { 0, "CL_SUCCESS" },                            { -1, "CL_DEVICE_NOT_FOUND" },              { -2, "CL_DEVICE_NOT_AVAILABLE" },
-            { -3, "CL_COMPILER_NOT_AVAILABLE" },            { -4, "CL_MEM_OBJECT_ALLOCATION_FAILURE" }, { -5, "CL_OUT_OF_RESOURCES" },
-            { -6, "CL_OUT_OF_HOST_MEMORY" },                { -7, "CL_PROFILING_INFO_NOT_AVAILABLE" },  { -8, "CL_MEM_COPY_OVERLAP" },
-            { -9, "CL_IMAGE_FORMAT_MISMATCH" },             { -10, "CL_IMAGE_FORMAT_NOT_SUPPORTED" },   { -11, "CL_BUILD_PROGRAM_FAILURE" },
-            { -12, "CL_MAP_FAILURE" },                      { -13, "CL_MISALIGNED_SUB_BUFFER_OFFSET" }, { -14, "CL_EXEC_STATUS_ERROR_FOR_EVENTS_IN_WAIT_LIST" },
-            { -15, "CL_COMPILE_PROGRAM_FAILURE" },          { -16, "CL_LINKER_NOT_AVAILABLE" },         { -17, "CL_LINK_PROGRAM_FAILURE" },
-            { -18, "CL_DEVICE_PARTITION_FAILED" },          { -19, "CL_KERNEL_ARG_INFO_NOT_AVAILABLE" },
-            // compile-time errors
-            { -30, "CL_INVALID_VALUE" },                    { -31, "CL_INVALID_DEVICE_TYPE" },          { -32, "CL_INVALID_PLATFORM" },
-            { -33, "CL_INVALID_DEVICE" },                   { -34, "CL_INVALID_CONTEXT" },              { -35, "CL_INVALID_QUEUE_PROPERTIES" },
-            { -36, "CL_INVALID_COMMAND_QUEUE" },            { -37, "CL_INVALID_HOST_PTR" },             { -38, "CL_INVALID_MEM_OBJECT" },
-            { -39, "CL_INVALID_IMAGE_FORMAT_DESCRIPTOR" },  { -40, "CL_INVALID_IMAGE_SIZE" },           { -41, "CL_INVALID_SAMPLER" },
-            { -42, "CL_INVALID_BINARY" },                   { -43, "CL_INVALID_BUILD_OPTIONS" },        { -44, "CL_INVALID_PROGRAM" },
-            { -45, "CL_INVALID_PROGRAM_EXECUTABLE" },       { -46, "CL_INVALID_KERNEL_NAME" },          { -47, "CL_INVALID_KERNEL_DEFINITION" },
-            { -48, "CL_INVALID_KERNEL" },                   { -49, "CL_INVALID_ARG_INDEX" },            { -50, "CL_INVALID_ARG_VALUE" },
-            { -51, "CL_INVALID_ARG_SIZE" },                 { -52, "CL_INVALID_KERNEL_ARGS" },          { -53, "CL_INVALID_WORK_DIMENSION" },
-            { -54, "CL_INVALID_WORK_GROUP_SIZE" },          { -55, "CL_INVALID_WORK_ITEM_SIZE" },       { -56, "CL_INVALID_GLOBAL_OFFSET" },
-            { -57, "CL_INVALID_EVENT_WAIT_LIST" },          { -58, "CL_INVALID_EVENT" },                { -59, "CL_INVALID_OPERATION" },
-            { -60, "CL_INVALID_GL_OBJECT" },                { -61, "CL_INVALID_BUFFER_SIZE" },          { -62, "CL_INVALID_MIP_LEVEL" },
-            { -63, "CL_INVALID_GLOBAL_WORK_SIZE" },         { -64, "CL_INVALID_PROPERTY" },             { -65, "CL_INVALID_IMAGE_DESCRIPTOR" },
-            { -66, "CL_INVALID_COMPILER_OPTIONS" },         { -67, "CL_INVALID_LINKER_OPTIONS" },       { -68, "CL_INVALID_DEVICE_PARTITION_COUNT" }
-        };
-
-        os << "switch(error)";
-        {
-            CodeStream::Scope b(os);
-
-            for (const auto& e : allClErrors) {
-                os << "case " << e.first << ": return \"" << e.second << "\";" << std::endl;
-            }
-
-            os << "default: return \"Unknown OpenCL error\";" << std::endl;
-        }
-    }*/
+    os << std::endl;
     os << "// ------------------------------------------------------------------------" << std::endl;
     os << "// Helper macro for error-checking OpenCL calls" << std::endl;
     os << "#define CHECK_OPENCL_ERRORS(call) {\\" << std::endl;
     os << "    cl_int error = call;\\" << std::endl;
     os << "    if (error != CL_SUCCESS) {\\" << std::endl;
-    //os << "        throw std::runtime_error(__FILE__\": \" + std::to_string(__LINE__) + \": opencl error \" + std::to_string(error) + \": \" + clGetErrorString(error));\\" << std::endl;
+    os << "        throw std::runtime_error(__FILE__\": \" + std::to_string(__LINE__) + \": opencl error \" + std::to_string(error) + \": \" + opencl::clGetErrorString(error));\\" << std::endl;
     os << "    }\\" << std::endl;
     os << "}" << std::endl;
+
+    os << std::endl;
 
     // Declaration of OpenCL functions
     os << "// ------------------------------------------------------------------------" << std::endl;
@@ -715,7 +677,9 @@ void Backend::genDefinitionsInternalPreamble(CodeStream& os) const
         CodeStream::Scope b(os);
         os << "void setUpContext(cl::Context& context, cl::Device& device, const int deviceIndex);" << std::endl;
         os << "void createProgram(const char* kernelSource, cl::Program& program, cl::Context& context);" << std::endl;
+        os << "char* clGetErrorString(cl_int error);" << std::endl;
     }
+
     os << std::endl;
 
     // Declaration of OpenCL variables
@@ -831,6 +795,47 @@ void Backend::genRunnerPreamble(CodeStream& os) const
         os << "// Reading the kernel source for execution" << std::endl;
         os << "program = cl::Program(context, kernelSource, true);" << std::endl;
         os << "program.build(\"-cl-std=CL1.2\");" << std::endl;
+    }
+    os << std::endl;
+    os << "// Get OpenCL error as string" << std::endl;
+    os << "char* opencl::clGetErrorString(cl_int error)";
+    {
+        CodeStream::Scope b(os);
+        std::map<cl_int, std::string> allClErrors = {
+            // run-time and JIT compiler errors
+            { 0, "CL_SUCCESS" },                            { -1, "CL_DEVICE_NOT_FOUND" },              { -2, "CL_DEVICE_NOT_AVAILABLE" },
+            { -3, "CL_COMPILER_NOT_AVAILABLE" },            { -4, "CL_MEM_OBJECT_ALLOCATION_FAILURE" }, { -5, "CL_OUT_OF_RESOURCES" },
+            { -6, "CL_OUT_OF_HOST_MEMORY" },                { -7, "CL_PROFILING_INFO_NOT_AVAILABLE" },  { -8, "CL_MEM_COPY_OVERLAP" },
+            { -9, "CL_IMAGE_FORMAT_MISMATCH" },             { -10, "CL_IMAGE_FORMAT_NOT_SUPPORTED" },   { -11, "CL_BUILD_PROGRAM_FAILURE" },
+            { -12, "CL_MAP_FAILURE" },                      { -13, "CL_MISALIGNED_SUB_BUFFER_OFFSET" }, { -14, "CL_EXEC_STATUS_ERROR_FOR_EVENTS_IN_WAIT_LIST" },
+            { -15, "CL_COMPILE_PROGRAM_FAILURE" },          { -16, "CL_LINKER_NOT_AVAILABLE" },         { -17, "CL_LINK_PROGRAM_FAILURE" },
+            { -18, "CL_DEVICE_PARTITION_FAILED" },          { -19, "CL_KERNEL_ARG_INFO_NOT_AVAILABLE" },
+            // compile-time errors
+            { -30, "CL_INVALID_VALUE" },                    { -31, "CL_INVALID_DEVICE_TYPE" },          { -32, "CL_INVALID_PLATFORM" },
+            { -33, "CL_INVALID_DEVICE" },                   { -34, "CL_INVALID_CONTEXT" },              { -35, "CL_INVALID_QUEUE_PROPERTIES" },
+            { -36, "CL_INVALID_COMMAND_QUEUE" },            { -37, "CL_INVALID_HOST_PTR" },             { -38, "CL_INVALID_MEM_OBJECT" },
+            { -39, "CL_INVALID_IMAGE_FORMAT_DESCRIPTOR" },  { -40, "CL_INVALID_IMAGE_SIZE" },           { -41, "CL_INVALID_SAMPLER" },
+            { -42, "CL_INVALID_BINARY" },                   { -43, "CL_INVALID_BUILD_OPTIONS" },        { -44, "CL_INVALID_PROGRAM" },
+            { -45, "CL_INVALID_PROGRAM_EXECUTABLE" },       { -46, "CL_INVALID_KERNEL_NAME" },          { -47, "CL_INVALID_KERNEL_DEFINITION" },
+            { -48, "CL_INVALID_KERNEL" },                   { -49, "CL_INVALID_ARG_INDEX" },            { -50, "CL_INVALID_ARG_VALUE" },
+            { -51, "CL_INVALID_ARG_SIZE" },                 { -52, "CL_INVALID_KERNEL_ARGS" },          { -53, "CL_INVALID_WORK_DIMENSION" },
+            { -54, "CL_INVALID_WORK_GROUP_SIZE" },          { -55, "CL_INVALID_WORK_ITEM_SIZE" },       { -56, "CL_INVALID_GLOBAL_OFFSET" },
+            { -57, "CL_INVALID_EVENT_WAIT_LIST" },          { -58, "CL_INVALID_EVENT" },                { -59, "CL_INVALID_OPERATION" },
+            { -60, "CL_INVALID_GL_OBJECT" },                { -61, "CL_INVALID_BUFFER_SIZE" },          { -62, "CL_INVALID_MIP_LEVEL" },
+            { -63, "CL_INVALID_GLOBAL_WORK_SIZE" },         { -64, "CL_INVALID_PROPERTY" },             { -65, "CL_INVALID_IMAGE_DESCRIPTOR" },
+            { -66, "CL_INVALID_COMPILER_OPTIONS" },         { -67, "CL_INVALID_LINKER_OPTIONS" },       { -68, "CL_INVALID_DEVICE_PARTITION_COUNT" }
+        };
+
+        os << "switch(error)";
+        {
+            CodeStream::Scope b(os);
+
+            for (const auto& e : allClErrors) {
+                os << "case " << e.first << ": return \"" << e.second << "\";" << std::endl;
+            }
+
+            os << "default: return \"Unknown OpenCL error\";" << std::endl;
+        }
     }
     os << std::endl;
 }
