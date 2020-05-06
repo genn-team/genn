46c52d660a2b0e6153f2ed61358341b1d3845482
ginpachi <m.fawaadali98@gmail.com>
2020-05-06 15:13:05 +0500

    Use getSynapseDynamicsCode() for KernelSynapseDynamicsUpdate

--- src/genn/backends/cuda/optimiser.cc
+++ src/genn/backends/cuda/optimiser.cc
@@ -109,6 +109,9 @@ void calcGroupSizes(const cudaDeviceProp &deviceProps, const CodeGenerator::CUDA
 
         if(!s.second.getWUModel()->getLearnPostCode().empty()) {
             groupSizes[KernelPostsynapticUpdate].push_back(Backend::getNumPostsynapticUpdateThreads(s.second));
+        }
+
+        if(!s.second.getWUModel()->getSynapseDynamicsCode().empty()) {
             groupSizes[KernelSynapseDynamicsUpdate].push_back(Backend::getNumSynapseDynamicsThreads(s.second));
         }
 
