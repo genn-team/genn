934c9078dabfb0c75a55ceee1023c8babb7bc16a
neworderofjamie <neworderofjamie@gmail.com>
2020-08-03 18:36:28 +0100

    missing ``GENN_EXPORT``

--- include/genn/genn/code_generator/codeGenUtils.h
+++ include/genn/genn/code_generator/codeGenUtils.h
@@ -121,9 +121,9 @@ inline size_t padSize(size_t size, size_t blockSize)
     return ceilDivide(size, blockSize) * blockSize;
 }
 
-void genParamValVecInit(CodeStream &os, const CodeGenerator::Substitutions &subs, const Snippet::Base::ParamValVec &paramValVec, 
-                        const std::string &errorContext, bool constant = false, bool definition = true, 
-                        const std::string &prefix = "", const std::string &suffix = "");
+GENN_EXPORT void genParamValVecInit(CodeStream &os, const CodeGenerator::Substitutions &subs, const Snippet::Base::ParamValVec &paramValVec, 
+                                    const std::string &errorContext, bool constant = false, bool definition = true, 
+                                    const std::string &prefix = "", const std::string &suffix = "");
 
 template<typename T>
 void genMergedGroupPush(CodeStream &os, const std::vector<T> &groups, const MergedStructData &mergedStructData,
