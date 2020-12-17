c424070cd015294e679de87c0ebc5c9f9857d31b
neworderofjamie <neworderofjamie@gmail.com>
2020-12-17 16:23:44 +0000

    remove latex infrastructure

--- makedoc.sh
+++ makedoc.sh
@@ -1,8 +1,3 @@
 #! /bin/bash
 export GENN_PATH=$(dirname $(realpath "$0"))
 (cat doxygen/genn-doxygen.conf  ; echo "PROJECT_NUMBER=`cat version.txt`") | doxygen -
-doxygen/fixLatexPageref.pl documentation/latex/files.tex
-doxygen/fixLatexPageref.pl documentation/latex/hierarchy.tex
-doxygen/fixLatexPageref.pl documentation/latex/annotated.tex
-doxygen/fixLatexLabels.pl documentation/latex/refman.tex
-doxygen/replace_in_files.pl "\\\\backmatter" "" documentation/latex/refman.tex
