1fbec0e35c783afa6f9a4ed22868d33c2b7da99b
neworderofjamie <neworderofjamie@gmail.com>
2021-01-28 13:35:43 +0000

    added various checks for validity to CustomUpdate constructor

--- src/genn/genn/customUpdate.cc
+++ src/genn/genn/customUpdate.cc
@@ -1,12 +1,14 @@
 #include "customUpdate.h"
 
-
 // Standard includes
 #include <algorithm>
 #include <cmath>
 
 // GeNN includes
 #include "gennUtils.h"
+#include "currentSource.h"
+#include "neuronGroupInternal.h"
+#include "synapseGroupInternal.h"
 
 //------------------------------------------------------------------------
 // CustomUpdate
@@ -43,21 +45,70 @@ CustomUpdate::CustomUpdate(const std::string &name, const std::string &updateGro
     m_VarInitialisers(varInitialisers), m_VarReferences(varReferences), m_VarLocation(varInitialisers.size(), defaultVarLocation),
     m_ExtraGlobalParamLocation(customUpdateModel->getExtraGlobalParams().size(), defaultExtraGlobalParamLocation)
 {
-    // Loop through all variable references and check types of variable references against those specified in model
-    // **THINK** due to GeNN's current string-based type system this is rather conservative
+    // Determine whether this is a WU var update
+    const bool wuCustomUpdate = std::all_of(m_VarReferences.cbegin(), m_VarReferences.cend(),
+                                            [](const Models::VarReference &v) { return v.getType() == Models::VarReference::Type::WU; });
+
+
+    // Loop through all variable references
     for(size_t i = 0; i < varReferences.size(); i++) {
-        if(m_VarReferences.at(i).getVar().type != getCustomUpdateModel()->getVarRefs().at(i).type) {
+        const auto varRef = m_VarReferences.at(i);
+
+        // Check types of variable references against those specified in model
+        // **THINK** due to GeNN's current string-based type system this is rather conservative
+        if(varRef.getVar().type != getCustomUpdateModel()->getVarRefs().at(i).type) {
             throw std::runtime_error("Incompatible type for variable reference '" + getCustomUpdateModel()->getVarRefs().at(i).name + "'");
+        }
+
+        // If this isn't a WU var custom update but this reference points to a WU variable, give error
+        if(!wuCustomUpdate && varRef.getType() == Models::VarReference::Type::WU) {
+            throw std::runtime_error("Either ALL or NONE of the variables referenced by a custom update must be weight update variables.");
+        }
+    }
+
+    // If there are any variable references
+    if(!m_VarReferences.empty()) {
+        // If this is a weight update variable custom update
+        if(wuCustomUpdate) {
+            const SynapseGroupInternal *firstSG = static_cast<const SynapseGroupInternal *>(m_VarReferences.front().getSynapseGroup());
+            const size_t preSize = firstSG->getSrcNeuronGroup()->getNumNeurons();
+            const size_t maxRowLength = firstSG->getMaxConnections();
+            const bool sparse = (firstSG->getMatrixType() & SynapseMatrixConnectivity::SPARSE);
+
+            for(const auto &v : m_VarReferences) {
+                // Check that connectivity types match
+                const SynapseGroupInternal *sg =  static_cast<const SynapseGroupInternal*>(v.getSynapseGroup());
+                if((sg->getMatrixType() & SynapseMatrixConnectivity::SPARSE) != sparse) {
+                    throw std::runtime_error("Variable references to weight update model variables must all be to populations with the same connectivity type.");
+                }
+                
+                // Check that presynaptic population size and maximum row lengths match
+                if(sg->getSrcNeuronGroup()->getNumNeurons() != preSize || sg->getMaxConnections() != maxRowLength) {
+                    throw std::runtime_error("Variable references must all be to populations of the same size.");
+                }
+
+                // Check that, if connectivity is sparse, all referenced variables belong to the same population
+                // **NOTE** this is because sparse connectivity on two differnt populations 
+                // might have the same max row length but totally different connectivity
+                if(sparse && firstSG != sg) {
+                    throw std::runtime_error("Variable references to sparse weight update model variables must all be in the same synapse group.");
+                }
+            }
         }
     }
 
     // If this is a transpose operation
     if(m_Operation == Operation::UPDATE_TRANSPOSE) {
-        // Give error if any of the variable references aren't to weight update variables
-        if(std::any_of(m_VarReferences.cbegin(), m_VarReferences.cend(),
-                       [](const Models::VarReference &v) { return v.getType() != Models::VarReference::Type::WU; }))
-        {
+        // Give error if this isn't a WU var update
+        if(!wuCustomUpdate) {
             throw std::runtime_error("Custom updates that perform a transpose operation can only operate on weight update model variables.");
+        }
+
+        // Give error if any of the variable references aren't dense
+        if(std::any_of(m_VarReferences.cbegin(), m_VarReferences.cend(),
+                       [](const Models::VarReference &v) { return !(v.getSynapseGroup()->getMatrixType() & SynapseMatrixConnectivity::DENSE); }))
+        {
+            throw std::runtime_error("Custom updates that perform a transpose operation can currently only be used on DENSE synaptic matrices.");
         }
     }
 }
