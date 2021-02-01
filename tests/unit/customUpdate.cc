46c0563fe3058ed007a6e4e34d80e636c67c0eb1
neworderofjamie <neworderofjamie@gmail.com>
2021-02-01 11:55:13 +0000

    added a couple more tests

--- tests/unit/customUpdate.cc
+++ tests/unit/customUpdate.cc
@@ -26,7 +26,7 @@ IMPLEMENT_MODEL(Sum);
 //--------------------------------------------------------------------------
 TEST(CustomUpdates, VarReferenceTypeChecks)
 {
-   ModelSpecInternal model;
+    ModelSpecInternal model;
 
     // Add two neuron group to model
     NeuronModels::Izhikevich::ParamValues paramVals(0.02, 0.2, -65.0, 8.0);
@@ -55,9 +55,72 @@ TEST(CustomUpdates, VarReferenceTypeChecks)
     }
     catch(const std::runtime_error &) {
     }
-    // Finalize model
-    model.finalize();
+}
+//--------------------------------------------------------------------------
+TEST(CustomUpdates, VarSizeChecks)
+{
+    ModelSpecInternal model;
 
+    // Add two neuron group to model
+    NeuronModels::Izhikevich::ParamValues paramVals(0.02, 0.2, -65.0, 8.0);
+    NeuronModels::Izhikevich::VarValues varVals(0.0, 0.0);
+    auto *ng1 = model.addNeuronPopulation<NeuronModels::Izhikevich>("Neuron1", 10, paramVals, varVals);
+    auto *ng2 = model.addNeuronPopulation<NeuronModels::Izhikevich>("Neuron2", 10, paramVals, varVals);
+    auto *ng3 = model.addNeuronPopulation<NeuronModels::Izhikevich>("Neuron3", 25, paramVals, varVals);
+
+    Sum::VarValues sumVarValues(0.0);
+    Sum::VarReferences sumVarReferences1(createVarRef(ng1, "V"), createVarRef(ng1, "U"));
+    Sum::VarReferences sumVarReferences2(createVarRef(ng1, "V"), createVarRef(ng2, "V"));
+    Sum::VarReferences sumVarReferences3(createVarRef(ng1, "V"), createVarRef(ng3, "V"));
+
+    model.addCustomUpdate<Sum>("Sum1", "CustomUpdate",
+                               {}, sumVarValues, sumVarReferences1);
+    model.addCustomUpdate<Sum>("Sum2", "CustomUpdate",
+                               {}, sumVarValues, sumVarReferences2);
+
+    try {
+        model.addCustomUpdate<Sum>("Sum3", "CustomUpdate",
+                                   {}, sumVarValues, sumVarReferences3);
+        FAIL();
+    }
+    catch(const std::runtime_error &) {
+    }
+}
+//--------------------------------------------------------------------------
+TEST(CustomUpdates, WUVarSynapseGroupChecks)
+{
+    ModelSpecInternal model;
+
+    // Add two neuron group to model
+    NeuronModels::Izhikevich::ParamValues paramVals(0.02, 0.2, -65.0, 8.0);
+    NeuronModels::Izhikevich::VarValues varVals(0.0, 0.0);
+    model.addNeuronPopulation<NeuronModels::Izhikevich>("Pre", 10, paramVals, varVals);
+    model.addNeuronPopulation<NeuronModels::Izhikevich>("Post", 25, paramVals, varVals);
+
+    auto *sg1 = model.addSynapsePopulation<WeightUpdateModels::StaticPulseDendriticDelay, PostsynapticModels::DeltaCurr>(
+        "Synapses1", SynapseMatrixType::DENSE_INDIVIDUALG, NO_DELAY,
+        "Pre", "Post",
+        {}, {1.0, 4},
+        {}, {});
+    auto *sg2 = model.addSynapsePopulation<WeightUpdateModels::StaticPulseDendriticDelay, PostsynapticModels::DeltaCurr>(
+        "Synapses2", SynapseMatrixType::DENSE_INDIVIDUALG, NO_DELAY,
+        "Pre", "Post",
+        {}, {1.0, 4},
+        {}, {});
 
     
+    Sum::VarValues sumVarValues(0.0);
+    Sum::WUVarReferences sumVarReferences1(createWUVarRef(sg1, "g"), createWUVarRef(sg1, "g"));
+    Sum::WUVarReferences sumVarReferences2(createWUVarRef(sg1, "g"), createWUVarRef(sg2, "d"));
+
+    model.addCustomUpdate<Sum>("SumWeight1", "CustomUpdate", CustomUpdateWU::Operation::UPDATE,
+                               {}, sumVarValues, sumVarReferences1);
+    
+    try {
+        model.addCustomUpdate<Sum>("SumWeight2", "CustomUpdate", CustomUpdateWU::Operation::UPDATE,
+                                   {}, sumVarValues, sumVarReferences2);
+        FAIL();
+    }
+    catch(const std::runtime_error &) {
+    }
 }
