24b1fe206ca2cf990464db777918cab35dc3338b
Thomas Nowotny <tn41@inf900324.inf.susx.ac.uk>
2021-06-15 19:20:44 +0100

    There was a problem with the new binomialInverseCDF in that many of the terms added to make up the CDF are flushed to 0 even for long double floating point precision. For more challenging examples, this means that the CDF is under-estimated and in the worst cases, the desired CDF is never reached and the function returned the inverse "n". Here I have introduced a tweak (hack?) that makes use of the fact that the missing contributions are for small k << n*p and for large k >> n*p. If the desired CDF is small, we use the usual algorithm and the missing values for small k aren't that important as the contributions of each term to the sum increase in size rapidly and the last contribution easily outweighs all missing conributions, such that the error made is only on the order of 1.
    To get the same benefits of rising contributions for the case of large
    desired CDFs (desired CDF > p), we calculate the sum of probabilities for k
    starting at n and descending to 0 and call the inverse CDF result, when this
    sum has surpassed (1- desired CDF). With this tweak, we get the same benefits
    and the error incurred due to missing terms for large k causes only minor
    errors on the inverse CDF value.
    
    I have tested against SciPy's PPF function for n=1e6, p=0.02 and desired CDFS:
    1e-10 to 1e-20, and (1-1e-10) ... (1-1e-16) and the results differed at most
    by 1.

--- src/genn/genn/binomial.cc
+++ src/genn/genn/binomial.cc
@@ -28,38 +28,74 @@ unsigned int binomialInverseCDF(double cdf, unsigned int n, double p)
     //                      |k|
 
     // The logarithms in the middle two terms can be pre-calculated and then added on for every k:
-    const long double logProbRatio = log(p) - log(1.0 - p);
+    const long double logProbRatio = log((long double)p) - log((long double)(1.0 - p));
 
-    // The final term is a constant so we can again calculate it once at the start of our sum:
-    long double logPMF = (long double)n * log((long double)(1.0 - p));
- 
-    // Because the first three terms of (2) will be zero for k=0,
-    // we can can calculate the CDF by taking the exponent of the constant term
-    long double cdfTotal = exp(logPMF);
+    if (cdf < p) {
+        // The final term is a constant so we can again calculate it once at the start of our sum:
+        long double logPMF = (long double)n * log((long double)(1.0 - p));
+      
+        // Because the first three terms of (2) will be zero for k=0,
+        // we can can calculate the CDF by taking the exponent of the constant term
+        long double cdfTotal = exp(logPMF);
 
-    // Loop through ks <= n 
-    for (unsigned int k = 0; k < n; k++) {
-        // If we have reached the CDF value we're looking for, return k
-        if(cdfTotal >= cdf) {
-            return k;
-        }
+	// Loop through ks <= n 
+	for (unsigned int k = 0; k < n; k++) {
+            // If we have reached the CDF value we're looking for, return k
+	    if(cdfTotal >= cdf) {
+                return k;
+	    }
 
-        // Binomiral coefficients can be calculated iteratively (for k from 0 to n) with:
-        // |  n  | =  n - k  |n|
-        // |k + 1|   ------- |k|
-        //            k + 1
+	    // Binomiral coefficients can be calculated iteratively (for k from 0 to n) with:
+	    // |  n  | =  n - k  |n|
+	    // |k + 1|   ------- |k|
+	    //            k + 1
+	    
+	    // This can, again, be moved to the log domain:
+	    // log|  n  | = log(n - k) - log(k + 1) + log| n |
+	    //    |k + 1|                                | k |
 
-        // This can, again, be moved to the log domain:
-        // log|  n  | = log(n - k) - log(k + 1) + log| n |
-        //    |k + 1|                                | k |
-
-        // So we can update our log PMF for the next k by adding 
-        // these log terms as well as the one pre-calculated earlier
-        logPMF += log((long double)(n - k)) - log((long double)(k + 1)) + logProbRatio;
-
-        // Add the exponent of the updated PMF to the CDF total
-        cdfTotal += exp(logPMF);
+	    // So we can update our log PMF for the next k by adding 
+	    // these log terms as well as the one pre-calculated earlier
+	    logPMF += log((long double)(n - k)) - log((long double)(k + 1)) + logProbRatio;
+	    
+	    // Add the exponent of the updated PMF to the CDF total
+	    cdfTotal += exp(logPMF);
+	}
+	return n;
     }
-    return n;
+    else {
+        // same approach as above but counting down from high k
+        cdf= 1.0-cdf;
+        long double logPMF = (long double)n * log((long double)p);
+	
+        // Because the first three terms of (2) will be zero for k=0,
+        // we can can calculate the CDF by taking the exponent of the constant term
+	long double cdfTotal = exp(logPMF);
+	
+        // Loop through ks >= 0 
+        for (unsigned int k = n-1; k >= 0; k--) {
+            // If we have reached the CDF value we're looking for, return k
+            if(cdfTotal > cdf) {
+                return k+1;
+	    }
+
+            // Binomiral coefficients can be calculated iteratively (for k from n-1 down to 0) with:
+            // | n | =  k + 1  |   n  |
+	    // | k |   ------- | k + 1|
+	    //          n - k
+	    
+	    // This can, again, be moved to the log domain:
+	    // log| n | = log(k + 1) - log(n - k) + log|   n   |
+	    //    | k |                                | k + 1 |
+	    
+	    // So we can update our log PMF for the previous k by adding 
+	    // these log terms as well as the one pre-calculated earlier
+	    logPMF += log((long double)(k + 1)) - log((long double)(n - k)) - logProbRatio;
+
+	    // Add the exponent of the updated PMF to the CDF total
+	    cdfTotal += exp(logPMF);
+	}
+	return 0;
+    }
 }
    
