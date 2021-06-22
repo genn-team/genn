2dd1407b06ced3487c760aa4fd616a7047a51106
neworderofjamie <neworderofjamie@gmail.com>
2021-06-22 17:03:26 +0100

    simplified tests to only handle single hash

--- tests/unit/modelSpecMerged.cc
+++ tests/unit/modelSpecMerged.cc
@@ -17,23 +17,14 @@
 //--------------------------------------------------------------------------
 namespace
 {
-enum Module
-{
-    ModuleNeuronUpdate,
-    ModuleSynapseUpdate,
-    ModuleCustomUpdate,
-    ModuleInit,
-    ModuleMax,
-};
-
 template<typename T, typename M, size_t N>
-void test(const std::pair<T, std::array<bool, ModuleMax>> (&modelModifiers)[N], M applyModifierFn)
+void test(const std::pair<T, bool> (&modelModifiers)[N], M applyModifierFn)
 {
     // Create a backend
     CodeGenerator::SingleThreadedCPU::Preferences preferences;
 
     // Create array of module digests
-    boost::uuids::detail::sha1::digest_type moduleHash[ModuleMax][N];
+    boost::uuids::detail::sha1::digest_type moduleHash[N];
 
     // Loop through
     for(size_t i = 0; i < N; i++) {
@@ -59,19 +50,13 @@ void test(const std::pair<T, std::array<bool, ModuleMax>> (&modelModifiers)[N], 
          // Merge model
         CodeGenerator::ModelSpecMerged modelSpecMerged(model, backend);
 
-        // Write hash digests of model modules to arrays
-        moduleHash[ModuleNeuronUpdate][i] = modelSpecMerged.getNeuronUpdateHashDigest();
-        moduleHash[ModuleSynapseUpdate][i] = modelSpecMerged.getSynapseUpdateHashDigest();
-        moduleHash[ModuleCustomUpdate][i] = modelSpecMerged.getCustomUpdateHashDigest();
-        moduleHash[ModuleInit][i] = modelSpecMerged.getInitHashDigest();
+        // Write hash digests of model to array
+        moduleHash[i] = modelSpecMerged.getHashDigest();
     }
 
     // Loop through modified models
     for(size_t i = 1; i < N; i++) {
-        // Loop through modules and check that the hashes compare in the expected way to the baseline
-        for(size_t m = 0; m < ModuleMax; m++) {
-            ASSERT_TRUE((moduleHash[m][i] == moduleHash[m][0]) == modelModifiers[i].second[m]);
-        }
+        ASSERT_TRUE((moduleHash[i] == moduleHash[0]) == modelModifiers[i].second);
     }
 }
 }   // Anonymous namespace
@@ -81,16 +66,16 @@ void test(const std::pair<T, std::array<bool, ModuleMax>> (&modelModifiers)[N], 
 TEST(ModelSpecMerged, CompareModelChanges)
 {
     // Make array of functions to modify default model and flags determining whether the hashes should match baseline
-    const std::pair<std::function<void(ModelSpecInternal &)>, std::array<bool, ModuleMax>> modelModifiers[] = {
-        {nullptr, {}},
-        {nullptr, {true, true, true, true}},
-        {[](ModelSpecInternal &model) { model.setName("interesting_name"); }, {false, false, false, false}},
-        {[](ModelSpecInternal &model) { model.setDT(1.0); }, {false, false, false, false}},
-        {[](ModelSpecInternal &model) { model.setTiming(true); }, {false, false, false, false}},
-        {[](ModelSpecInternal &model) { model.setPrecision(GENN_DOUBLE); }, {false, false, false, false}},
-        {[](ModelSpecInternal &model) { model.setTimePrecision(TimePrecision::DOUBLE); }, {false, false, false, false}},
-        {[](ModelSpecInternal &model) { model.setBatchSize(10); }, {false, false, false, false}},
-        {[](ModelSpecInternal &model) { model.setSeed(1234); }, {true, true, true, false}}};
+    const std::pair<std::function<void(ModelSpecInternal &)>, bool> modelModifiers[] = {
+        {nullptr, true},
+        {nullptr, true},
+        {[](ModelSpecInternal &model) { model.setName("interesting_name"); }, false},
+        {[](ModelSpecInternal &model) { model.setDT(1.0); }, false},
+        {[](ModelSpecInternal &model) { model.setTiming(true); }, false},
+        {[](ModelSpecInternal &model) { model.setPrecision(GENN_DOUBLE); }, false},
+        {[](ModelSpecInternal &model) { model.setTimePrecision(TimePrecision::DOUBLE); }, false},
+        {[](ModelSpecInternal &model) { model.setBatchSize(10); }, false},
+        {[](ModelSpecInternal &model) { model.setSeed(1234); }, true}};
     
     test(modelModifiers, 
          [](std::function<void(ModelSpecInternal &)> modifier, ModelSpecInternal &model)
@@ -104,14 +89,14 @@ TEST(ModelSpecMerged, CompareModelChanges)
 TEST(ModelSpecMerged, CompareNeuronPopSizeChanges)
 {
     // Make array of population size to build model with and flags determining whether the hashes should match baseline
-    const std::pair<std::vector<unsigned int>, std::array<bool, ModuleMax>> populationSizes[] = {
-        {{10, 50},      {}},
-        {{10, 50},      {true, true, true, true}},
-        {{50, 10},      {false, true, true, false}},
-        {{20, 20},      {false, true, true, false}},
-        {{},            {false, true, true, false}},
-        {{10, 20, 30},  {false, true, true, false}},
-        {{20},          {false, true, true, false}}};
+    const std::pair<std::vector<unsigned int>, bool> populationSizes[] = {
+        {{10, 50},      true},
+        {{10, 50},      true},
+        {{50, 10},      false},
+        {{20, 20},      false},
+        {{},            false},
+        {{10, 20, 30},  false},
+        {{20},          false}};
     
     test(populationSizes, 
          [](const std::vector<unsigned int> &popSizes, ModelSpecInternal &model)
@@ -136,13 +121,13 @@ TEST(ModelSpecMerged, CompareNeuronParamChanges)
     const std::array<double, 4> paramVals3{0.02, 0.2, -50.0, 2.0};
 
     // Make array of population parameters to build model with and flags determining whether the hashes should match baseline
-    const std::pair<std::vector<std::array<double, 4>>, std::array<bool, ModuleMax>> populationSizes[] = {
-        {{paramVals1, paramVals2},  {}},
-        {{paramVals1, paramVals2},  {true, true, true, true}},
-        {{paramVals1, paramVals1},  {false, true, true, true}},
-        {{paramVals2, paramVals3},  {false, true, true, true}},
-        {{},                        {false, true, true, false}},
-        {{paramVals1},              {false, true, true, false}}};
+    const std::pair<std::vector<std::array<double, 4>>, bool> populationSizes[] = {
+        {{paramVals1, paramVals2},  true},
+        {{paramVals1, paramVals2},  true},
+        {{paramVals1, paramVals1},  false},
+        {{paramVals2, paramVals3},  false},
+        {{},                        false},
+        {{paramVals1},              false}};
 
     test(populationSizes, 
          [](const std::vector<std::array<double, 4>> &popParams, ModelSpecInternal &model)
@@ -169,13 +154,13 @@ TEST(ModelSpecMerged, CompareNeuronVarInitChanges)
     const std::array<double, 2> varInit2{30.0, 0.0};
 
     // Make array of population variable initialiesers to build model with and flags determining whether the hashes should match baseline
-    const std::pair<std::vector<std::array<double, 2>>, std::array<bool, ModuleMax>> populationSizes[] = {
-        {{varInit1, varInit2},  {}},
-        {{varInit1, varInit2},  {true, true, true, true}},
-        {{varInit2, varInit1},  {true, true, true, false}},
-        {{varInit2},            {false, true, true, false}},
-        {{},                    {false, true, true, false}},
-        {{varInit1},            {false, true, true, false}}};
+    const std::pair<std::vector<std::array<double, 2>>, bool> populationSizes[] = {
+        {{varInit1, varInit2},  true},
+        {{varInit1, varInit2},  true},
+        {{varInit2, varInit1},  false},
+        {{varInit2},            false},
+        {{},                    false},
+        {{varInit1},            false}};
 
     test(populationSizes, 
          [](const std::vector<std::array<double, 2>> &popVarInit, ModelSpecInternal &model)
