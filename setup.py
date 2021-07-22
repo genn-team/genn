2fca3ee53c9c7e343dd7663a6d85d13baeb6da61
neworderofjamie <neworderofjamie@gmail.com>
2021-07-22 10:21:54 +0100

    release notes for 4.5.1 and updated versions

--- setup.py
+++ setup.py
@@ -178,7 +178,7 @@ for filename, namespace, kwargs in backends:
                                  **backend_extension_kwargs))
 
 setup(name = "pygenn",
-      version = "0.4.5",
+      version = "0.4.6",
       packages = find_packages(),
       package_data={"pygenn": package_data},
 
