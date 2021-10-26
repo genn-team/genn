07e9840dd5832938ccf8c9754b004dd7e59b1364
neworderofjamie <neworderofjamie@gmail.com>
2021-10-26 13:53:10 +0100

    expose some more bits of PyGeNN at module level

--- pygenn/__init__.py
+++ pygenn/__init__.py
@@ -1,3 +1,3 @@
 # Import pygenn interface
-from .genn_groups import SynapseGroup, NeuronGroup, CurrentSource
+from .genn_groups import SynapseGroup, NeuronGroup, CurrentSource, CustomUpdate
 from .genn_model import GeNNModel
