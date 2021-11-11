6cb325540862b0c14f565932165bad0dc10ae0d8
neworderofjamie <neworderofjamie@gmail.com>
2021-11-11 10:44:58 +0000

    doxygen tag

--- include/genn/genn/code_generator/groupMerged.h
+++ include/genn/genn/code_generator/groupMerged.h
@@ -1041,7 +1041,7 @@ public:
     //! Get expression for kernel size in dimension (may be literal or group->kernelSizeXXX)
     std::string getKernelSize(size_t dimensionIndex) const;
     
-    // Generate an index into a kernel based on the id_kernel_XXX variables in subs
+    //! Generate an index into a kernel based on the id_kernel_XXX variables in subs
     void genKernelIndex(std::ostream &os, const CodeGenerator::Substitutions &subs) const;
 
     std::string getPreSlot(unsigned int batchSize) const;
