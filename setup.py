aaf036b57a076cacf2700f13a1ece97190292b15
neworderofjamie <neworderofjamie@gmail.com>
2022-01-05 15:09:58 +0000

    more setup.py wrangling

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
 import os
 import sys
+from platform import system
 
 from pybind11.setup_helpers import Pybind11Extension, build_ext, WIN
 from setuptools import find_packages, setup
+
+linux = system() == "Linux"
 
 # Determine correct suffix for GeNN libraries
 if WIN:
@@ -18,17 +21,27 @@ genn_wrapper_src = os.path.join(genn_wrapper_path, "src")
 genn_include = os.path.join(genn_path, "include", "genn", "genn")
 genn_third_party_include = os.path.join(genn_path, "include", "genn", "third_party")
 
+extension_kwargs = {
+    "include_dirs": [genn_include, genn_third_party_include],
+    "library_dirs": [genn_wrapper_path],
+    "libraries": ["genn" + genn_lib_suffix],
+    "define_macros": [("LINKING_GENN_DLL", "1"), ("LINKING_BACKEND_DLL", "1")],
+    "cxx_std": 17,
+    }
+
+# On Linux, we want to add extension directory i.e. $ORIGIN to runtime
+# directories so libGeNN and backends can be found wherever package is installed
+if linux:
+    extension_kwargs["runtime_library_dirs"] = ["$ORIGIN"]
+
 ext_modules = [
     Pybind11Extension("genn",
         [os.path.join(genn_wrapper_src, "genn.cc")],
-        include_dirs=[genn_include, genn_third_party_include],
-        library_dirs=[genn_wrapper_path],
-        libraries=["genn" + genn_lib_suffix],
-        cxx_std=17)]
+        **extension_kwargs)]
     
     
 setup(
-    name="pygenn2",
+    name="pygenn",
     version="0.0.1",
     packages = find_packages(),
     url="https://github.com/genn_team/genn",
