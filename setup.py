f3cc98fb7b384ff89fabd0881db86cb5fa661299
neworderofjamie <neworderofjamie@gmail.com>
2022-01-12 20:09:21 +0000

    build pybind11 extensions within top-level PyGeNN package

--- setup.py
+++ setup.py
@@ -6,6 +6,10 @@ from platform import system
 from pybind11.setup_helpers import Pybind11Extension, build_ext, WIN, MACOS
 from setuptools import find_packages, setup
 
+# Determine is this is a debug build
+# **YUCK** this is not a great test
+debug_build = "--debug" in sys.argv
+
 # Get CUDA path from environment variable - setting this up is a required CUDA post-install step
 cuda_path = os.environ.get("CUDA_PATH")
 
@@ -24,23 +28,22 @@ LINUX = system() == "Linux"
 
 # Determine correct suffix for GeNN libraries
 if WIN:
-    genn_lib_suffix = "_Release_DLL"# "_Debug_DLL" if debug_build else "_Release_DLL"
+    genn_lib_suffix = "_Debug_DLL" if debug_build else "_Release_DLL"
 else:
-    genn_lib_suffix = "_dynamic"#"_dynamic_debug" if debug_build else "_dynamic"
+    genn_lib_suffix = "_dynamic_debug" if debug_build else "_dynamic"
 
 genn_path = os.path.dirname(os.path.abspath(__file__))
 
 pygenn_path = os.path.join(genn_path, "pygenn")
-genn_wrapper_path = os.path.join(pygenn_path, "genn_wrapper")
-genn_wrapper_src = os.path.join(genn_wrapper_path, "src")
-genn_wrapper_include = os.path.join(genn_wrapper_path, "include")
+pygenn_src = os.path.join(pygenn_path, "src")
+pygenn_include = os.path.join(pygenn_path, "include")
 genn_include = os.path.join(genn_path, "include", "genn", "genn")
 genn_third_party_include = os.path.join(genn_path, "include", "genn", "third_party")
 
 # Define standard kwargs for building all extensions
 extension_kwargs = {
-    "include_dirs": [genn_wrapper_include],
-    "library_dirs": [genn_wrapper_path],
+    "include_dirs": [pygenn_include],
+    "library_dirs": [pygenn_path],
     "cxx_std": 17,
     "extra_link_args": []}
 
@@ -56,8 +59,8 @@ genn_extension_kwargs["libraries"] = ["genn" + genn_lib_suffix]
 genn_extension_kwargs["define_macros"] = [("LINKING_GENN_DLL", "1"), ("LINKING_BACKEND_DLL", "1")]
 
 # Always package LibGeNN
-package_data = ["genn_wrapper/genn" + genn_lib_suffix + ".*" if WIN 
-                else "genn_wrapper/libgenn" + genn_lib_suffix + ".*"]
+package_data = ["genn" + genn_lib_suffix + ".*" if WIN 
+                else "libgenn" + genn_lib_suffix + ".*"]
 
 # On Linux, we want to add extension directory i.e. $ORIGIN to runtime
 # directories so libGeNN and backends can be found wherever package is installed
@@ -87,31 +90,31 @@ if cuda_installed:
 
 ext_modules = [
     Pybind11Extension("shared_library_model",
-                      [os.path.join(genn_wrapper_src, "sharedLibraryModel.cc")],
+                      [os.path.join(pygenn_src, "sharedLibraryModel.cc")],
                       **extension_kwargs),
     Pybind11Extension("genn",
-                      [os.path.join(genn_wrapper_src, "genn.cc")],
+                      [os.path.join(pygenn_src, "genn.cc")],
                       **genn_extension_kwargs),
     Pybind11Extension("init_sparse_connectivity_snippets",
-                      [os.path.join(genn_wrapper_src, "initSparseConnectivitySnippets.cc")],
+                      [os.path.join(pygenn_src, "initSparseConnectivitySnippets.cc")],
                       **genn_extension_kwargs),
     Pybind11Extension("init_toeplitz_connectivity_snippets",
-                      [os.path.join(genn_wrapper_src, "initToeplitzConnectivitySnippets.cc")],
+                      [os.path.join(pygenn_src, "initToeplitzConnectivitySnippets.cc")],
                       **genn_extension_kwargs),
     Pybind11Extension("init_var_snippets",
-                      [os.path.join(genn_wrapper_src, "initVarSnippets.cc")],
+                      [os.path.join(pygenn_src, "initVarSnippets.cc")],
                       **genn_extension_kwargs),
     Pybind11Extension("current_source_models",
-                      [os.path.join(genn_wrapper_src, "currentSourceModels.cc")],
+                      [os.path.join(pygenn_src, "currentSourceModels.cc")],
                       **genn_extension_kwargs),
     Pybind11Extension("neuron_models",
-                      [os.path.join(genn_wrapper_src, "neuronModels.cc")],
+                      [os.path.join(pygenn_src, "neuronModels.cc")],
                       **genn_extension_kwargs),
     Pybind11Extension("postsynaptic_models",
-                      [os.path.join(genn_wrapper_src, "postsynapticModels.cc")],
+                      [os.path.join(pygenn_src, "postsynapticModels.cc")],
                       **genn_extension_kwargs),
     Pybind11Extension("weight_update_models",
-                      [os.path.join(genn_wrapper_src, "weightUpdateModels.cc")],
+                      [os.path.join(pygenn_src, "weightUpdateModels.cc")],
                       **genn_extension_kwargs)]
     
  # Loop through namespaces of supported backends
@@ -126,9 +129,9 @@ for filename, namespace, kwargs in backends:
     # Add relocatable version of backend library to libraries
     # **NOTE** this is added BEFORE libGeNN as this library needs symbols FROM libGeNN
     if WIN:
-        package_data.append("genn_wrapper/genn_" + filename + "_backend" + genn_lib_suffix + ".*")
+        package_data.append("genn_" + filename + "_backend" + genn_lib_suffix + ".*")
     else:
-        package_data.append("genn_wrapper/libgenn_" + filename + "_backend" + genn_lib_suffix + ".*")
+        package_data.append("libgenn_" + filename + "_backend" + genn_lib_suffix + ".*")
 
     # Add backend include directory to both SWIG and C++ compiler options
     backend_include_dir = os.path.join(genn_path, "include", "genn", "backends", filename)
@@ -137,7 +140,7 @@ for filename, namespace, kwargs in backends:
 
     # Add extension to list
     ext_modules.append(Pybind11Extension(filename + "_backend", 
-                                         [os.path.join(genn_wrapper_src, filename + "Backend.cc")],
+                                         [os.path.join(pygenn_src, filename + "Backend.cc")],
                                          **backend_extension_kwargs))
 
 setup(
@@ -147,7 +150,7 @@ setup(
     package_data={"pygenn": package_data},
 
     url="https://github.com/genn_team/genn",
-    ext_package="pygenn.genn_wrapper",
+    ext_package="pygenn",
     ext_modules=ext_modules,
     zip_safe=False,
     python_requires=">=3.6",
