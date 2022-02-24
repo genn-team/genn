f7b2a752ce36345eeef340d0f5aa63956b365d0a
neworderofjamie <neworderofjamie@gmail.com>
2022-02-24 11:22:43 +0000

    expose more stuff at module-level to save typing

--- pygenn/__init__.py
+++ pygenn/__init__.py
@@ -6,7 +6,14 @@ from .genn import (create_var_ref, create_psm_var_ref, create_wu_pre_var_ref,
                    create_wu_post_var_ref, create_wu_var_ref, PlogSeverity, 
                    ScalarPrecision, SpanType, SynapseMatrixType, TimePrecision,
                    VarAccess, VarAccessMode, VarLocation)
-from .genn_model import (GeNNModel, init_sparse_connectivity, 
+from .genn_model import (GeNNModel, create_custom_neuron_class,
+                         create_custom_postsynaptic_class, 
+                         create_custom_weight_update_class,
+                         create_custom_current_source_class,
+                         create_custom_custom_update_class,
+                         create_custom_init_var_snippet_class,
+                         create_custom_sparse_connect_init_snippet_class,
+                         init_sparse_connectivity, 
                          init_toeplitz_connectivity, init_var)
 
 if sys.version_info >= (3, 8):
