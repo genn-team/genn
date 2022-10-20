3fb1e5460652c4be6fd2978665bae14def377945
neworderofjamie <neworderofjamie@gmail.com>
2022-10-20 12:22:35 +0100

    framework for first custom connectivity update feature test

---
+++ tests/features/custom_connectivity_update_remove/model.cc
@@ -0,0 +1,101 @@
+//--------------------------------------------------------------------------
+/*! \file custom_connectivity_update_remove/model.cc
+
+\brief model definition file that is part of the feature testing
+suite of minimal models with known analytic outcomes that are used for continuous integration testing.
+*/
+//--------------------------------------------------------------------------
+
+
+#include "modelSpec.h"
+// 10 pre, 10 post neurons
+// Triangle connectivity i.e. pre connected to all subsequent post
+// Initialise weights to  postsynaptic index * (presynaptic index + 1)
+// Initialise custom connectivity update variable to something else
+// connectivity update which removes first synapse in each row
+
+// Initialise model
+// Launch custom update
+// pull connectivity, state variables et
+// check all synapse variables and connectivity match expectations
+
+class TestNeuron : public NeuronModels::Base
+{
+public:
+    DECLARE_MODEL(TestNeuron, 0, 1);
+
+    SET_VARS({{"V","scalar"}});
+};
+IMPLEMENT_MODEL(TestNeuron);
+
+class TestWUM : public WeightUpdateModels::Base
+{
+public:
+    DECLARE_WEIGHT_UPDATE_MODEL(TestWUM, 0, 2, 0, 0);
+
+    SET_VARS({{"g", "scalar", VarAccess::READ_ONLY},
+              {"d", "uint8_t", VarAccess::READ_ONLY}});
+};
+IMPLEMENT_MODEL(TestWUM);
+
+class Weight : public InitVarSnippet::Base
+{
+    DECLARE_SNIPPET(Weight, 0);
+    
+    SET_CODE("$(value) = ($(id_pre) * 100) + $(id_post);");
+};
+IMPLEMENT_SNIPPET(Weight);
+
+class Delay : public InitVarSnippet::Base
+{
+    DECLARE_SNIPPET(Delay, 0);
+    
+    SET_CODE("$(value) = ($(id_post) * 100) + $(id_pre);");
+};
+IMPLEMENT_SNIPPET(Delay);
+
+class Triangle : public InitSparseConnectivitySnippet::Base
+{
+public:
+    DECLARE_SNIPPET(Triangle, 0);
+
+    SET_ROW_BUILD_CODE(
+        "if(j > $(num_post)) {\n"
+        "   if(j > $(id_pre)) {\n"
+        "       $(addSynapse, j);\n"
+        "   }\n"
+        "}\n"
+        "else {\n"
+        "   $(endRow);\n"
+        "}\n"
+        "j++;\n");
+    SET_ROW_BUILD_STATE_VARS({{"j", "unsigned int", 0}});
+};
+IMPLEMENT_SNIPPET(Triangle);
+
+void modelDefinition(ModelSpec &model)
+{
+#ifdef CL_HPP_TARGET_OPENCL_VERSION
+    if(std::getenv("OPENCL_DEVICE") != nullptr) {
+        GENN_PREFERENCES.deviceSelectMethod = DeviceSelect::MANUAL;
+        GENN_PREFERENCES.manualDeviceID = std::atoi(std::getenv("OPENCL_DEVICE"));
+    }
+    if(std::getenv("OPENCL_PLATFORM") != nullptr) {
+        GENN_PREFERENCES.manualPlatformID = std::atoi(std::getenv("OPENCL_PLATFORM"));
+    }
+#endif
+    model.setDT(1.0);
+    model.setName("custom_connectivity_update_remove");
+    
+    model.addNeuronPopulation<NeuronModels::SpikeSource>("SpikeSource", 100, {}, {});
+    model.addNeuronPopulation<TestNeuron>("Neuron", 100, {}, {0.0});
+    
+    TestWUM::VarValues testWUMInit(initVar<Weight>(), initVar<Delay>());
+    model.addSynapsePopulation<TestWUM, PostsynapticModels::DeltaCurr>(
+        "Syn", SynapseMatrixType::SPARSE_INDIVIDUALG, NO_DELAY, "SpikeSource", "Neuron",
+        {}, testWUMInit,
+        {}, {},
+        initConnectivity<Triangle>({}));
+
+    model.setPrecision(GENN_FLOAT);
+}
---
+++ tests/features/custom_connectivity_update_remove/test.cc
@@ -0,0 +1,29 @@
+//--------------------------------------------------------------------------
+/*! \file custom_connectivity_update_remove/test.cc
+
+\brief Main test code that is part of the feature testing
+suite of minimal models with known analytic outcomes that are used for continuous integration testing.
+*/
+//--------------------------------------------------------------------------
+
+
+// Google test includes
+#include "gtest/gtest.h"
+
+// Auto-generated simulation code includess
+#include "custom_connectivity_update_remove_CODE/definitions.h"
+
+// **NOTE** base-class for simulation tests must be
+// included after auto-generated globals are includes
+#include "../../utils/simulation_test.h"
+
+//----------------------------------------------------------------------------
+// SimTest
+//----------------------------------------------------------------------------
+class SimTest : public SimulationTest
+{
+};
+
+TEST_F(SimTest, CustomConnectivityUpdateRemove)
+{
+}
