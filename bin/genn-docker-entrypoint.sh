d46ea77da718922c5ae170eb95d5baadc6c29e9c
neworderofjamie <neworderofjamie@gmail.com>
2022-10-26 17:09:49 +0100

    no need for bash wrapper to jupyter-notebook

--- bin/genn-docker-entrypoint.sh
+++ bin/genn-docker-entrypoint.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
-# Run commands in the Docker container with a particular UID and GID.
-# The idea is to run the container like
-#   docker run -i \
-#     -v `pwd`:/work \
-#     -e LOCAL_USER_ID=`id -u $USER` \
-#     -e LOCAL_GROUP_ID=`id -g $USER` \
-#     image-name bash
-# where the -e flags pass the env vars to the container, which are read by this script.
-# By setting copying this script to the container and setting it to the
-# ENTRYPOINT, and subsequent commands run in the container will run as the user
-# who ran `docker` on the host, and so any output files will have the correct
-# permissions.
-
+# Read desired user and group ID from environment varibles (typically set on docker command line with -e)
 USER_ID=${LOCAL_USER_ID:-9001}
 GROUP_ID=${LOCAL_GROUP_ID:-$USER_ID}
 
@@ -34,7 +22,7 @@ if [[ "$1" = "script" ]]; then
 elif [[ "$1" = "notebook" ]]; then
     # Extract working directory from next command line argument, otherwise use home
     CWD=${2:-$HOME}
-    exec gosu genn:genn /bin/bash -c "/usr/local/bin/jupyter-notebook --ip=0.0.0.0 --port=8080 --no-browser --notebook-dir=\"$CWD\""
+    exec gosu genn:genn /usr/local/bin/jupyter-notebook --ip=0.0.0.0 --port=8080 --no-browser --notebook-dir="$CWD"
 # Otherwise, execute arguments
 else
     exec gosu genn:genn "$@"
