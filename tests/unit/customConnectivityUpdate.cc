bd62df8ddf748da720ff54bcbdd7db7be528ac62
neworderofjamie <neworderofjamie@gmail.com>
2022-11-07 11:36:24 +0000

    completed dependent variable unit test implementation

--- tests/unit/customConnectivityUpdate.cc
+++ tests/unit/customConnectivityUpdate.cc
@@ -57,7 +57,16 @@ public:
         "});\n");
 };
 IMPLEMENT_MODEL(RemoveSynapseVarRef);
+
+bool hasVarRef(const std::vector<Models::WUVarReference> &varRefs, const std::string &targetName, const std::string &varName)
+{
+    return std::find_if(varRefs.cbegin(), varRefs.cend(), 
+                        [&targetName, &varName](const Models::WUVarReference &r)
+                        { 
+                            return (r.getTargetName() == targetName) && (r.getVar().name == varName);
+                        }) != varRefs.cend();
 }
+}   // Anonymous namespace
 //--------------------------------------------------------------------------
 // Tests
 //--------------------------------------------------------------------------
@@ -72,7 +81,7 @@ TEST(CustomConnectivityUpdate, DependentVariables)
     model.addNeuronPopulation<NeuronModels::Izhikevich>("Post", 10, paramVals, varVals);
 
     // Create synapse group with global weights
-    auto *sg1 = model.addSynapsePopulation<WeightUpdateModels::StaticPulse, PostsynapticModels::DeltaCurr>(
+    model.addSynapsePopulation<WeightUpdateModels::StaticPulse, PostsynapticModels::DeltaCurr>(
         "Synapses1", SynapseMatrixType::SPARSE_GLOBALG, NO_DELAY,
         "Pre", "Post",
         {}, {1.0},
@@ -84,7 +93,7 @@ TEST(CustomConnectivityUpdate, DependentVariables)
                                                                   {}, {}, {});
 
     // Create synapse group with individual weights
-    auto *sg2 = model.addSynapsePopulation<WeightUpdateModels::StaticPulse, PostsynapticModels::DeltaCurr>(
+    model.addSynapsePopulation<WeightUpdateModels::StaticPulse, PostsynapticModels::DeltaCurr>(
         "Synapses2", SynapseMatrixType::SPARSE_INDIVIDUALG, NO_DELAY,
         "Pre", "Post",
         {}, {1.0},
@@ -104,7 +113,7 @@ TEST(CustomConnectivityUpdate, DependentVariables)
 
     // Attach custom update model
     Sum::WUVarReferences cu3VarRefs(createWUVarRef(sg3, "g"));
-    auto *cu3 = model.addCustomUpdate<Sum>("CustomUpdate3", "Test1", {}, {0.0}, cu3VarRefs);
+    model.addCustomUpdate<Sum>("CustomUpdate3", "Test1", {}, {0.0}, cu3VarRefs);
 
     // Attach custom connectivity update
     auto *ccu3 = model.addCustomConnectivityUpdate<RemoveSynapse>("CustomConnectivityUpdate3", "Test2", "Synapses3",
@@ -112,16 +121,16 @@ TEST(CustomConnectivityUpdate, DependentVariables)
                                                                   {}, {}, {});
 
     // Create synapse group with individual weights
-    auto *sg4 = model.addSynapsePopulation<WeightUpdateModels::StaticPulse, PostsynapticModels::DeltaCurr>(
+    model.addSynapsePopulation<WeightUpdateModels::StaticPulse, PostsynapticModels::DeltaCurr>(
         "Synapses4", SynapseMatrixType::SPARSE_INDIVIDUALG, NO_DELAY,
         "Pre", "Post",
         {}, {1.0},
         {}, {});
 
     // Attach custom connectivity update
-    auto *ccu42 = model.addCustomConnectivityUpdate<RemoveSynapse>("CustomConnectivityUpdate42", "Test2", "Synapses4",
-                                                                   {}, {1.0}, {}, {},
-                                                                   {}, {}, {});
+    model.addCustomConnectivityUpdate<RemoveSynapse>("CustomConnectivityUpdate42", "Test2", "Synapses4",
+                                                     {}, {1.0}, {}, {},
+                                                     {}, {}, {});
 
     // Attach custom connectivity update
     auto *ccu4 = model.addCustomConnectivityUpdate<RemoveSynapse>("CustomConnectivityUpdate4", "Test1", "Synapses4",
@@ -137,36 +146,19 @@ TEST(CustomConnectivityUpdate, DependentVariables)
     // Check dependencies for CCU2
     auto ccu2DependentVars = static_cast<CustomConnectivityUpdateInternal*>(ccu2)->getDependentVariables();
     ASSERT_EQ(ccu2DependentVars.size(), 1);
-    ASSERT_EQ(ccu2DependentVars[0].getTargetName(), "Synapses2");
-    ASSERT_EQ(ccu2DependentVars[0].getVar().name, "g");
+    ASSERT_TRUE(hasVarRef(ccu2DependentVars, "Synapses2", "g"));
    
     // Check dependencies for CCU3
     auto ccu3DependentVars = static_cast<CustomConnectivityUpdateInternal*>(ccu3)->getDependentVariables();
     ASSERT_EQ(ccu3DependentVars.size(), 2);
-    ASSERT_TRUE(std::find_if(ccu3DependentVars.cbegin(), ccu3DependentVars.cend(), 
-                             [](const Models::WUVarReference &r)
-                             { 
-                                 return (r.getTargetName() == "Synapses3") && (r.getVar().name == "g");
-                             }) != ccu3DependentVars.cend());
-    ASSERT_TRUE(std::find_if(ccu3DependentVars.cbegin(), ccu3DependentVars.cend(), 
-                             [](const Models::WUVarReference &r)
-                             { 
-                                 return (r.getTargetName() == "CustomUpdate3") && (r.getVar().name == "sum");
-                             }) != ccu3DependentVars.cend());
+    ASSERT_TRUE(hasVarRef(ccu3DependentVars, "Synapses3", "g"));
+    ASSERT_TRUE(hasVarRef(ccu3DependentVars, "CustomUpdate3", "sum"));
 
     // Check dependencies for CCU4
     auto ccu4DependentVars = static_cast<CustomConnectivityUpdateInternal*>(ccu4)->getDependentVariables();
     ASSERT_EQ(ccu4DependentVars.size(), 2);
-    ASSERT_TRUE(std::find_if(ccu4DependentVars.cbegin(), ccu4DependentVars.cend(), 
-                             [](const Models::WUVarReference &r)
-                             { 
-                                 return (r.getTargetName() == "Synapses4") && (r.getVar().name == "g");
-                             }) != ccu4DependentVars.cend());
-    ASSERT_TRUE(std::find_if(ccu4DependentVars.cbegin(), ccu4DependentVars.cend(), 
-                             [](const Models::WUVarReference &r)
-                             { 
-                                 return (r.getTargetName() == "CustomConnectivityUpdate42") && (r.getVar().name == "a");
-                             }) != ccu4DependentVars.cend());
+    ASSERT_TRUE(hasVarRef(ccu4DependentVars, "Synapses4", "g"));
+    ASSERT_TRUE(hasVarRef(ccu4DependentVars, "CustomConnectivityUpdate42", "a"));
 }
 //--------------------------------------------------------------------------
 TEST(CustomConnectivityUpdate, DependentVariablesManualReferences)
@@ -222,23 +214,21 @@ TEST(CustomConnectivityUpdate, DependentVariablesManualReferences)
 
     model.finalize();
 
-    // Check dependencies for CCU12
+    // Check synapse group variable has been removed from CCU12 dependent variables as it's manually referenced
     auto ccu12DependentVars = static_cast<CustomConnectivityUpdateInternal*>(ccu12)->getDependentVariables();
-    
-    // Check dependencies for CCU12
+    ASSERT_EQ(ccu12DependentVars.size(), 2);
+    ASSERT_TRUE(hasVarRef(ccu12DependentVars, "CustomUpdate0", "sum"));
+    ASSERT_TRUE(hasVarRef(ccu12DependentVars, "CustomConnectivityUpdate0", "a"));
+                
+    // Check custom update variable has been removed from CCU22 dependent variables as it's manually referenced
     auto ccu22DependentVars = static_cast<CustomConnectivityUpdateInternal*>(ccu22)->getDependentVariables();
-
-    // Check dependencies for CCU12
-    auto ccu32DependentVars = static_cast<CustomConnectivityUpdateInternal*>(ccu32)->getDependentVariables();
-
-    while(false);
-    // Create model with 4 synapse groups, each with individualg, custom wu update, custom connectivity update
-
-    // Attach custom connectivity update to each one:
-    // 1) No var references
-    // 2) Var reference to synapse var
-    // 3) Var reference to custom update var
-    // 4) Var reference to custom connectivity update var
+    ASSERT_EQ(ccu22DependentVars.size(), 2);
+    ASSERT_TRUE(hasVarRef(ccu22DependentVars, "Synapses1", "g"));
+    ASSERT_TRUE(hasVarRef(ccu22DependentVars, "CustomConnectivityUpdate1", "a"));
     
-    // Then check that dependent variables contains everything expected
+    // Check custom connectivity update variable has been removed from CCU32 dependent variables as it's manually referenced
+    auto ccu32DependentVars = static_cast<CustomConnectivityUpdateInternal*>(ccu32)->getDependentVariables();
+    ASSERT_EQ(ccu32DependentVars.size(), 2);
+    ASSERT_TRUE(hasVarRef(ccu32DependentVars, "Synapses2", "g"));
+    ASSERT_TRUE(hasVarRef(ccu32DependentVars, "CustomUpdate2", "sum"));
 }
