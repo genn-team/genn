c9410753977e89f64d4c29a2ca9b9e17a2de2866
neworderofjamie <neworderofjamie@gmail.com>
2023-03-20 11:20:19 +0000

    free functions for creating EGP references

--- include/genn/genn/modelSpec.h
+++ include/genn/genn/modelSpec.h
@@ -177,6 +177,42 @@ inline Models::WUVarReference createWUVarRef(const CustomUpdateWU *cu, const std
     return Models::WUVarReference(cu, varName);
 }
 
+//! Creates a reference to a neuron group extra global parameter
+inline Models::EGPReference createEGPRef(const NeuronGroup *ng, const std::string &egpName)
+{
+    return Models::EGPReference::createEGPRef(ng, egpName);
+}
+
+//! Creates a reference to a current source extra global parameter
+inline Models::EGPReference createEGPRef(const CurrentSource *cs, const std::string &egpName)
+{
+    return Models::EGPReference::createEGPRef(cs, egpName);
+}
+
+//! Creates a reference to a custom update extra global parameter
+inline Models::EGPReference createEGPRef(const CustomUpdate *cu, const std::string &egpName)
+{
+    return Models::EGPReference::createEGPRef(cu, egpName);
+}
+
+//! Creates a reference to a custom weight update extra global parameter
+inline Models::EGPReference createEGPRef(const CustomUpdateWU *cu, const std::string &egpName)
+{
+    return Models::EGPReference::createEGPRef(cu, egpName);
+}
+
+//! Creates a reference to a postsynaptic model extra global parameter
+inline Models::EGPReference createPSMEGPRef(const SynapseGroup *sg, const std::string &egpName)
+{
+    return Models::EGPReference::createPSMEGPRef(sg, egpName);
+}
+
+//! Creates a reference to a weight update model extra global parameter
+inline Models::EGPReference createWUEGPRef(const SynapseGroup *sg, const std::string &egpName)
+{
+    return Models::EGPReference::createWUEGPRef(sg, egpName);
+}
+
 //----------------------------------------------------------------------------
 // ModelSpec
 //----------------------------------------------------------------------------
