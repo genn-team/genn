b82fb6c91d92bee5b51bd925473278f8fac57bea
neworderofjamie <neworderofjamie@gmail.com>
2023-06-30 11:59:31 +0100

    lazy string doesn't treat $(0) style parameter placeholders as things to be lazily evaluated - pretty printer handles these

--- src/genn/genn/code_generator/lazyString.cc
+++ src/genn/genn/code_generator/lazyString.cc
@@ -18,7 +18,8 @@ using namespace GeNN::CodeGenerator;
 LazyString::LazyString(const std::string &format, EnvironmentExternalBase &env)
 {
     // Create regex iterator to iterate over $(XXX) style varibles in format string
-    std::regex regex("\\$\\(([\\w]+)\\)");
+    // **NOTE** this doesn't match function argument $(0) 
+    std::regex regex("\\$\\(([a-zA-Z_][\\w]+)\\)");
     std::sregex_iterator matchesBegin(format.cbegin(), format.cend(), regex);
     std::sregex_iterator matchesEnd;
     
