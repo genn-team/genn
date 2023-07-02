b917b1be75e9d7b4bb8296d10594ed261b709280
neworderofjamie <neworderofjamie@gmail.com>
2023-07-02 11:42:51 +0100

    strip out unsused code gen utils

--- include/genn/genn/code_generator/codeGenUtils.h
+++ include/genn/genn/code_generator/codeGenUtils.h
@@ -37,35 +37,6 @@ class EnvironmentExternalBase;
 //--------------------------------------------------------------------------
 namespace GeNN::CodeGenerator
 {
-//--------------------------------------------------------------------------
-//! \brief Tool for substituting strings in the neuron code strings or other templates
-//--------------------------------------------------------------------------
-GENN_EXPORT void substitute(std::string &s, const std::string &trg, const std::string &rep);
-
-//--------------------------------------------------------------------------
-//! \brief Tool for substituting variable  names in the neuron code strings or other templates using regular expressions
-//--------------------------------------------------------------------------
-GENN_EXPORT bool regexVarSubstitute(std::string &s, const std::string &trg, const std::string &rep);
-
-//--------------------------------------------------------------------------
-//! \brief Tool for substituting function names in the neuron code strings or other templates using regular expressions
-//--------------------------------------------------------------------------
-GENN_EXPORT bool regexFuncSubstitute(std::string &s, const std::string &trg, const std::string &rep);
-
-//--------------------------------------------------------------------------
-/*! \brief This function substitutes function calls in the form:
- *
- *  $(functionName, parameter1, param2Function(0.12, "string"))
- *
- * with replacement templates in the form:
- *
- *  actualFunction(CONSTANT, $(0), $(1))
- *
- */
-//--------------------------------------------------------------------------
-GENN_EXPORT void functionSubstitute(std::string &code, const std::string &funcName,
-                                    unsigned int numParams, const std::string &replaceFuncTemplate);
-
 //! Divide two integers, rounding up i.e. effectively taking ceil
 inline size_t ceilDivide(size_t numerator, size_t denominator)
 {
@@ -79,18 +50,6 @@ inline size_t padSize(size_t size, size_t blockSize)
 }
 
 GENN_EXPORT void genTypeRange(CodeStream &os, const Type::ResolvedType &type, const std::string &prefix);
-
-//--------------------------------------------------------------------------
-/*! \brief This function implements a parser that converts any floating point constant in a code snippet to a floating point constant with an explicit precision (by appending "f" or removing it).
- */
-//--------------------------------------------------------------------------
-GENN_EXPORT std::string ensureFtype(const std::string &oldcode, const std::string &type);
-
-//--------------------------------------------------------------------------
-/*! \brief This function checks for unknown variable definitions and returns a gennError if any are found
- */
-//--------------------------------------------------------------------------
-GENN_EXPORT void checkUnreplacedVariables(const std::string &code, const std::string &codeName);
 
 //--------------------------------------------------------------------------
 /*! \brief This function substitutes function names in a code with namespace as prefix of the function name for backends that do not support namespaces by checking that the function indeed exists in the support code and returns the substituted code.
