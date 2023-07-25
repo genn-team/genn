1c4a429302934cfbbfa93088bc133b7da93a8401
neworderofjamie <neworderofjamie@gmail.com>
2023-07-25 21:12:19 +0100

    connectivity init test

---
+++ tests/features/test_connect_init.py
@@ -0,0 +1,58 @@
+import numpy as np
+import pytest
+from pygenn import types
+
+from pygenn import GeNNModel
+
+from pygenn import init_sparse_connectivity
+
+@pytest.mark.parametrize("backend", ["single_threaded_cpu", "cuda"])
+@pytest.mark.parametrize("precision", [types.Double, types.Float])
+def test_connect_init(backend, precision):
+    model = GeNNModel(precision, "test_connect_init", backend=backend)
+    model.narrow_sparse_ind_enabled = True
+    
+    # Create pre and postsynaptic neuron populations
+    pre_pop = model.add_neuron_population("Pre", 100, "SpikeSource", {}, {})
+    post_pop = model.add_neuron_population("Post", 100, "SpikeSource", {}, {})
+    
+    # Add synapse populations with different types of built-in connectivity
+    fixed_number_total_s_pop = model.add_synapse_population(
+        "FixedNumberTotal", "SPARSE", 0,
+        pre_pop, post_pop,
+        "StaticPulseConstantWeight", {"g": 1.0}, {}, {}, {},
+        "DeltaCurr", {}, {},
+        init_sparse_connectivity("FixedNumberTotalWithReplacement", {"total": 1000}))
+
+    fixed_number_pre_s_pop = model.add_synapse_population(
+        "FixedNumberPre", "SPARSE", 0,
+        pre_pop, post_pop,
+        "StaticPulseConstantWeight", {"g": 1.0}, {}, {}, {},
+        "DeltaCurr", {}, {},
+        init_sparse_connectivity("FixedNumberPreWithReplacement", {"colLength": 10}))
+    
+    fixed_number_post_s_pop = model.add_synapse_population(
+        "FixedNumberPost", "SPARSE", 0,
+        pre_pop, post_pop,
+        "StaticPulseConstantWeight", {"g": 1.0}, {}, {}, {},
+        "DeltaCurr", {}, {},
+        init_sparse_connectivity("FixedNumberPostWithReplacement", {"rowLength": 10}))
+
+    # Build and load model
+    model.build()
+    model.load()
+    
+    # Pull connectivity
+    fixed_number_total_s_pop.pull_connectivity_from_device()
+    fixed_number_pre_s_pop.pull_connectivity_from_device()
+    fixed_number_post_s_pop.pull_connectivity_from_device()
+    
+    # Check connectivity
+    assert np.all(np.bincount(fixed_number_post_s_pop.get_sparse_pre_inds()) == 10)
+    assert np.all(np.bincount(fixed_number_pre_s_pop.get_sparse_post_inds()) == 10)
+    assert len(fixed_number_total_s_pop.get_sparse_pre_inds()) == 1000
+
+    # **TODO** we could also build a histogram of postsynaptic neurons and check that they are approximately uniformly distributed
+
+if __name__ == '__main__':
+    test_connect_init("single_threaded_cpu", types.Float)
