f4aca434a6c4cbec2b28228def3466bb5ffc55fc
neworderofjamie <neworderofjamie@gmail.com>
2023-07-27 16:22:39 +0100

    re-enable fixed number total connectivity test

--- tests/features/test_connect_init.py
+++ tests/features/test_connect_init.py
@@ -18,12 +18,12 @@ def test_connect_init(backend, precision):
     post_pop = model.add_neuron_population("Post", 100, "SpikeSource", {}, {})
     
     # Add synapse populations with different types of built-in connectivity
-    #fixed_number_total_s_pop = model.add_synapse_population(
-    #    "FixedNumberTotal", "SPARSE", 0,
-    #    pre_pop, post_pop,
-    #    "StaticPulseConstantWeight", {"g": 1.0}, {}, {}, {},
-    #    "DeltaCurr", {}, {},
-    #    init_sparse_connectivity("FixedNumberTotalWithReplacement", {"total": 1000}))
+    fixed_number_total_s_pop = model.add_synapse_population(
+        "FixedNumberTotal", "SPARSE", 0,
+        pre_pop, post_pop,
+        "StaticPulseConstantWeight", {"g": 1.0}, {}, {}, {},
+        "DeltaCurr", {}, {},
+        init_sparse_connectivity("FixedNumberTotalWithReplacement", {"total": 1000}))
 
     fixed_number_pre_s_pop = model.add_synapse_population(
         "FixedNumberPre", "SPARSE", 0,
@@ -44,14 +44,14 @@ def test_connect_init(backend, precision):
     model.load()
     
     # Pull connectivity
-    #fixed_number_total_s_pop.pull_connectivity_from_device()
+    fixed_number_total_s_pop.pull_connectivity_from_device()
     fixed_number_pre_s_pop.pull_connectivity_from_device()
     fixed_number_post_s_pop.pull_connectivity_from_device()
     
     # Check connectivity
     assert np.all(np.bincount(fixed_number_post_s_pop.get_sparse_pre_inds()) == 10)
     assert np.all(np.bincount(fixed_number_pre_s_pop.get_sparse_post_inds()) == 10)
-    #assert len(fixed_number_total_s_pop.get_sparse_pre_inds()) == 1000
+    assert len(fixed_number_total_s_pop.get_sparse_pre_inds()) == 1000
 
     # Check neurons are uniformly distributed within each row/column
     assert stats.chisquare(np.bincount(fixed_number_post_s_pop.get_sparse_post_inds(), minlength=100)).pvalue > 0.05
