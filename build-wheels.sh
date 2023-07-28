340e5b8306af336254e3d862f4100347a0b76e5a
Ben Evans <ben.d.evans@gmail.com>
2023-07-28 12:57:01 +0100

    No need to make install

--- build-wheels.sh
+++ build-wheels.sh
@@ -27,7 +27,7 @@ for PYBIN in /opt/python/*/bin; do
     # "${PYBIN}/pip" install -r /io/dev-requirements.txt
     "${PYBIN}/pip" install numpy swig
     # "${PYBIN}/pip" wheel /opt/genn/ --no-deps -w dist/
-    make install -j `lscpu -p | egrep -v '^#' | sort -u -t, -k 2,4 | wc -l`
+    # make install -j `lscpu -p | egrep -v '^#' | sort -u -t, -k 2,4 | wc -l`
     make DYNAMIC=1 LIBRARY_DIRECTORY=${GENN_PATH}/pygenn/genn_wrapper/ -j `lscpu -p | egrep -v '^#' | sort -u -t, -k 2,4 | wc -l`
     "${PYBIN}/python" setup.py bdist_wheel
     "${PYBIN}/python" setup.py bdist_wheel
