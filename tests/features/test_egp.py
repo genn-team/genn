13fbdc671f77eabba929eddc3668866015296e9c
neworderofjamie <neworderofjamie@gmail.com>
2023-08-11 11:42:24 +0100

    extend test - crashing

--- tests/features/test_egp.py
+++ tests/features/test_egp.py
@@ -25,7 +25,7 @@ def test_egp_var_init(backend, precision):
         value = values[id % 10];
         """,
         extra_global_params=[("values", "scalar*")])
-    
+
     # Create var init snippet which fills variable with 10 repeated value
     pre_repeat_var_init_snippet = create_var_init_snippet(
         "pre_repeat_var_init",
@@ -34,7 +34,7 @@ def test_egp_var_init(backend, precision):
         value = values[id_pre % 10];
         """,
         extra_global_params=[("values", "scalar*")])
-        
+
     # Create var init snippet which fills variable with 10 repeated value
     post_repeat_var_init_snippet = create_var_init_snippet(
         "post_repeat_var_init",
@@ -64,30 +64,71 @@ def test_egp_var_init(backend, precision):
     
     model = GeNNModel(precision, "test_egp_var_init", backend=backend)
 
-    ss1_pop = model.add_neuron_population("SpikeSource1", 1, "SpikeSource", {}, {});
-    ss2_pop = model.add_neuron_population("SpikeSource2", 50000, "SpikeSource", {}, {});
+    ss_pop = model.add_neuron_population("SpikeSource", 20, "SpikeSource", {}, {});
     
     # Create populations with randomly-initialised variables
-    n_pop = model.add_neuron_population("Neurons", 50000, nop_neuron_model, {}, {"repeat": init_var(repeat_var_init_snippet)})
+    correct = np.arange(10.0)
+    n_pop = model.add_neuron_population("Neurons", 20, nop_neuron_model, {}, {"repeat": init_var(repeat_var_init_snippet)})
+    n_pop.vars["repeat"].extra_global_params["values"].set_values(correct)
+    
     cs = model.add_current_source("CurrentSource", nop_current_source_model, n_pop,
                                   {}, {"repeat": init_var(repeat_var_init_snippet)})
-
+    cs.vars["repeat"].extra_global_params["values"].set_values(correct)
+    
     dense_s_pop = model.add_synapse_population(
         "DenseSynapses", "DENSE", 0,
-        ss1_pop, n_pop,
-        nop_weight_update_model, {}, {"repeat": init_var(repeat_var_init_snippet)}, {"pre_repeat": init_var(pre_repeat_var_init_snippet)}, {"post_repeat": init_var(post_repeat_var_init_snippet)},
+        ss_pop, n_pop,
+        nop_weight_update_model, {}, {"repeat": init_var(pre_repeat_var_init_snippet)}, {"pre_repeat": init_var(repeat_var_init_snippet)}, {"post_repeat": init_var(repeat_var_init_snippet)},
         nop_postsynaptic_update_model, {}, {"psm_repeat": init_var(repeat_var_init_snippet)})
-        
+    dense_s_pop.vars["repeat"].extra_global_params["values"].set_values(correct)
+    dense_s_pop.pre_vars["pre_repeat"].extra_global_params["values"].set_values(correct)
+    dense_s_pop.post_vars["post_repeat"].extra_global_params["values"].set_values(correct)
+    dense_s_pop.psm_vars["psm_repeat"].extra_global_params["values"].set_values(correct)
+    
     sparse_s_pop = model.add_synapse_population(
         "SparseSynapses", "SPARSE", 0,
-        ss2_pop, n_pop,
-        nop_weight_update_model, {}, {"repeat": init_var(repeat_var_init_snippet)}, {"pre_repeat": init_var(pre_repeat_var_init_snippet)}, {"post_repeat": init_var(post_repeat_var_init_snippet)},
+        ss_pop, n_pop,
+        nop_weight_update_model, {}, {"repeat": init_var(post_repeat_var_init_snippet)}, {"pre_repeat": init_var(repeat_var_init_snippet)}, {"post_repeat": init_var(repeat_var_init_snippet)},
         "DeltaCurr", {}, {},
         init_sparse_connectivity("OneToOne"))
-    
+    dense_s_pop.vars["repeat"].extra_global_params["values"].set_values(correct)
+    dense_s_pop.pre_vars["pre_repeat"].extra_global_params["values"].set_values(correct)
+    dense_s_pop.post_vars["post_repeat"].extra_global_params["values"].set_values(correct)
+
     # Build model and load
     model.build()
     model.load()
+
+    # Loop through populations
+    tiled_correct = np.tile(correct, 2)
+    vars = [(n_pop, "", n_pop.vars), (cs, "", cs.vars),
+            (dense_s_pop, "pre_", dense_s_pop.pre_vars), 
+            (dense_s_pop, "post_", dense_s_pop.post_vars),
+            (dense_s_pop, "psm_", dense_s_pop.psm_vars,),
+            (sparse_s_pop, "pre_", dense_s_pop.pre_vars), 
+            (sparse_s_pop, "post_", dense_s_pop.post_vars)]
+    for pop, prefix, var_dict in vars:
+        # Pull var from devices
+        pop.pull_var_from_device(prefix + "repeat")
+
+        # If distribution is discrete
+        view = var_dict[prefix +  "repeat"].view
+        
+        if not np.allclose(view, tiled_correct):
+            assert False, f"'{pop.name}' initialisation incorrect"
+    
+    # Check dense
+    dense_s_pop.pull_var_from_device("repeat")
+    if not np.allclose(dense_s_pop.get_var_values("repeat"), np.tile(tiled_correct, (1, 20))):
+        assert False, f"'{dense_s_pop.name}' initialisation incorrect"
+    
+    # Download sparse connectivity
+    sparse_s_pop.pull_connectivity_from_device()
+
+    # Check sparse
+    sparse_s_pop.pull_var_from_device("repeat")
+    if not np.allclose(sparse_s_pop.get_var_values("repeat"), tiled_correct):
+        assert False, f"'{sparse_s_pop.name}' initialisation incorrect"
 
 if __name__ == '__main__':
     test_egp_var_init("single_threaded_cpu", types.Float)
