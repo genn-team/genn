09437c5bbf241bfec98620a8a34ad2bd880f815d
neworderofjamie <neworderofjamie@gmail.com>
2024-02-08 17:31:37 +0000

    started hooking up DOC macros to pybind11 bindings

--- pygenn/src/neuronModels.cc
+++ pygenn/src/neuronModels.cc
@@ -5,6 +5,9 @@
 // GeNN includes
 #include "neuronModels.h"
 
+// Doc strings
+#include "docStrings.h"
+
 using namespace GeNN::NeuronModels;
 
 namespace
@@ -28,16 +31,16 @@ PYBIND11_MODULE(neuron_models, m)
     //------------------------------------------------------------------------
     // **THINK** with some cunning, standard macros could maybe populate
     // an array with instance pointers that we could loop over
-    m.def("RulkovMap", &getBaseInstance<RulkovMap>, pybind11::return_value_policy::reference);
-    m.def("Izhikevich", &getBaseInstance<Izhikevich>, pybind11::return_value_policy::reference);
-    m.def("IzhikevichVariable", &getBaseInstance<IzhikevichVariable>, pybind11::return_value_policy::reference);
-    m.def("LIF", &getBaseInstance<LIF>, pybind11::return_value_policy::reference);
-    m.def("SpikeSource", &getBaseInstance<SpikeSource>, pybind11::return_value_policy::reference);
-    m.def("SpikeSourceArray", &getBaseInstance<SpikeSourceArray>, pybind11::return_value_policy::reference);
-    m.def("Poisson", &getBaseInstance<Poisson>, pybind11::return_value_policy::reference);
-    m.def("PoissonNew", &getBaseInstance<PoissonNew>, pybind11::return_value_policy::reference);
-    m.def("TraubMiles", &getBaseInstance<TraubMiles>, pybind11::return_value_policy::reference);
-    m.def("TraubMilesFast", &getBaseInstance<TraubMilesFast>, pybind11::return_value_policy::reference);
-    m.def("TraubMilesAlt", &getBaseInstance<TraubMilesAlt>, pybind11::return_value_policy::reference);
-    m.def("TraubMilesNStep", &getBaseInstance<TraubMilesNStep>, pybind11::return_value_policy::reference);
+    m.def("RulkovMap", &getBaseInstance<RulkovMap>, pybind11::return_value_policy::reference, DOC(NeuronModels, RulkovMap));
+    m.def("Izhikevich", &getBaseInstance<Izhikevich>, pybind11::return_value_policy::reference, DOC(NeuronModels, Izhikevich));
+    m.def("IzhikevichVariable", &getBaseInstance<IzhikevichVariable>, pybind11::return_value_policy::reference, DOC(NeuronModels, IzhikevichVariable));
+    m.def("LIF", &getBaseInstance<LIF>, pybind11::return_value_policy::reference, DOC(NeuronModels, LIF));
+    m.def("SpikeSource", &getBaseInstance<SpikeSource>, pybind11::return_value_policy::reference, DOC(NeuronModels, SpikeSource));
+    m.def("SpikeSourceArray", &getBaseInstance<SpikeSourceArray>, pybind11::return_value_policy::reference, DOC(NeuronModels, SpikeSourceArray));
+    m.def("Poisson", &getBaseInstance<Poisson>, pybind11::return_value_policy::reference, DOC(NeuronModels, Poisson));
+    m.def("PoissonNew", &getBaseInstance<PoissonNew>, pybind11::return_value_policy::reference, DOC(NeuronModels, PoissonNew));
+    m.def("TraubMiles", &getBaseInstance<TraubMiles>, pybind11::return_value_policy::reference, DOC(NeuronModels, TraubMiles));
+    m.def("TraubMilesFast", &getBaseInstance<TraubMilesFast>, pybind11::return_value_policy::reference, DOC(NeuronModels, TraubMilesFast));
+    m.def("TraubMilesAlt", &getBaseInstance<TraubMilesAlt>, pybind11::return_value_policy::reference, DOC(NeuronModels, TraubMilesAlt));
+    m.def("TraubMilesNStep", &getBaseInstance<TraubMilesNStep>, pybind11::return_value_policy::reference, DOC(NeuronModels, TraubMilesNStep));
 }
