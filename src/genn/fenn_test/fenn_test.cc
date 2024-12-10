f124142188a08c6718f39d0d011f0ba48c564728
neworderofjamie <neworderofjamie@gmail.com>
2024-12-10 18:44:24 +0000

    first, very hacky pass at assembler

--- src/genn/fenn_test/fenn_test.cc
+++ src/genn/fenn_test/fenn_test.cc
@@ -1,6 +1,9 @@
 // Standard C++ includes
 #include <variant>
 
+// Third-party includes
+#include <fast_float/fast_float.h>
+
 // GeNN includes
 #include "gennUtils.h"
 #include "neuronModels.h"
@@ -93,6 +96,394 @@ private:
     std::unordered_map<std::string, RegisterPtr> m_LocalVariables;
 };
 
+//---------------------------------------------------------------------------
+// Visitor
+//---------------------------------------------------------------------------
+class Visitor : public Expression::Visitor, public Statement::Visitor
+{
+public:
+    Visitor(const Statement::StatementList &statements, EnvironmentInternal &environment,
+            const Type::TypeContext &context, const TypeChecker::ResolvedTypeMap &resolvedTypes,
+            ScalarRegisterAllocator &scalarRegisterAllocator, VectorRegisterAllocator &vectorRegisterAllocator)
+    :   m_Environment(environment), m_Context(context), m_ResolvedTypes(resolvedTypes), 
+        m_ScalarRegisterAllocator(scalarRegisterAllocator), m_VectorRegisterAllocator(vectorRegisterAllocator)
+    {
+         for(auto &s : statements) {
+            s.get()->accept(*this);
+        }
+    }
+
+    Visitor(const Expression::ExpressionPtr &expression, EnvironmentInternal &environment,
+            const Type::TypeContext &context, const TypeChecker::ResolvedTypeMap &resolvedTypes,
+            ScalarRegisterAllocator &scalarRegisterAllocator, VectorRegisterAllocator &vectorRegisterAllocator)
+    :   m_Environment(environment), m_Context(context), m_ResolvedTypes(resolvedTypes),
+        m_ScalarRegisterAllocator(scalarRegisterAllocator), m_VectorRegisterAllocator(vectorRegisterAllocator)
+    {
+        expression.get()->accept(*this);
+    }
+
+private:
+    //---------------------------------------------------------------------------
+    // Expression::Visitor virtuals
+    //---------------------------------------------------------------------------
+    virtual void visit(const Expression::ArraySubscript &arraySubscript) final
+    {
+        assert(false);
+    }
+
+    virtual void visit(const Expression::Assignment &assignement) final
+    {
+        assignement.getAssignee()->accept(*this);
+        const auto assigneeReg = m_ExpressionRegister.value();
+        assert(std::holds_alternative<VectorRegisterAllocator::RegisterPtr>(assigneeReg));
+
+        const auto varReg = m_VectorRegisterAllocator.getRegister();
+
+        // Copy assigneeReg into result
+        // **TODO** add flag alongside expression register to specify whether it can be trashed
+        m_Environment.get().getCodeGenerator().vadd(*varReg, *std::get<VectorRegisterAllocator::RegisterPtr>(assigneeReg),
+                                                    *std::get<VectorRegisterAllocator::RegisterPtr>(m_Environment.get().getRegister("_zero")));
+        
+        m_ExpressionRegister = varReg;
+    }
+
+    virtual void visit(const Expression::Binary &binary) final
+    {
+        binary.getLeft()->accept(*this);
+        const auto leftReg = m_ExpressionRegister.value();
+        assert(std::holds_alternative<VectorRegisterAllocator::RegisterPtr>(leftReg));
+
+        binary.getRight()->accept(*this);
+        const auto rightReg = m_ExpressionRegister.value();
+        assert(std::holds_alternative<VectorRegisterAllocator::RegisterPtr>(rightReg));
+
+        const auto resultReg = m_VectorRegisterAllocator.getRegister();
+        if(binary.getOperator().type == Token::Type::MINUS) {
+            // **TODO** saturation?
+            m_Environment.get().getCodeGenerator().vsub(*resultReg, *std::get<VectorRegisterAllocator::RegisterPtr>(leftReg), 
+                                                        *std::get<VectorRegisterAllocator::RegisterPtr>(rightReg));
+        }
+        else if(binary.getOperator().type == Token::Type::PLUS) {
+            // **TODO** saturation?
+            m_Environment.get().getCodeGenerator().vadd(*resultReg, *std::get<VectorRegisterAllocator::RegisterPtr>(leftReg), 
+                                                        *std::get<VectorRegisterAllocator::RegisterPtr>(rightReg));
+        }
+        else if(binary.getOperator().type == Token::Type::STAR) {
+            // **TODO** fixed point format
+            m_Environment.get().getCodeGenerator().vmul(8, *resultReg, *std::get<VectorRegisterAllocator::RegisterPtr>(leftReg), 
+                                                        *std::get<VectorRegisterAllocator::RegisterPtr>(rightReg));
+        }
+        else {
+            assert(false);
+        }
+
+        // Set result register
+        m_ExpressionRegister = resultReg;
+    }
+
+    virtual void visit(const Expression::Call &call) final
+    {
+        assert(false);
+    }
+
+    virtual void visit(const Expression::Cast &cast) final
+    {
+        // **NOTE** leave expression register alone
+        cast.getExpression()->accept(*this);
+    }
+
+    virtual void visit(const Expression::Conditional &conditional) final
+    {
+        conditional.getCondition()->accept(*this);
+        const auto conditionReg = m_ExpressionRegister.value();
+        assert(std::holds_alternative<ScalarRegisterAllocator::RegisterPtr>(conditionReg));
+
+        conditional.getTrue()->accept(*this);
+        const auto trueReg = m_ExpressionRegister.value();
+        assert(std::holds_alternative<VectorRegisterAllocator::RegisterPtr>(trueReg));
+
+        conditional.getFalse()->accept(*this);
+        const auto falseReg = m_ExpressionRegister.value();
+        assert(std::holds_alternative<VectorRegisterAllocator::RegisterPtr>(falseReg));
+
+        m_Environment.get().getCodeGenerator().vsel(*std::get<VectorRegisterAllocator::RegisterPtr>(falseReg),
+                                                    *std::get<ScalarRegisterAllocator::RegisterPtr>(conditionReg),
+                                                    *std::get<VectorRegisterAllocator::RegisterPtr>(trueReg));
+        m_ExpressionRegister = falseReg;
+    }
+
+    virtual void visit(const Expression::Grouping &grouping) final
+    {
+        // **NOTE** leave expression register alone
+        grouping.getExpression()->accept(*this);
+    }
+
+    virtual void visit(const Expression::Literal &literal) final
+    {
+        const char *lexemeBegin = literal.getValue().lexeme.c_str();
+        const char *lexemeEnd = lexemeBegin + literal.getValue().lexeme.size();
+        
+        // If literal is uint
+        int64_t integerResult;
+        if (literal.getValue().type == Token::Type::UINT32_NUMBER) {
+            unsigned int result;
+            auto answer = fast_float::from_chars(lexemeBegin, lexemeEnd, result);
+            assert(answer.ec == std::errc());
+            integerResult = result;
+        }
+        else if(literal.getValue().type == Token::Type::INT32_NUMBER) {
+            int result;
+            auto answer = fast_float::from_chars(lexemeBegin, lexemeEnd, result);
+            assert(answer.ec == std::errc());
+            integerResult = result;
+        }
+        else {
+            // **TODO** fixed point
+            float result;
+            auto answer = fast_float::from_chars(lexemeBegin, lexemeEnd, result);
+            assert(answer.ec == std::errc());
+            integerResult = std::round(result * (1u << 8));
+        }
+
+        assert(integerResult >= std::numeric_limits<int16_t>::min());
+        assert(integerResult <= std::numeric_limits<int16_t>::max());
+
+        const auto resultReg = m_VectorRegisterAllocator.getRegister();
+        m_Environment.get().getCodeGenerator().vlui(*resultReg, (uint32_t)integerResult);
+        m_ExpressionRegister = resultReg;
+    }
+
+    virtual void visit(const Expression::Logical &logical) final
+    {
+        logical.getLeft()->accept(*this);
+        const auto leftReg = m_ExpressionRegister.value();
+        assert(std::holds_alternative<ScalarRegisterAllocator::RegisterPtr>(leftReg));
+
+        logical.getRight()->accept(*this);
+        const auto rightReg = m_ExpressionRegister.value();
+        assert(std::holds_alternative<ScalarRegisterAllocator::RegisterPtr>(rightReg));
+
+        // **TODO** short-circuiting
+        const auto resultReg = m_ScalarRegisterAllocator.getRegister();
+        if(logical.getOperator().type == Token::Type::AMPERSAND_AMPERSAND) {
+            m_Environment.get().getCodeGenerator().and_(*resultReg, *std::get<ScalarRegisterAllocator::RegisterPtr>(leftReg),
+                                                        *std::get<ScalarRegisterAllocator::RegisterPtr>(rightReg));
+        }
+        else if(logical.getOperator().type == Token::Type::PIPE_PIPE){
+            m_Environment.get().getCodeGenerator().or_(*resultReg, *std::get<ScalarRegisterAllocator::RegisterPtr>(leftReg),
+                                                       *std::get<ScalarRegisterAllocator::RegisterPtr>(rightReg));
+        }
+        else {
+            assert(false);
+        }
+
+        m_ExpressionRegister = resultReg;
+    }
+
+    virtual void visit(const Expression::PostfixIncDec &postfixIncDec) final
+    {
+        assert(false);
+        //postfixIncDec.getTarget()->accept(*this);
+        //m_Environment.get().getStream() <<  postfixIncDec.getOperator().lexeme;
+    }
+
+    virtual void visit(const Expression::PrefixIncDec &prefixIncDec) final
+    {
+        assert(false);
+        //m_Environment.get().getStream() << prefixIncDec.getOperator().lexeme;
+        //prefixIncDec.getTarget()->accept(*this);
+    }
+
+    virtual void visit(const Expression::Identifier &variable) final
+    {
+        m_ExpressionRegister = m_Environment.get().getRegister(variable.getName().lexeme);
+    }
+
+    virtual void visit(const Expression::Unary &unary) final
+    {
+        assert(false);
+        //m_Environment.get().getStream() << unary.getOperator().lexeme;
+        //unary.getRight()->accept(*this);
+    }
+
+    //---------------------------------------------------------------------------
+    // Statement::Visitor virtuals
+    //---------------------------------------------------------------------------
+    virtual void visit(const Statement::Break&) final
+    {
+        assert(false);
+    }
+
+    virtual void visit(const Statement::Compound &compound) final
+    {
+        // Cache reference to current reference
+        std::reference_wrapper<EnvironmentBase> oldEnvironment = m_Environment; 
+
+        // Create new environment and set to current
+        EnvironmentInternal environment(m_Environment);
+        m_Environment = environment;
+
+        for(auto &s : compound.getStatements()) {
+            s->accept(*this);
+        }
+
+        // Restore old environment
+        m_Environment = oldEnvironment;
+    }
+
+    virtual void visit(const Statement::Continue&) final
+    {
+        assert(false);
+    }
+
+    virtual void visit(const Statement::Do &doStatement) final
+    {
+        assert(false);
+        /*m_Environment.get().getStream() << "do";
+        doStatement.getBody()->accept(*this);
+        m_Environment.get().getStream() << "while(";
+        doStatement.getCondition()->accept(*this);
+        m_Environment.get().getStream() << ");" << std::endl;*/
+    }
+
+    virtual void visit(const Statement::Expression &expression) final
+    {
+        if(expression.getExpression()) {
+            expression.getExpression()->accept(*this);
+        }
+    }
+
+    virtual void visit(const Statement::For &forStatement) final
+    {
+        // Cache reference to current reference
+        /*std::reference_wrapper<EnvironmentBase> oldEnvironment = m_Environment; 
+
+        // Create new environment and set to current
+        EnvironmentInternal environment(m_Environment);
+        m_Environment = environment;
+
+        m_Environment.get().getStream() << "for(";
+        if(forStatement.getInitialiser()) {
+            forStatement.getInitialiser()->accept(*this);
+        }
+        else {
+            m_Environment.get().getStream() << ";";
+        }
+        m_Environment.get().getStream() << " ";
+
+        if(forStatement.getCondition()) {
+            forStatement.getCondition()->accept(*this);
+        }
+
+        m_Environment.get().getStream() << "; ";
+        if(forStatement.getIncrement()) {
+            forStatement.getIncrement()->accept(*this);
+        }
+        m_Environment.get().getStream() << ")";
+        forStatement.getBody()->accept(*this);
+
+        // Restore old environment
+        m_Environment = oldEnvironment;*/
+    }
+
+    virtual void visit(const Statement::ForEachSynapse &forEachSynapseStatement) final
+    {
+        assert(false);
+    }
+
+    virtual void visit(const Statement::If &ifStatement) final
+    {
+        ifStatement.getCondition()->accept(*this);
+        const auto conditionReg = m_ExpressionRegister.value();
+        assert(std::holds_alternative<ScalarRegisterAllocator::RegisterPtr>(conditionReg));
+
+        // If we already have a mask register, AND it with result of condition into new register
+        std::optional<RegisterPtr> oldMaskRegister = m_MaskRegister;
+        if(oldMaskRegister) {
+            auto combinedMaskRegister = m_ScalarRegisterAllocator.getRegister();
+            m_Environment.get().getCodeGenerator().and_(*combinedMaskRegister, *std::get<ScalarRegisterAllocator::RegisterPtr>(*oldMaskRegister),
+                                                        *std::get<ScalarRegisterAllocator::RegisterPtr>(conditionReg));
+            m_MaskRegister = combinedMaskRegister;
+        }
+        // Otherwise, just
+        else {
+            m_MaskRegister = conditionReg;
+        }
+
+        ifStatement.getThenBranch()->accept(*this);
+
+
+        if(ifStatement.getElseBranch()) {
+            // Negate mask
+            auto elseMaskRegister = m_ScalarRegisterAllocator.getRegister();
+            m_Environment.get().getCodeGenerator().not_(*elseMaskRegister, *std::get<ScalarRegisterAllocator::RegisterPtr>(conditionReg));
+
+            // If we have an old mask, and it with this
+            if(oldMaskRegister) {
+                m_Environment.get().getCodeGenerator().and_(*elseMaskRegister, *std::get<ScalarRegisterAllocator::RegisterPtr>(*oldMaskRegister),
+                                                            *elseMaskRegister);
+            }
+
+            m_MaskRegister = elseMaskRegister;
+
+            ifStatement.getElseBranch()->accept(*this);
+        }
+
+        // Restore old mask register
+        m_MaskRegister = oldMaskRegister;
+    }
+
+    virtual void visit(const Statement::Labelled &labelled) final
+    {
+        assert(false);
+    }
+
+    virtual void visit(const Statement::Switch &switchStatement) final
+    {
+        assert(false);
+    }
+
+    virtual void visit(const Statement::VarDeclaration &varDeclaration) final
+    {
+        const size_t numDeclarators = varDeclaration.getInitDeclaratorList().size();
+        for(size_t i = 0; i < numDeclarators; i++) {
+            const auto &var = varDeclaration.getInitDeclaratorList()[i];
+
+            // Allocate register and define variable to it
+            const auto varReg = m_VectorRegisterAllocator.getRegister(std::get<0>(var).lexeme.c_str());
+            m_Environment.get().define(std::get<0>(var).lexeme, varReg);
+            
+            if(std::get<1>(var)) {
+                std::get<1>(var)->accept(*this);
+
+                // Copy result into result
+                // **TODO** add flag alongside expression register to specify whether it can be trashed
+                m_Environment.get().getCodeGenerator().vadd(*varReg, *std::get<VectorRegisterAllocator::RegisterPtr>(*m_ExpressionRegister),
+                                                            *std::get<VectorRegisterAllocator::RegisterPtr>(m_Environment.get().getRegister("_zero")));
+            } 
+        }
+    }
+
+    virtual void visit(const Statement::While &whileStatement) final
+    {
+        /*m_Environment.get().getStream() << "while(";
+        whileStatement.getCondition()->accept(*this);
+        m_Environment.get().getStream() << ")" << std::endl;
+        whileStatement.getBody()->accept(*this);*/
+    }
+
+private:
+    //---------------------------------------------------------------------------
+    // Members
+    //---------------------------------------------------------------------------
+    std::reference_wrapper<EnvironmentBase> m_Environment;
+    const Type::TypeContext &m_Context;
+    std::optional<RegisterPtr> m_ExpressionRegister;
+    std::optional<RegisterPtr> m_MaskRegister;
+    const TypeChecker::ResolvedTypeMap &m_ResolvedTypes;
+    ScalarRegisterAllocator &m_ScalarRegisterAllocator;
+    VectorRegisterAllocator &m_VectorRegisterAllocator;
+};
 
 class EnvironmentExternal : public EnvironmentBase, public Transpiler::TypeChecker::EnvironmentBase
 {
@@ -236,4 +627,46 @@ int main()
         throw std::runtime_error("Parse error " + errorHandler.getContext());
     }
 
+    ScalarRegisterAllocator scalarRegisterAllocator;
+    VectorRegisterAllocator vectorRegisterAllocator;
+    Assembler::CodeGenerator codeGenerator;
+    EnvironmentExternal env(codeGenerator);
+
+    // Allocate registers for Isyn and dt
+    env.add(Type::Int16, "Isyn", vectorRegisterAllocator.getRegister("Isyn"));
+    env.add(Type::Int16, "dt", vectorRegisterAllocator.getRegister("dt"));
+
+    env.add(Type::Int16, "_zero", vectorRegisterAllocator.getRegister("_zero"));
+
+    // Allocate registers for neuron variables
+    const auto neuronVars = model->getVars();
+    for(const auto &v : neuronVars) {
+        env.add(Type::Int16, v.name, vectorRegisterAllocator.getRegister(v.name.c_str()));
+    }
+
+    // Allocate registers for neuron parameters
+    const auto neuronParams = model->getParams();
+    for(const auto &p : neuronParams) {
+        env.add(Type::Int16, p.name, vectorRegisterAllocator.getRegister(p.name.c_str()));
+    }
+
+    // Allocate registers for neuron derived
+    const auto neuronDerivedParams = model->getDerivedParams();
+    for(const auto &d : neuronDerivedParams) {
+        env.add(Type::Int16, d.name, vectorRegisterAllocator.getRegister(d.name.c_str()));
+    }
+
+    // Resolve types within one scope
+    TypeChecker::EnvironmentInternal typeCheckEnv(env);
+    EnvironmentInternal assemblerEnv(env);
+    {
+        const auto resolvedTypeMap = TypeChecker::typeCheck(simCodeStatements, typeCheckEnv, typeContext, errorHandler);
+        if(errorHandler.hasError()) {
+            throw std::runtime_error("Type check error " + errorHandler.getContext());
+        }
+
+
+        Visitor visitor(simCodeStatements, assemblerEnv, typeContext, 
+                        resolvedTypeMap, scalarRegisterAllocator, vectorRegisterAllocator);
+    }
 }
