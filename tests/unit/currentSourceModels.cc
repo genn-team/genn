dff0580a8214e90750fb69a8c245cea06cc3eb21
neworderofjamie <neworderofjamie@gmail.com>
2025-03-06 11:56:45 +0000

    fixed unit tests

--- tests/unit/currentSourceModels.cc
+++ tests/unit/currentSourceModels.cc
@@ -38,24 +38,24 @@ TEST(CurrentSourceModels, ValidateParamValues)
     const ParamValues paramValsMissing{{"mean", 0.0}};
     const ParamValues paramValsExtra{{"mean", 0.0}, {"sd", 1.0}, {"var", 1.0}};
 
-    CurrentSourceModels::GaussianNoise::getInstance()->validate(paramValsCorrect, {}, {}, "Current source");
+    CurrentSourceModels::GaussianNoise::getInstance()->validate(paramValsCorrect, {}, {}, {}, "Current source");
 
     try {
-        CurrentSourceModels::GaussianNoise::getInstance()->validate(paramValsMisSpelled, {}, {}, "Current source");
+        CurrentSourceModels::GaussianNoise::getInstance()->validate(paramValsMisSpelled, {}, {}, {}, "Current source");
         FAIL();
     }
     catch(const std::runtime_error &) {
     } 
 
     try {
-        CurrentSourceModels::GaussianNoise::getInstance()->validate(paramValsMissing, {}, {}, "Current source");
+        CurrentSourceModels::GaussianNoise::getInstance()->validate(paramValsMissing, {}, {}, {}, "Current source");
         FAIL();
     }
     catch(const std::runtime_error &) {
     } 
 
     try {
-        CurrentSourceModels::GaussianNoise::getInstance()->validate(paramValsExtra, {}, {}, "Current source");
+        CurrentSourceModels::GaussianNoise::getInstance()->validate(paramValsExtra, {}, {}, {}, "Current source");
         FAIL();
     }
     catch(const std::runtime_error &) {
@@ -71,24 +71,24 @@ TEST(CurrentSourceModels, ValidateVarValues)
     const VarValues varValsMissing{};
     const VarValues varValsExtra{{"current", 0.0}, {"power", 1.0}};
 
-    CurrentSourceModels::PoissonExp::getInstance()->validate(paramVals, varValsCorrect, {}, "Current source");
+    CurrentSourceModels::PoissonExp::getInstance()->validate(paramVals, varValsCorrect, {}, {}, "Current source");
 
     try {
-        CurrentSourceModels::PoissonExp::getInstance()->validate(paramVals, varValsMisSpelled, {}, "Current source");
+        CurrentSourceModels::PoissonExp::getInstance()->validate(paramVals, varValsMisSpelled, {}, {}, "Current source");
         FAIL();
     }
     catch(const std::runtime_error &) {
     } 
 
     try {
-        CurrentSourceModels::PoissonExp::getInstance()->validate(paramVals, varValsMissing, {}, "Current source");
+        CurrentSourceModels::PoissonExp::getInstance()->validate(paramVals, varValsMissing, {}, {}, "Current source");
         FAIL();
     }
     catch(const std::runtime_error &) {
     } 
 
     try {
-        CurrentSourceModels::PoissonExp::getInstance()->validate(paramVals, varValsExtra, {}, "Current source");
+        CurrentSourceModels::PoissonExp::getInstance()->validate(paramVals, varValsExtra, {}, {}, "Current source");
         FAIL();
     }
     catch(const std::runtime_error &) {
